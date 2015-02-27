32b4cf7c0ab80141c3ea902febecda290db6837e
Vincent Fiduccia <vincent@rancher.com>
2015-02-26 23:27:41 -0700

    Fix changing github clientId after pageload

--- app/pods/settings/auth/controller.js
+++ app/pods/settings/auth/controller.js
@@ -55,9 +55,13 @@ export default Ember.ObjectController.extend({
       this.send('clearError');
 
       config.providers['github-oauth2'].apiKey = this.get('model.clientId');
-      githubOauth.state = Math.random()+"";
 
-      this.get('torii').open('github-oauth2',{windowOptions: util.popupWindowOptions()}).then(github => {
+      var torii = this.get('torii');
+      var provider = torii.container.lookup('torii-provider:github-oauth2');
+      provider.set('clientId', this.get('model.clientId'));
+      provider.set('state', Math.random()+"");
+
+      torii.open('github-oauth2',{windowOptions: util.popupWindowOptions()}).then(github => {
         var headers = {};
         headers[C.AUTH_HEADER] = undefined; // Explicitly not send auth
         headers[C.PROJECT_HEADER] = undefined; // Explicitly not send project
