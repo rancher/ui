7e2868f8aaf8c60567f3b36451a2bf0067052e79
Vincent Fiduccia <vincent@rancher.com>
2015-03-16 14:28:21 -0700

    Update ember and ember-cli

--- config/environment.js
+++ config/environment.js
@@ -57,7 +57,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_TRANSITIONS = true;
     ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.contentSecurityPolicy['script-src'] = ENV.contentSecurityPolicy['script-src'] + " 'unsafe-eval'";
+//    ENV.contentSecurityPolicy['script-src'] = ENV.contentSecurityPolicy['script-src'] + " 'unsafe-eval'";
   }
 
   if (environment === 'test') {
