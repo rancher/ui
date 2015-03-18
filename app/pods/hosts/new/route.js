1f4d9bb7253c3610abf942b7b3ab22f87676331a
Vincent Fiduccia <vincent@rancher.com>
2015-03-17 23:56:22 -0700

    Don't blow up on add host for project

--- app/pods/hosts/new/route.js
+++ app/pods/hosts/new/route.js
@@ -3,11 +3,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
+    var store = this.get('store');
     var userType = this.get('session').get(C.USER_TYPE_SESSION_KEY);
     var isAdmin = userType === undefined || userType === C.USER_TYPE_ADMIN;
-    if ( isAdmin )
+    if ( isAdmin && store.hasRecordFor('schema','setting') )
     {
-      return this.get('store').find('setting', C.SETTING_API_HOST).then((setting) => {
+      return store.find('setting', C.SETTING_API_HOST).then((setting) => {
         if ( setting.get('value') )
         {
           return Ember.RSVP.resolve();
