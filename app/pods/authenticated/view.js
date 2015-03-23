9ca504f8b980dd4f1efb5456e0e167432e8e2f30
Vincent Fiduccia <vincent@rancher.com>
2015-03-23 14:06:57 -0700

    Bordered main white box

--- app/pods/authenticated/view.js
+++ app/pods/authenticated/view.js
@@ -1,15 +1,31 @@
 import Ember from "ember";
 
 //   Toggle position:relative on the main page container depending on the height of the content
-var cur = null;
+var pos = null;
+var mainHeight = null;
 function relativeRelative() {
   var auth = $('.authenticated');
-  var neu = ( auth.height() > $(window).height() ? 'relative' : 'static');
+  var winHeight = $(window).height();
+  var neu = ( auth.height() > winHeight ? 'relative' : 'static');
 
-  if ( neu !== cur )
+  if ( neu !== pos )
   {
-    cur = neu;
-    auth.css({position: cur});
+    pos = neu;
+    auth.css({position: pos});
+  }
+
+  var white = $('BODY').hasClass('white');
+  var main = $('MAIN');
+  var height = null;
+  if ( white )
+  {
+    height = winHeight - main.position().top - parseInt(main.css('margin-top'),10) - parseInt(main.css('margin-bottom'),10);
+  }
+
+  if ( height !== mainHeight )
+  {
+    mainHeight = height;
+    main.css('min-height', height);
   }
 }
 
