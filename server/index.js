06b535f5763dd404b89fe094291382e8e5333c95
Vincent Fiduccia <vincent@rancher.com>
2015-04-08 16:07:07 -0700

    Remove unused body parsing

--- server/index.js
+++ server/index.js
@@ -8,22 +8,10 @@
 // };
 
 module.exports = function(app, options) {
-  var bodyParser = require('body-parser');
   var globSync   = require('glob').sync;
   var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
   var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-
   mocks.forEach(function(route) { route(app, options); });
-
-  // proxy expects a stream, but express will have turned
-  // the request stream into an object because bodyParser
-  // has run. We have to convert it back to stream:
-  // https://github.com/nodejitsu/node-http-proxy/issues/180
-  app.use(require('connect-restreamer')());
   proxies.forEach(function(route) { route(app, options); });
 };
