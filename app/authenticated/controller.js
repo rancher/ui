1d60267c189c080703266f240570cb259e3c9d59
Vincent Fiduccia <vincent@rancher.com>
2015-04-17 12:19:51 -0700

    Send Auth info when downloading machine/enviroment

--- app/authenticated/controller.js
+++ app/authenticated/controller.js
@@ -1,4 +1,6 @@
-import Ember from "ember";
+import Ember from 'ember';
+import Util from 'ui/utils/util';
+import C from 'ui/utils/constants';
 
 export default Ember.Controller.extend({
   error: null,
@@ -15,4 +17,20 @@ export default Ember.Controller.extend({
   projects: null,
   project: null,
 
+  addAuthParams: function(url) {
+    var session = this.get('session');
+    var token = session.get(C.AUTH_SESSION_KEY);
+    if ( token )
+    {
+      url = Util.addQueryParam(url, 'token', token);
+    }
+
+    var projectId = session.get(C.PROJECT_SESSION_KEY);
+    if ( projectId )
+    {
+      url = Util.addQueryParam(url, 'projectId', projectId);
+    }
+
+    return url;
+  }
 });
