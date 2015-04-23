cc145bc472f9207b410eca2a8a7c36e3d2128847
Vincent Fiduccia <vincent@rancher.com>
2015-04-22 18:52:51 -0700

    Send user header for more things

--- app/activesetting/model.js
+++ app/activesetting/model.js
@@ -1,13 +1,13 @@
 import Cattle from 'ui/utils/cattle';
 import C from 'ui/utils/constants';
 
-var Setting = Cattle.TransitioningResource.extend({
+var ActiveSetting = Cattle.TransitioningResource.extend({
 });
 
-Setting.reopenClass({
+ActiveSetting.reopenClass({
   headers: {
     [C.HEADER.PROJECT]: C.HEADER.PROJECT_USER_SCOPE, // Requests for projects use the user's scope, not the project
   }
 });
 
-export default Setting;
+export default ActiveSetting;
