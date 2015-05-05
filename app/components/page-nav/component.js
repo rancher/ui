45cc990d1e0da4e8c86f2681195211060cc682ef
Vincent Fiduccia <vincent@rancher.com>
2015-05-05 10:15:10 -0700

    Enable services tab

--- app/components/page-nav/component.js
+++ app/components/page-nav/component.js
@@ -7,6 +7,6 @@ export default Ember.Component.extend({
   tagName: 'nav',
   hasServices: function() {
     var store = this.get('store');
-    return store && store.hasRecordFor('schema','service') && this.get('prefs.showServices');
+    return store && store.hasRecordFor('schema','service');
   }.property(),
 });
