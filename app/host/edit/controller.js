c58f5d3551f4accb7b09d825c59635305f7afc11
Vincent Fiduccia <vincent@rancher.com>
2015-05-28 10:54:45 -0700

    Host labeling

--- app/host/edit/controller.js
+++ app/host/edit/controller.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import Cattle from 'ui/utils/cattle';
+import EditLabels from 'ui/mixins/edit-labels';
 
-export default Ember.ObjectController.extend(Cattle.NewOrEditMixin, {
+export default Ember.ObjectController.extend(Cattle.NewOrEditMixin, EditLabels, {
   editing: true,
 
   doneSaving: function() {
--- app/host/edit/view.js
+++ app/host/edit/view.js
@@ -1,4 +1,14 @@
-import Overlay from "ui/overlay/view";
+import Ember from 'ember';
+import Overlay from 'ui/overlay/view';
+
+export function addAction(action, selector) {
+  return function() {
+    this.get('controller').send(action);
+    Ember.run.next(this, function() {
+      this.$(selector).last().focus();
+    });
+  };
+}
 
 export default Overlay.extend({
   actions: {
@@ -9,5 +19,7 @@ export default Overlay.extend({
     overlayEnter: function() {
       this.get('controller').send('save');
     },
+
+    addLabel:       addAction('addLabel',      '.label-key'),
   }
 });
