05341a2f1cb777bef96cbee020c6638bac025add
Vincent Fiduccia <vincent@rancher.com>
2015-06-04 22:56:06 -0700

    Cannot scale dns or external

--- app/components/labels-section/component.js
+++ app/components/labels-section/component.js
@@ -1,32 +1,8 @@
 import Ember from 'ember';
 import C from 'ui/utils/constants';
+import ReadLabels from 'ui/mixins/read-labels';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(ReadLabels,{
   model: null,
-
-  labelArray: function() {
-    var out = [];
-    var obj = this.get('model')||{};
-    var keys = Ember.keys(obj);
-    keys.forEach(function(key) {
-      var type = 'user';
-      if ( key.indexOf(C.LABEL.SCHED_AFFINITY) === 0 )
-      {
-        type = 'affinity';
-      }
-      else if ( key.indexOf(C.LABEL.SYSTEM_PREFIX) === 0 )
-      {
-        type = 'system';
-      }
-
-      out.push(Ember.Object.create({
-        key: key,
-        value: obj[key],
-        type: type,
-        isUser: (type === 'user'),
-      }));
-    });
-
-    return out;
-  }.property('model.@each.{key,value}'),
+  labelResource: Ember.computed.alias('model'),
 });
--- app/service/edit/controller.js
+++ app/service/edit/controller.js
@@ -1,12 +1,20 @@
 import Ember from 'ember';
 import Cattle from 'ui/utils/cattle';
 import EditService from 'ui/mixins/edit-service';
+import C from 'ui/utils/constants';
 
 export default Ember.ObjectController.extend(Cattle.NewOrEditMixin, EditService, {
   editing: true,
 
-  hasScale: function() {
-    return this.get('service.type') !== 'dnsService';
+  canScale: function() {
+    if ( ['service','loadbalancerservice'].indexOf(this.get('service.type').toLowerCase()) >= 0 )
+    {
+      return !this.getLabel(C.LABEL.SCHED_GLOBAL);
+    }
+    else
+    {
+      return false;
+    }
   }.property('service.type'),
 
   doneSaving: function() {
