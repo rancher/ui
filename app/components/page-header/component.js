aff9decba0aa626a79c0cc10733aeeb68a57dfff
Vincent Fiduccia <vincent@rancher.com>
2015-06-10 16:49:22 -0700

    Download compose cli

--- app/components/page-header/component.js
+++ app/components/page-header/component.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import C from 'ui/utils/constants';
+import Util from 'ui/utils/util';
 
 export default Ember.Component.extend({
   currentPath: null,
@@ -42,6 +43,22 @@ export default Ember.Component.extend({
 
     goToPrevious: function() {
       this.sendAction('goToPrevious');
-    }
+    },
+
+    composeDownload: function(os) {
+      this.get('store').find('setting',null,{filter: {all: 'false'}}).then((settings) => {
+        var map = {};
+        settings.forEach((setting) => {
+          var name = setting.get('name').replace(/\./g,'_').toLowerCase();
+          map[name] = setting.get('value');
+        });
+
+        var url = map['rancher_compose_'+os+'_url'];
+        if ( url )
+        {
+          Util.download(url);
+        }
+      });
+    },
   },
 });
