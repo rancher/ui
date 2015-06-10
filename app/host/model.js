9d91103c39d7db39ddaf308188ff4b917987971e
Vincent Fiduccia <vincent@rancher.com>
2015-06-10 14:09:04 -0700

    Show agent state on host

--- app/host/model.js
+++ app/host/model.js
@@ -10,8 +10,8 @@ var Host = Cattle.TransitioningResource.extend({
 
   state: function() {
     var host = this.get('hostState');
-    var agent = this.get('agent.state');
-    if ( host === 'active' && agent  )
+    var agent = this.get('agentState');
+    if ( host === 'active' && agent )
     {
       return agent;
     }
@@ -19,56 +19,16 @@ var Host = Cattle.TransitioningResource.extend({
     {
       return host;
     }
-  }.property('hostState','agent.state'),
-
-  transitioning: function() {
-    var host = this.get('hostTransitioning');
-    var agent = this.get('agent.transitioning');
-    if ( host === 'no' && agent )
-    {
-      return this.get('agent.transitioning');
-    }
-    else
-    {
-      return host;
-    }
-  }.property('hostTransitioning','agent.transitioning'),
-
-  transitioningMessage: function() {
-    if ( this.get('hostTransitioning') === 'no' && this.get('agent.transitioning'))
-    {
-      return this.get('agent.transitioningMessage');
-    }
-    else
-    {
-      return this.get('hostTransitioningMessage');
-    }
-  }.property('hostTransitioningMessage','agent.{transitioning,transitioningMessage}'),
-
-  transitioningProgress: function() {
-    if ( this.get('hostTransitioning') === 'no' && this.get('agent.transitioning'))
-    {
-      return this.get('agent.transitioningProgress');
-    }
-    else
-    {
-      return this.get('hostTransitioningProgress');
-    }
-  }.property('hostTransitioningProgress','agent.{transitioning,transitioningProgress}'),
+  }.property('hostState','agentState'),
 });
 
 Host.reopenClass({
-  alwaysInclude: ['agent','instances','ipAddresses'],
+  alwaysInclude: ['instances','ipAddresses'],
 
-  // Remap the host fields to host+[Field] so that the regular names can be a computed combination of host + agent status.
+  // Remap the host state to hostState so the regular state can be a computed combination of host+agent state.
   mangleIn: function(data) {
-    var keys = ['state','transitioning','transitioningMessage','transitioningProgress'];
-    keys.forEach(function(key) {
-      var newKey = 'host' + key.substr(0,1).toUpperCase() + key.substr(1);
-      data[newKey] = data[key];
-      delete data[key];
-    });
-
+    data['hostState'] = data['state'];
+    delete data['state'];
     return data;
   },
 });
