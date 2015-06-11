9f98562021db86f3dcbc19e74220f4495128fe34
Vincent Fiduccia <vincent@rancher.com>
2015-06-11 16:55:54 -0700

    Hide project name in service container boxes (rancherio/rancher#1179)

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -76,6 +76,7 @@ export default {
   LABEL: {
     SYSTEM_PREFIX: 'io.rancher.',
     SERVICE_NAME: 'io.rancher.service.name',
+    PROJECT_NAME: 'io.rancher.project.name',
     SCHED_GLOBAL: 'io.rancher.scheduler.global',
     SCHED_AFFINITY: 'io.rancher.scheduler.affinity:',
     SCHED_CONTAINER: 'io.rancher.scheduler.affinity:container',
