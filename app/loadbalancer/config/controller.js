13b10c66faac52b792bebdbac359cada6e309d03
Vincent Fiduccia <vincent@rancher.com>
2015-06-11 20:01:25 -0700

    Fix lblistener display on balancer (rancherio/rancher#1135)

--- app/loadbalancer/config/controller.js
+++ app/loadbalancer/config/controller.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
+import UnremovedArrayProxy from 'ui/utils/unremoved-array-proxy';
 
 export default Ember.ObjectController.extend({
   actions: {
     editConfig: function() {
       this.transitionToRoute('loadbalancerconfig.edit', this.get('config.id'));
     }
-  }
+  },
+
+  unremovedListeners: function() {
+    return UnremovedArrayProxy.create({
+      sourceContent: this.get('listeners'),
+    });
+  }.property('listeners'),
 });
