d710faebb40bbbe23298db5c3efc59faecaa20e4
Vincent Fiduccia <vincent@rancher.com>
2015-06-13 00:43:59 -0700

    Fix newlines on compose config (rancherio/rancher#1208)

--- app/environment/code/route.js
+++ app/environment/code/route.js
@@ -4,6 +4,10 @@ export default Ember.Route.extend({
   model: function(/*params, transition*/) {
     var env = this.modelFor('environment');
     return env.doAction('exportconfig').then((config) => {
+      // Windows needs CRLFs
+      config.dockerComposeConfig = config.dockerComposeConfig.split(/\r?\n/).join('\r\n');
+      config.rancherComposeConfig = config.rancherComposeConfig.split(/\r?\n/).join('\r\n');
+
       return Ember.Object.create({
         environment: env,
         composeConfig: config
