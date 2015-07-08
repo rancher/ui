bf2297456715b2ea7a10d464b7a9c1e49523bb18
Vincent Fiduccia <vincent@rancher.com>
2015-07-08 12:25:55 -0700

    bugfix

--- app/utils/add-view-action.js
+++ app/utils/add-view-action.js
@@ -2,7 +2,15 @@ import Ember from 'ember';
 
 export function addAction(action, selector) {
   return function() {
-    this._super();
+    if ( Ember.Component.detectInstance(this) )
+    {
+      this._super();
+    }
+    else
+    {
+      this.get('controller').send(action);
+    }
+
     Ember.run.next(this, function() {
       this.$(selector).last().focus();
     });
