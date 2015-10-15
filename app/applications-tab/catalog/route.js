374419f5e5906b7d5f5859a981fb5b182cc53116
Vincent Fiduccia <vincent@rancher.com>
2015-10-15 12:12:53 -0700

    Cache catalog response

--- app/applications-tab/catalog/route.js
+++ app/applications-tab/catalog/route.js
@@ -1,25 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  cache: null,
   model: function(params) {
+    var cache = this.get('cache');
+    if ( cache )
+    {
+      return filter(cache, params.type);
+    }
+
     return Ember.$.ajax('/v1-catalog/templates', 'GET').then((response) => {
-      var data = response.data.sortBy('name');
-      var responseData  = Ember.Object.create({
-          categories: this.buildCategories(response.data)
-        });
-      if (params.type === 'all') {
-        responseData.set('catalog', data);
-      } else {
-        responseData.set('catalog', _.where(data, {category: params.type}));
-      }
-      return responseData;
-    }, function(/*error*/){});
-  },
-  buildCategories: function(categories) {
-    var catArr = ['all'];
-    _.each(categories, (catalogItem) => {
-      catArr.push(catalogItem.category);
+      this.set('cache', response.data);
+      return filter(response.data, params.type);
     });
-    return _.sortBy(catArr, function(i){ return i.toLowerCase();});
-  }
+
+    function filter(data, type) {
+      data = data.sortBy('name');
+      var out = Ember.Object.create({
+        categories: categories(data),
+
+      });
+
+      if ( type === 'all' ) {
+        out.set('catalog', data);
+      } else {
+        out.set('catalog', data.filterBy('category', type));
+      }
+
+      return out;
+    }
+
+    function categories(data) {
+      var out = data.map((item) => item.category);
+      out.uniq().sort((a,b) => a.localeCompare(b, 'en', {sensitivity: 'base'}));
+      out.unshift('all');
+      return out;
+    }
+  },
 });
