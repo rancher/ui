7952deef5346a0d21c17bbac80771d1da023170a
Vincent Fiduccia <vincent@rancher.com>
2015-10-30 16:42:07 -0700

    Fix orphaned sidekicks on service screen

--- app/components/service-pod/component.js
+++ app/components/service-pod/component.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 import ReadLabels from 'ui/mixins/read-labels';
 import FasterLinksAndMenus from 'ui/mixins/faster-links-and-menus';
+import GroupedInstances from 'ui/mixins/grouped-instances';
 import C from 'ui/utils/constants';
 
-export default Ember.Component.extend(ReadLabels, FasterLinksAndMenus, {
+export default Ember.Component.extend(ReadLabels, FasterLinksAndMenus, GroupedInstances, {
   model: null,
   mode: null,
 
@@ -25,65 +26,6 @@ export default Ember.Component.extend(ReadLabels, FasterLinksAndMenus, {
 
   arrangedInstances: function() {
     return (this.get('model.instances')||[]).sortBy('name','id');
-  }.property('model.instances.@each.{name,id}'),
-
-  groupedInstances: function() {
-    var groups = [];
-
-    function getOrCreateGroup(groupName)
-    {
-      var group = groups.filterBy('name', groupName)[0];
-      if ( !group )
-      {
-        group = { name: groupName, instances: [], hasChildren: false };
-        groups.push(group);
-      }
-
-      return group;
-    }
-
-    function getOrCreateUnit(groupName, deploymentUnit)
-    {
-      var group = getOrCreateGroup(groupName);
-      var entry = group.instances.filterBy('unit', deploymentUnit)[0];
-      if ( !entry )
-      {
-        entry = {unit: deploymentUnit, main: null, children: []};
-        group.instances.push(entry);
-      }
-
-      return [group, entry];
-    }
-
-
-    (this.get('model.instances')||[]).forEach((instance) => {
-      var labels = instance.get('labels')||{};
-      var deploymentUnit = labels[C.LABEL.DEPLOYMENT_UNIT];
-      var isSidekick = deploymentUnit && labels[C.LABEL.LAUNCH_CONFIG] !== C.LABEL.LAUNCH_CONFIG_PRIMARY;
-      var groupName = deploymentUnit;
-
-      //console.log(deploymentUnit, groupName, isSidekick, instance.get('id'), instance.get('name'));
-
-      let [group, unit] = getOrCreateUnit(groupName, deploymentUnit);
-      if ( isSidekick )
-      {
-        unit.children.push(instance);
-        group.hasChildren = true;
-      }
-      else
-      {
-        unit.main = instance;
-      }
-    });
-
-    groups = groups.sortBy('name');
-    if ( groups[0] && groups[0].name === '' )
-    {
-      // Move no name/standalone containers to the end of the list instead of the beginning
-      groups.push(groups.shift());
-    }
-
-    return groups;
   }.property('model.instances.@each.{name,id}'),
 
   hasChildren: function() {
