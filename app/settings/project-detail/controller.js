3b070615f474cef440e6fbe5abbe3d611164e138
Vincent Fiduccia <vincent@rancher.com>
2015-11-11 15:38:29 -0700

    plus/minus buttons

--- app/settings/project-detail/controller.js
+++ app/settings/project-detail/controller.js
@@ -2,11 +2,20 @@ import Ember from 'ember';
 import Sortable from 'ui/mixins/sortable';
 
 export default Ember.Controller.extend(Sortable, {
-  sortableContent: Ember.computed.alias('model.projectMembers'),
+  application: Ember.inject.controller(),
+
+  project: Ember.computed.alias('model.project'),
+  sortableContent: Ember.computed.alias('project.projectMembers'),
   sortBy: 'name',
   sorts: {
     name:   ['externalId'],
     type:   ['externalIdType','externalId'],
     role:   ['role','externalId'],
+  },
+
+  actions: {
+    changeProject(project) {
+      this.get('application').transitionToRoute('settings.project-detail', project.get('id'));
+    },
   },
 });
