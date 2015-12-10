9e102710fd226e104b69417c7b64cf363dea08bc
Vincent Fiduccia <vincent@rancher.com>
2015-12-09 17:24:19 -0700

    Custom HAProxy config support

--- app/components/new-balancer/component.js
+++ app/components/new-balancer/component.js
@@ -8,6 +8,7 @@ export default Ember.Component.extend(NewOrEdit, SelectTab, {
   service: null,
   existing: null,
   balancerConfig: null,
+  haproxyConfig: null,
   allHosts: null,
   allServices: null,
   allCertificates: null,
--- app/models/loadbalancerconfig.js
+++ app/models/loadbalancerconfig.js
@@ -3,14 +3,13 @@ import Resource from 'ember-api-store/models/resource';
 
 var LoadBalancerConfig = Resource.extend({
   type: 'loadBalancerConfig',
-  listeners: Ember.computed.alias('loadBalancerListeners'),
   config: function() {
     return this;
   }.property()
 });
 
 LoadBalancerConfig.reopenClass({
-  alwaysInclude: ['loadBalancerListeners','loadBalancers'],
+  alwaysInclude: ['loadBalancers'],
 });
 
 export default LoadBalancerConfig;
