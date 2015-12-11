381ef30e71cdc4272ad848440afb84cfcc3af7d1
Westly Wright <westly.wright@gmail.com>
2015-12-11 13:36:11 -0700

    Fix error with setting catalog items

--- app/admin-tab/settings/route.js
+++ app/admin-tab/settings/route.js
@@ -1,16 +1,21 @@
 import Ember from 'ember';
 import C from 'ui/utils/constants';
+import {denormalizeName} from 'ui/services/settings';
 
 export default Ember.Route.extend({
   endpoint: Ember.inject.service(),
   settings: Ember.inject.service(),
 
   model: function() {
-    return this.get('store').findAll('setting').then((/* response */) => {
+    return Ember.RSVP.all([
+      this.get('store').find('setting', denormalizeName(C.SETTING.API_HOST)),
+      this.get('store').find('setting', denormalizeName(C.SETTING.CATALOG_URL)),
+      this.get('store').find('setting', denormalizeName(C.SETTING.VM_ENABLED)),
+    ]).then((/* response */) => {
       return Ember.Object.create({
         host: this.get('settings').get(C.SETTING.API_HOST),
         catalog: this.get('settings').get(C.SETTING.CATALOG_URL),
-        vm: this.get('settings').get(C.SETTING.VM_ENABLED),
+        vm: this.get('settings').get(C.SETTING.VM_ENABLED) || false,
       });
     });
   },
