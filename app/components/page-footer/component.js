42026c327abfb5c74d90671ab981f2fbbccd030c
lvuch <laurenvuchetich@gmail.com>
2015-12-16 14:26:54 -0700

    adding a minimal footer that shows versions

---
+++ app/components/page-footer/component.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'footer',
+  className: 'clearfix',
+
+  settings: Ember.inject.service(),
+  
+  actions: {
+    showAbout() {
+      this.sendAction('showAbout');
+    },
+  }
+});
+
+
