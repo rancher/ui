4b100c091237dad12ec5ca1049503c229031d6ea
Westly Wright <westly.wright@gmail.com>
2015-12-18 16:44:31 -0700

    Fix a bunch of issues found during QA

--- app/components/modal-auditlog-info/component.js
+++ app/components/modal-auditlog-info/component.js
@@ -7,8 +7,8 @@ export default Ember.Component.extend({
   responseJSON: null,
 
   didInitAttrs: function() {
-    this.set('requestJSON', JSON.stringify(this.get('requestObject'), null,2));
-    this.set('responseJSON', JSON.stringify(this.get('responseObject'), null,2));
+    this.set('requestJSON', JSON.stringify(JSON.parse(this.get('requestObject')),null,2));
+    this.set('responseJSON', JSON.stringify(JSON.parse(this.get('responseObject')),null,2));
     Ember.run.next(() =>{
       this.highlightAll();
     });
