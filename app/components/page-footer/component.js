bbb83abc8c464b03caa46ef75d698bcb8c8ebfbd
Vincent Fiduccia <vincent@rancher.com>
2016-01-28 13:41:58 -0700

    Add debug info to file issue button (rancher/rancher#3444)

--- app/components/page-footer/component.js
+++ app/components/page-footer/component.js
@@ -7,6 +7,27 @@ export default Ember.Component.extend({
   className: 'clearfix',
 
   settings: Ember.inject.service(),
+  access: Ember.inject.service(),
+
+  issueBody: function() {
+    var str = '*Describe your issue here*\n\n\n---\n| Useful | Info |\n| :-- | :-- |\n' +
+      `|Versions|Rancher \`${this.get('settings.rancherVersion')||'-'}\` ` +
+        `Cattle: \`${this.get('settings.cattleVersion')||'-'}\` ` +
+        `UI: \`${this.get('settings.uiVersion')||'--'}\` |\n`;
+
+      if ( this.get('access.enabled') )
+      {
+        str += `|Access|\`${this.get('access.provider').replace(/config/,'')}\`, \`${this.get('access.admin') ? 'admin' : ''}\`|\n`;
+      }
+      else
+      {
+        str += '|Access|`Disabled`}|\n';
+      }
+
+      str += `|Route|\`${this.get('application.currentRouteName')}\`|\n`;
+
+    return encodeURIComponent(str);
+  }.property(),
 
   actions: {
     showAbout() {
