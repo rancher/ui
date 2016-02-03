792c94b98748178e87714840d5c40b934813a7f1
Westly Wright <westly.wright@gmail.com>
2016-02-03 14:21:00 -0700

    Add custom Azure Host page

--- app/hosts/new/controller.js
+++ app/hosts/new/controller.js
@@ -4,6 +4,7 @@ var defaultDriver = 'custom';
 var driverChoices = [
   {name: 'custom',       label: 'Custom',        css: 'custom',       sort: 1,                              },
   {name: 'amazonec2',    label: 'Amazon EC2',    css: 'amazon',       sort: 2, schema: 'amazonec2config'    },
+  {name: 'azure',        label: 'Azure',         css: 'azure',        sort: 2, schema: 'azureConfig'        },
   {name: 'digitalocean', label: 'DigitalOcean',  css: 'digitalocean', sort: 2, schema: 'digitaloceanconfig' },
   {name: 'exoscale',     label: 'Exoscale',      css: 'exoscale',     sort: 2, schema: 'exoscaleconfig'     },
   {name: 'packet',       label: 'Packet',        css: 'packet',       sort: 2, schema: 'packetconfig'       },
