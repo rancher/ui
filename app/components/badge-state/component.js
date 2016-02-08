eb66448af8ceec2d4e60a86c22e284da38f06d6d
lvuch <laurenvuchetich@gmail.com>
2016-02-08 10:05:04 -0700

    fix for #3552

--- app/components/badge-state/component.js
+++ app/components/badge-state/component.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'SPAN',
-  classNames: ['badge','state'],
-  classNameBindings: ['model.stateBackground'],
+  classNames: ['state',],
+  classNameBindings: ['model.stateColor'],
 });
