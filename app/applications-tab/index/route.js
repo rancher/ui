5b9e1e848baa6932ec1600a7e92c769ec888c61c
Vincent Fiduccia <vincent@rancher.com>
2016-02-24 14:29:26 -0700

    Not kubernetes services on system

--- app/applications-tab/index/route.js
+++ app/applications-tab/index/route.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
-import C from 'ui/utils/constants';
 
 export default Ember.Route.extend({
   redirect: function() {
-    if ( window.lc('authenticated').get('hasKubernetes') )
-    {
-      this.transitionTo('environments', {queryParams: {which: C.EXTERNALID.KIND_NOT_KUBERNETES}});
-    }
-    else
-    {
-      this.transitionTo('environments');
-    }
+    this.transitionTo('environments');
   }
 });
