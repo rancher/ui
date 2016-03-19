bb4a5e54f0f547bd5d4eecf7927cda73fd676ab1
Vincent Fiduccia <vincent@rancher.com>
2016-03-19 14:28:29 -0700

    Add switch namespace action (rancher/rancher#3790)

--- app/models/k8s-namespace.js
+++ app/models/k8s-namespace.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import K8sResource from 'ui/models/k8s-resource';
 
 var Namespace = K8sResource.extend({
+  k8s: Ember.inject.service(),
+
   isSystem: Ember.computed.equal('id','kube-system'),
 
   icon: function() {
@@ -14,6 +16,24 @@ var Namespace = K8sResource.extend({
       return 'icon icon-folder';
     }
   }.property('isSystem'),
+
+  actions: {
+    switchTo: function() {
+      // @TODO bad
+      window.lc('authenticated').send('switchNamespace', this.get('id'));
+    },
+  },
+
+  availableActions: function() {
+    var choices = this._super();
+    choices.unshift({ divider: true });
+    choices.unshift({label: 'Switch to this Namespace', icon: 'icon icon-folder-open',  action: 'switchTo', enabled: this.get('canSwitchTo')});
+    return choices;
+  }.property('canSwitchTo'),
+
+  canSwitchTo: function() {
+    return this.get('id') !== this.get('k8s.namespace.id');
+  }.property('id','k8s.namespace.id'),
 });
 
 export default Namespace;
