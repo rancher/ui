f4b34f6aadf45ea909debb85f0dfd5f6975d285b
fyoudine-digitaslbi <fyoudine-digitaslbi@users.noreply.github.com>
2016-04-15 03:04:29 +0200

    create contextual help button linking to documentation (#584)
    Create contextual help button linking to documentation

---
+++ app/components/help-btn/component.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import C from 'ui/utils/constants';
+
+export default Ember.Component.extend({
+  settings: Ember.inject.service(),
+
+  title: 'Go to documentation',
+  link: '',
+  target: '_blank',
+
+  tagName: 'A',
+  attributeBindings: ['title','href', 'target'],
+  classNames: ['small'],
+  classNameBindings: ['settings.isPrivateLabel:hide'],
+
+  href: function() {
+    return C.EXT_REFERENCES.DOCS + this.get('link');
+  }.property('link')
+
+});
