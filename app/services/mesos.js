f0f9af50d536b7e46960d9cc7d5a5bdabb350da7
James Oliver <joliver@rancher.com>
2016-04-29 11:20:20 -0700

    Remove unused code

--- app/services/mesos.js
+++ app/services/mesos.js
@@ -94,36 +94,6 @@ export default Ember.Service.extend({
     return promise;
   },
 
-
-  light_request: function(method, path) {
-    var store = this.get('store');
-    var opt = {};
-    opt.url = path;
-    opt.method = method;
-    var promise = new Ember.RSVP.Promise(function(resolve,reject) {
-      store.rawRequest(opt).then(success, fail);
-
-      function success(obj) {
-        var xhr = obj.xhr;
-        var response = {status: xhr.status};
-        if (xhr.status === 200)
-        {
-          resolve(response);
-        }
-        else {
-          reject(ApiError.create(response));
-        }
-      }
-
-      function fail() {
-        var response = ApiError.create({status: 404});
-        reject(response);
-      }
-
-    }, 'Request: '+ opt.url);
-    return promise; 
-  },
-
   isReady: function() {
     var projectId = this.get(`tab-session.${C.TABSESSION.PROJECT}`);
     return this.request({
