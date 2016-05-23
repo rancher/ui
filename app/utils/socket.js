f9c8b7751b6b4e35a8530234ffa1bc192ee2723f
Vincent Fiduccia <vincent@rancher.com>
2016-05-22 21:44:45 -0700

    Cherry-pick #680

--- app/utils/socket.js
+++ app/utils/socket.js
@@ -1,27 +1,42 @@
 import Ember from "ember";
 import { isSafari } from 'ui/utils/platform';
+import Util from 'ui/utils/util';
 
 var INSECURE = 'ws://';
 var SECURE   = 'wss://';
 var sockId = 1;
+var safariWarningShown = false;
+
+const DISCONNECTED = 'disconnected';
+const CONNECTING = 'connecting';
+const CONNECTED = 'connected';
+const CLOSING = 'closing';
+const RECONNECTING = 'reconnecting';
 
 export default Ember.Object.extend(Ember.Evented, {
   growl: Ember.inject.service(),
 
   url: null,
-  socket: null,
   autoReconnect: true,
-  connected: false,
-  tries: 0,
-  disconnectedAt: null,
-  disconnectCb: null,
+  frameTimeout: 11000,
+  metadata: null,
 
-  connect: function() {
-    var socket = this.get('socket');
-    if ( socket )
-    {
-      this.disconnect();
+  _socket: null,
+  _state: DISCONNECTED,
+  _framesReceived: 0,
+  _frameTimer: null,
+  _reconnectTimer: null,
+  _tries: 0,
+  _disconnectedAt: null,
+  _closingId: null,
+
+  connect(metadata) {
+    if ( this.get('_socket') ) {
+      console.log('Socket refusing to connect while another socket exists');
+      return;
     }
+
+    this.set('metadata', metadata||this.get('metadata')||{});
 
     var url = this.get('url');
     // If the site is SSL, the WebSocket should be too...
@@ -32,59 +47,84 @@ export default Ember.Object.extend(Ember.Evented, {
     }
 
     var id = sockId++;
-    console.log('Socket connect',id,'to', url.replace(/\?.*/,'')+'...');
+    console.log(`Socket connecting (id=${id}, url=${url.replace(/\?.*/,'')+'...'})`);
 
-    socket = new WebSocket(url);
-    socket.__sockId = id;
-    this.set('socket', socket);
-
+    var socket = new WebSocket(Util.addQueryParam(url,'sockId',id));
+    socket.__sockId  = id;
+    socket.metadata  = this.get('metadata');
     socket.onmessage = Ember.run.bind(this, this._message);
-    socket.onopen = Ember.run.bind(this, this._opened);
-    socket.onerror = Ember.run.bind(this, this._error);
-    socket.onclose = Ember.run.bind(this, this._closed);
-  },
-
-  disconnect: function(cb) {
-    this._log('disconnect');
-
-    if ( !this.get('connected') )
-    {
-      if ( cb )
-      {
-        cb();
-      }
-      return;
-    }
+    socket.onopen    = Ember.run.bind(this, this._opened);
+    socket.onerror   = Ember.run.bind(this, this._error);
+    socket.onclose   = Ember.run.bind(this, this._closed);
 
     this.setProperties({
-      'autoReconnect': false,
-      'disconnectCb': cb
+      _socket: socket,
+      _state: CONNECTING,
     });
+  },
 
-    var socket = this.get('socket');
+  disconnect() {
+    this.set('autoReconnect', false);
+    this._close();
+  },
+
+  reconnect(metadata) {
+    this.set('metadata', metadata||{});
+    if ( this.get('_socket') )
+    {
+      this._close();
+    } else {
+      this.connect(metadata);
+    }
+  },
+
+  getMetadata() {
+    let socket = this.get('_socket');
+    if ( socket ) {
+      return socket.metadata;
+    } else {
+      return {};
+    }
+  },
+
+  getId() {
+    let socket = this.get('_socket');
+    if ( socket ) {
+      return socket.__sockId;
+    } else {
+      return null;
+    }
+  },
+
+  _close() {
+    var socket = this.get('_socket');
     if ( socket )
     {
       try {
         this._log('closing');
+        this.set('_closingId', socket.__sockId);
         socket.onopen = null;
         socket.onerror = null;
         socket.onmessage = null;
         socket.close();
-        this.set('socket',null);
       }
       catch (e)
       {
-        this._log('exception', e);
+        this._log('Socket exception', e);
         // Meh..
       }
+
+      this.setProperties({
+        _state: CLOSING,
+      });
     }
   },
 
-  _opened: function() {
+  _opened() {
     this._log('opened');
     var now = (new Date()).getTime();
 
-    var at = this.get('disconnectedAt');
+    var at = this.get('_disconnectedAt');
     var after = null;
     if ( at )
     {
@@ -92,71 +132,91 @@ export default Ember.Object.extend(Ember.Evented, {
     }
 
     this.setProperties({
-      connected: true,
-      disconnectedAt: null,
+      _state: CONNECTED,
+      _framesReceived: 0,
+      _disconnectedAt: null,
     });
 
-    this.trigger('connected', this.get('tries'), after);
-
-    // Don't reset tries for a little bit, in case the socket immediately closes again.
-    // This prevents open/imediate close loops from hammering the server because the tries count is never incrementing.
-    Ember.run.later(this, '_resetTries', 1000);
+    this.trigger('connected', this.get('_tries'), after);
+    this._resetWatchdog();
   },
 
-  _resetTries: function() {
-    if ( this.get('connected') ) {
-      this.set('tries', 0);
-    }
-  },
-
-  _message: function(event) {
+  _message(event) {
+    this._resetWatchdog();
+    this.set('_tries', 0);
+    this.incrementProperty('_framesReceived');
     this.trigger('message',event);
   },
 
-  _error: function() {
-    this._log('error');
-    this.disconnect();
+  _resetWatchdog() {
+    if ( this.get('_frameTimer') )
+    {
+      Ember.run.cancel(this.get('_frameTimer'));
+    }
+
+    let timeout = this.get('frameTimeout');
+    if ( timeout && this.get('_state') === CONNECTED)
+    {
+      this.set('_frameTimer', Ember.run.later(this, function() {
+        this._log('Socket watchdog expired after', timeout, 'closing');
+        this._close();
+      }, timeout));
+    }
   },
 
-  _closed: function() {
-    console.log('Socket closed');
+  _error() {
+    this.set('_closingId', this.get('_socket.__sockId'));
+    this._log('error');
+  },
 
-    var wasConnected = this.get('connected');
-    this.set('connected',false);
-    this.incrementProperty('tries');
+  _closed() {
+    console.log('Socket', this.get('_closingId'), 'closed');
 
-    if ( this.get('disconnectedAt') === null )
-    {
-      this.set('disconnectedAt', (new Date()).getTime());
-    }
-
-    if ( wasConnected && this.get('autoReconnect') )
-    {
-      var delay = Math.max(1000, Math.min(1000 * this.get('tries'), 30000));
-      Ember.run.later(this, this.connect, delay);
-    }
-    else if ( isSafari && !wasConnected && this.get('url').indexOf('wss://') === 0 )
-    {
-      window.l('service:growl').error('Error connecting to WebSocket','Safari does not support WebSockets connecting to a self-signed or unrecognized certificate.  Use http:// instead of https:// or reconfigure the server with a valid certificate from a recognized certificate authority.  Streaming stats, logs, shell/console, and auto-updating of the state of resources in the UI will not work until this is resolved.');
-    }
-
-    if ( typeof this.get('disconnectCb') === 'function' )
-    {
-      this.get('disconnectCb')();
-    }
-
-    if ( wasConnected )
+    let wasConnected = false;
+    if ( [CONNECTED, CLOSING].indexOf(this.get('_state')) >= 0 )
     {
       this.trigger('disconnected');
+      wasConnected = true;
+    }
+
+    this.set('_closingId', null);
+    this.set('_socket', null);
+    Ember.run.cancel(this.get('_reconnectTimer'));
+    Ember.run.cancel(this.get('_frameTimer'));
+
+    if ( this.get('_disconnectedAt') === null )
+    {
+      this.set('_disconnectedAt', (new Date()).getTime());
+    }
+
+    if ( isSafari && !safariWarningShown && !wasConnected && this.get('url').indexOf('wss://') === 0 )
+    {
+      this.set('autoReconnect', false);
+      this.set('_state', DISCONNECTED);
+      safariWarningShown = true;
+      this.get('growl').error('Error connecting to WebSocket','Safari does not support WebSockets connecting to a self-signed or unrecognized certificate.  Use http:// instead of https:// or reconfigure the server with a valid certificate from a recognized certificate authority.  Streaming stats, logs, shell/console, and auto-updating of the state of resources in the UI will not work until this is resolved.');
+    }
+    else if ( this.get('autoReconnect') )
+    {
+      this.set('_state', RECONNECTING);
+      this.incrementProperty('_tries');
+      let delay = Math.max(1000, Math.min(1000 * this.get('_tries'), 30000));
+      this.set('_reconnectTimer', Ember.run.later(this, this.connect, delay));
+    }
+    else
+    {
+      this.set('_state', DISCONNECTED);
     }
   },
 
-  _log: function(/*arguments*/) {
-    var args = ['Socket', this.get('socket.__sockId') ];
+  _log(/*arguments*/) {
+    var args = ['Socket'];
     for ( var i = 0 ; i < arguments.length ; i++ )
     {
       args.push(arguments[i]);
     }
+
+    args.push(`(state=${this.get('_state')}, id=${this.get('_socket.__sockId')})`);
 
     console.log.apply(console, args);
   },
