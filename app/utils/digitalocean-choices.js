0323da9539316c9c4c9759325a1ebd78d34f48ff
Vishal <vishal-biyani@users.noreply.github.com>
2016-06-03 01:38:20 +0530

    Added BLR region (#698)
    * Added BLR region
    
    Digital Ocean added a new region BLR today and it is not visible in Rancher UI

--- app/utils/digitalocean-choices.js
+++ app/utils/digitalocean-choices.js
@@ -10,7 +10,8 @@ export let Regions = {
     {"name":"New York 3","slug":"nyc3","sizes":["32gb","16gb","2gb","1gb","4gb","8gb","512mb","64gb","48gb"],"features":["private_networking","backups","ipv6","metadata"],"available":true},
     {"name":"Amsterdam 3","slug":"ams3","sizes":["32gb","16gb","2gb","1gb","4gb","8gb","512mb","64gb","48gb"],"features":["private_networking","backups","ipv6","metadata"],"available":true},
     {"name":"Frankfurt 1","slug":"fra1","sizes":["32gb","16gb","2gb","1gb","4gb","8gb","512mb","64gb","48gb"],"features":["private_networking","backups","ipv6","metadata"],"available":true},
-    {"name":"Toronto 1","slug":"tor1","sizes":["32gb","16gb","2gb","1gb","4gb","8gb","512mb","64gb","48gb"],"features":["private_networking","backups","ipv6","metadata"],"available":true}
+    {"name":"Toronto 1","slug":"tor1","sizes":["32gb","16gb","2gb","1gb","4gb","8gb","512mb","64gb","48gb"],"features":["private_networking","backups","ipv6","metadata"],"available":true},
+    {"name":"Bangalore 1","slug":"blr1","sizes":["32gb","16gb","2gb","1gb","4gb","8gb","512mb","64gb","48gb"],"features":["private_networking","backups","ipv6","metadata"],"available":true}
   ],
   "links":{},
   "meta":{"total":10}
