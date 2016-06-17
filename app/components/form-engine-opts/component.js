bcdaa7b8c47937dd0cef22e5b40bc31472a9493a
Vincent Fiduccia <vincent@rancher.com>
2016-06-16 18:37:07 -0700

    Engine install URL options (rancher/rancher#5170)

--- app/components/form-engine-opts/component.js
+++ app/components/form-engine-opts/component.js
@@ -4,6 +4,7 @@ import C from 'ui/utils/constants';
 export default Ember.Component.extend({
   machine: null,
   settings: Ember.inject.service(),
+  intl: Ember.inject.service(),
   showEngineUrl: null,
 
   didReceiveAttrs() {
@@ -12,4 +13,22 @@ export default Ember.Component.extend({
       this.set('machine.engineInstallUrl', this.get(`settings.${C.SETTING.ENGINE_URL}`) || '');
     }
   },
+
+  engineUrlChoices: function() {
+    let def = this.get(`settings.${C.SETTING.ENGINE_URL}`);
+    let out = [
+      {label: this.get('intl').t('formEngineOpts.engineInstallUrl.recommended'), value: def},
+      {label: 'v1.10.x', value: 'https://releases.rancher.com/install-docker/1.10.sh'},
+      {label: 'v1.11.x', value: 'https://releases.rancher.com/install-docker/1.11.sh'},
+      {label: this.get('intl').t('formEngineOpts.engineInstallUrl.latest'), value: 'https://get.docker.com'},
+    ];
+
+    return out;
+  }.property('intl._locale',`settings.${C.SETTING.ENGINE_URL}`),
+
+  actions: {
+    setEngine(url) {
+      this.set('machine.engineInstallUrl', url);
+    }
+  }
 });
