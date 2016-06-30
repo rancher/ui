c32ad6d0d1ee0fdf3c920d95ed3a74dcf74145b5
Vincent Fiduccia <vincent@rancher.com>
2016-06-29 17:14:17 -0700

    Variable width viewport (rancher/rancher#5007)

---
+++ app/initializers/viewport.js
@@ -0,0 +1,10 @@
+export function initialize() {
+  if ( screen && (screen.width <= 570) ) {
+    document.getElementById('viewport').setAttribute('content','width=570');
+  }
+}
+
+export default {
+  name: 'viewport',
+  initialize: initialize
+};
