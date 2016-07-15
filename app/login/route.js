e4caf54d7962944951111419da05e0cfef22ef6d
Westly Wright <westly.wright@gmail.com>
2016-07-15 16:03:23 -0700

    Correctly remove the farm class on route deactivate
    rancher/rancher#5449

--- app/login/route.js
+++ app/login/route.js
@@ -24,4 +24,12 @@ export default Ember.Route.extend({
       }
     });
   },
+
+  activate: function() {
+    $('BODY').addClass('farm');
+  },
+
+  deactivate: function() {
+    $('BODY').removeClass('farm');
+  }
 });
