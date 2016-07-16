a6cb437c70fac1e18fe6f4770e477d87cc440700
Vincent Fiduccia <vincent@rancher.com>
2016-07-15 17:12:58 -0700

    Add coookie to set default timeout (#785)
    Add cookie to set default timeout

--- app/instance-initializers/store.js
+++ app/instance-initializers/store.js
@@ -3,6 +3,7 @@ import StoreTweaks from 'ui/mixins/store-tweaks';
 export function initialize(instance) {
   var application = instance.lookup('application:main');
   var store = instance.lookup('store:main');
+  var cookies = instance.lookup('service:cookies');
 
   store.reopen(StoreTweaks);
   store.reopen({
@@ -10,6 +11,11 @@ export function initialize(instance) {
     baseUrl: application.apiEndpoint,
     skipTypeifyKeys: ['labels'],
   });
+
+  let timeout = cookies.get('timeout');
+  if ( timeout ) {
+    store.defaultTimeout = timeout;
+  }
 }
 
 export default {
