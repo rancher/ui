08fe90e3431bd0e9c5276ffc3bbafcdac69a64ac
lvuch <laurenvuchetich@gmail.com>
2016-09-09 16:57:45 -0700

    welcome wip

--- app/components/modal-welcome/component.js
+++ app/components/modal-welcome/component.js
@@ -4,6 +4,8 @@ import C from 'ui/utils/constants';
 export default Ember.Component.extend({
   settings: Ember.inject.service(),
 
+  classNames      : ['modal-welcome'],
+
   optIn: null,
 
   didInitAttrs() {
