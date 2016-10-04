a82e3458f4abd6dedc6debe1e824e30f8667fcc1
Vincent Fiduccia <vincent@rancher.com>
2016-10-03 20:51:10 -0700

    bugs

--- app/components/upgrade-btn/component.js
+++ app/components/upgrade-btn/component.js
@@ -26,7 +26,7 @@ function getUpgradeInfo(task, cb) {
   var obj = task.obj;
 
   obj.get('userStore').request({url: task.url}).then((upgradeInfo) => {
-    if ( !this.isDestroyed )
+    if ( !obj.isDestroyed )
     {
       upgradeInfo.id = task.id;
       obj.set('upgradeInfo', upgradeInfo);
