76c1f4998827fbf9f1823d1d36b82691c4f6c6e2
Vincent Fiduccia <vincent@rancher.com>
2017-01-16 14:08:48 -0700

    Process list tweaks

--- app/admin-tab/processes/list/controller.js
+++ app/admin-tab/processes/list/controller.js
@@ -8,50 +8,78 @@ export default Ember.Controller.extend({
   sortBy: 'id',
   descending: false,
 
-  headers: [
-    {
-      displayName: 'ID',
-      name: 'id',
-      sort: ['id:desc'],
-      width: '75px',
-    },
-    {
-      displayName: 'Name',
-      name: 'processName',
-      sort: ['processName','id:desc'],
-    },
-    {
-      displayName: 'Resource',
-      name: 'resource',
-      sort: ['resourceType','resourceId','id:desc'],
-      searchField: ['resourceType','resourceId'],
-    },
-    {
-      displayName: 'Exit Reason',
-      name: 'exitReason',
-      sort: ['exitReason','id'],
-      width: '150px',
-    },
-    {
+  actions: {
+    replay(process) {
+      if ( process.hasAction('replay') ) {
+        process.doAction('replay');
+      }
+    }
+  },
+
+  headers: function() {
+    let which = this.get('which');
+    let out = [
+      {
+        displayName: 'ID',
+        name: 'id',
+        sort: ['id:desc'],
+        width: '75px',
+      },
+      {
+        displayName: 'Name',
+        name: 'processName',
+        sort: ['processName','id:desc'],
+      },
+      {
+        displayName: 'Resource',
+        name: 'resource',
+        sort: ['typeAndId','id:desc'],
+        searchField: ['typeAndId', 'resourceType','resourceId'],
+      }
+    ];
+
+    if ( which === 'completed' ) {
+      out.push({
+        displayName: 'Exit Reason',
+        name: 'exitReason',
+        sort: ['exitReason','id'],
+        width: '150px',
+      });
+    }
+
+    out.push({
       displayName: 'Start Time',
       name: 'startTime',
       sort: ['startTime','id:desc'],
-      width: '100px',
+      width: '150px',
       searchField: false,
-    },
-    {
-      displayName: 'End Time',
-      name: 'endTime',
-      sort: ['endTime:desc','id:desc'],
-      width: '100px',
-      searchField: false,
-    },
-    {
+    });
+
+    if ( which === 'completed' ) {
+      out.push({
+        displayName: 'End Time',
+        name: 'endTime',
+        sort: ['endTime:desc','id:desc'],
+        width: '150px',
+        searchField: false,
+      });
+    }
+
+    out.push({
       displayName: 'Run Time',
       name: 'runTime',
       sort: ['runTime:desc','id'],
       width: '100px',
       searchField: false,
-    },
-  ],
+    });
+
+    if ( which === 'delayed' ) {
+      out.push({
+        isActions: true,
+        width: '50px',
+      });
+    }
+
+    return out;
+  }.property('which'),
 });
