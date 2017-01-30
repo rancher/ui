8d153bb41ff76664de8eeb6a9f05bf0c49897a99
lvuch <laurenvuchetich@gmail.com>
2017-01-30 14:31:32 -0700

    lacsso menu badge clip

--- app/components/badge-state/component.js
+++ app/components/badge-state/component.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'SPAN',
   classNames: ['state', 'badge'],
-  classNameBindings: ['model.stateColor'],
+  classNameBindings: ['model.stateBackground'],
 });
