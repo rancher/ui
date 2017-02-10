dce615ba9a917d7c820411a6bece7b92efa34af1
Vincent Fiduccia <vincent@rancher.com>
2017-02-10 10:41:34 -0700

    form-ports

--- app/components/container/form-scale/component.js
+++ app/components/container/form-scale/component.js
@@ -4,16 +4,22 @@ export default Ember.Component.extend({
   initialLabel:   null,
   initialScale:   null,
   editing:        false,
-
-  userInput:      null,
   min:            1,
   max:            100,
+  mode:           'container',
+
+  userInput:      null,
+  showAdvanced:   false,
+  sliderMax:      10,
 
   classNames: ['inline-form'],
 
   init() {
     this._super(...arguments);
     this.set('userInput', (this.get('initialScale')||1)+'');
+    if ( this.get('mode') !== 'container' ) {
+      this.set('showAdvanced', true);
+    }
   },
 
   actions: {
@@ -23,7 +29,11 @@ export default Ember.Component.extend({
 
     decrease() {
       this.set('userInput', Math.max(this.get('min'), this.get('asInteger')-1));
-    }
+    },
+
+    showAdvanced() {
+      this.set('showAdvanced', true);
+    },
   },
 
   asInteger: Ember.computed('userInput', function() {
@@ -31,6 +41,9 @@ export default Ember.Component.extend({
   }),
 
   scaleChanged: Ember.observer('asInteger', function() {
-    this.sendAction('setScale', this.get('asInteger'));
+    let cur = this.get('asInteger');
+    this.sendAction('setScale', cur);
+
+    this.set('sliderMax', Math.min(this.get('max'), Math.max(this.get('sliderMax'), Math.ceil(cur/10)*10)));
   }),
 });
