7ec8a623fce71421544162eda9b92ae692171778
lvuch <laurenvuchetich@gmail.com>
2017-02-13 12:06:24 -0700

    light and dark setup need to call these variables throughout app

--- app/components/spark-line/component.js
+++ app/components/spark-line/component.js
@@ -135,13 +135,13 @@ export default Ember.Component.extend({
         out = '#2ecc71'; //$green
         break;
       case 'memory':
-        out = '#00558b'; //$blueDark
+        out = '#00558b'; //$primary-dark
         break;
       case 'network':
-        out = '#d35401'; //$orangeDark
+        out = '#d35401'; //mix($warning, $error, 20%)Dark
         break;
       case 'storage':
-        out = '#3a6f81'; //$teal
+        out = '#3a6f81'; //$info
         break;
       default:
         break;
