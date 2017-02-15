6b9f15d0cbe713bdbadd52cd473236626875494a
lvuch <laurenvuchetich@gmail.com>
2017-02-15 09:59:37 -0700

    tooltip alert start merge of lacsso and rancher files

--- app/components/tooltip-warning/component.js
+++ app/components/tooltip-warning/component.js
@@ -4,7 +4,7 @@ import C from 'ui/utils/constants';
 
 export default Ember.Component.extend(Tooltip, {
   prefs: Ember.inject.service(),
-  classNames: ['tooltip-warning-container'],
+  // classNames: ['tooltip-warning-container'],
   actions: {
     hideAccessWarning: function() {
       this.set(`prefs.${C.PREFS.ACCESS_WARNING}`, false);
