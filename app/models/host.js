6c341a6c37549d3c52b9ca9359b0cd025537d019
Vincent Fiduccia <vincent@rancher.com>
2017-03-02 11:32:39 -0700

    Host evacuate prompt, update manager setting, docker ce

--- app/models/host.js
+++ app/models/host.js
@@ -25,6 +25,12 @@ var Host = Resource.extend({
       return this.doAction('deactivate');
     },
 
+    promptEvacuate: function() {
+      this.get('modalService').toggleModal('modal-host-evacuate', {
+        model: [this]
+      });
+    },
+
     evacuate: function() {
       return this.doAction('evacuate');
     },
@@ -61,7 +67,7 @@ var Host = Resource.extend({
     var out = [
       { label: 'action.activate',   icon: 'icon icon-play',         action: 'activate',     enabled: !!a.activate, bulkable: true},
       { label: 'action.deactivate', icon: 'icon icon-pause',        action: 'deactivate',   enabled: !!a.deactivate, bulkable: true},
-      { label: 'action.evacuate',   icon: 'icon icon-snapshot',     action: 'evacuate',     enabled: !!a.evacuate, bulkable: true},
+      { label: 'action.evacuate',   icon: 'icon icon-snapshot',     action: 'promptEvacuate',enabled: !!a.evacuate, altAction: 'evacuate', bulkable: true},
       { label: 'action.remove',     icon: 'icon icon-trash',        action: 'promptDelete', enabled: !!a.remove, altAction: 'delete', bulkable: true},
       { label: 'action.purge',      icon: '',                       action: 'purge',        enabled: !!a.purge},
       { divider: true },
@@ -112,6 +118,8 @@ var Host = Resource.extend({
 
   supportState: function() {
     let my = this.get('dockerEngineVersion');
+    my = my.replace('-ce','').replace('-ee','');
+
     let supported = this.get(`settings.${C.SETTING.SUPPORTED_DOCKER}`);
     let newest = this.get(`settings.${C.SETTING.NEWEST_DOCKER}`);
 
