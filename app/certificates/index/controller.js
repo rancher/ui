c5870aa065ccc3415174225679b47e93503ff6ea
Westly Wright <westly.wright@gmail.com>
2017-03-03 14:48:04 -0700

    Add sortable table to certs route

--- app/certificates/index/controller.js
+++ app/certificates/index/controller.js
@@ -10,4 +10,32 @@ export default Ember.Controller.extend(FilterState, Sortable, {
     cn:       ['CN','id'],
     expires:  ['expiresDate','id'],
   },
+  headers:     [
+    {
+      name:           'state',
+      sort:           ['stateSort','name','id'],
+      translationKey: 'certificatesPage.index.table.header.state',
+      width:          '125',
+    },
+    {
+      name:           'name',
+      sort:           ['name','id'],
+      translationKey: 'certificatesPage.index.table.header.name',
+    },
+    {
+      name:           'cn',
+      sort:           ['CN','id'],
+      translationKey: 'certificatesPage.index.table.header.domain',
+    },
+    {
+      name:           'expires',
+      sort:           ['expiresDate','id'],
+      translationKey: 'certificatesPage.index.table.header.expires',
+      width:          '120',
+    },
+    {
+      noSort:         true,
+      width:          '75',
+    },
+  ],
 });
