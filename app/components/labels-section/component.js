f206536f383ee432ec8e94f0a22d8af85f8ce459
Westly Wright <westly.wright@gmail.com>
2017-03-03 16:06:40 -0700

    Add sortable table to labels component

--- app/components/labels-section/component.js
+++ app/components/labels-section/component.js
@@ -1,21 +1,33 @@
 import Ember from 'ember';
 import ManageLabels from 'ui/mixins/manage-labels';
-import Sortable from 'ui/mixins/sortable';
 
-export default Ember.Component.extend(ManageLabels, Sortable, {
+export default Ember.Component.extend(ManageLabels, {
   model           : null,
 
   labelSource     : Ember.computed.alias('model.labels'),
-  sortableContent : Ember.computed.alias('labelArray'),
   sortBy          : 'kind',
   showKind        : true,
   descending      : true,
 
-  sorts: {
-    kind  : ['type','key'],
-    key   : ['key'],
-    value : ['value','key'],
-  },
+  headers:     [
+    {
+      name:           'kind',
+      sort:           ['type','key'],
+      translationKey: 'labelsSection.kind',
+      width:          '90',
+    },
+    {
+      name:           'key',
+      sort:           ['key'],
+      translationKey: 'labelsSection.key',
+      width:          '350',
+    },
+    {
+      name:           'value',
+      sort:           ['value','key'],
+      translationKey: 'labelsSection.value',
+    },
+  ],
 
   labelsObserver: Ember.observer('model.labels', function () {
     this.initLabels(this.get('labelSource'));
