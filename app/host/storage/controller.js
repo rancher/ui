217f1cb8cb682d614e87e6c26f5a4a2137eef25d
Westly Wright <westly.wright@gmail.com>
2017-03-05 15:26:50 -0700

    Add sortable table to registries

--- app/host/storage/controller.js
+++ app/host/storage/controller.js
@@ -1,19 +1,13 @@
 import Ember from 'ember';
-import Sortable from 'ui/mixins/sortable';
 
-export default Ember.Controller.extend(Sortable, {
+export default Ember.Controller.extend({
   nonRootVolumes: function() {
     return this.get('model').filter(function(volume) {
       return !volume.get('instanceId') && volume.get('state') !== 'purged';
     });
   }.property('model.@each.{instanceId,state}'),
 
-  sortableContent: Ember.computed.alias(''),
   sortBy: 'name',
-  sorts: {
-    state:    ['stateSort','displayUri','id'],
-    hostPath: ['displayUri','id'],
-  },
   headers:  [
     {
       name:           'state',
