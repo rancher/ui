66c0971b0c16091e8887e40e6ad0ce80bd65e582
Westly Wright <westly.wright@gmail.com>
2017-03-05 15:34:19 -0700

    Add sortable table to service containers

--- app/service/containers/controller.js
+++ app/service/containers/controller.js
@@ -1,17 +1,45 @@
 import Ember from 'ember';
-import Sortable from 'ui/mixins/sortable';
 import ContainerSparkStats from 'ui/mixins/container-spark-stats';
 
-export default Ember.Controller.extend(Sortable, ContainerSparkStats, {
+export default Ember.Controller.extend(ContainerSparkStats, {
   sparkInstances: Ember.computed.alias('model.instances'),
 
-  sortableContent: Ember.computed.alias('model.instances'),
   sortBy: 'name',
-  sorts: {
-    state:    ['stateSort','name','id'],
-    name:     ['name','id'],
-    ip:       ['sortIp','name','id'],
-    host:     ['primaryHost.displayName','name','id'],
-    image:    ['imageUuid','command','name','id'],
-  },
+  headers:  [
+    {
+      name:           'state',
+      sort:           ['stateSort','name','id'],
+      translationKey: 'generic.state',
+      width:          '125',
+    },
+    {
+      name:           'name',
+      sort:           ['name','id'],
+      translationKey: 'generic.name',
+    },
+    {
+      name:           'ip',
+      sort:           ['sortIp','name','id'],
+      translationKey: 'servicePage.containersTab.table.header.ipAddress',
+      width:          '110',
+    },
+    {
+      name:           'host',
+      sort:           ['primaryHost.displayName','name','id'],
+      translationKey: 'servicePage.containersTab.table.header.host',
+    },
+    {
+      name:           'image',
+      sort:           ['imageUuid','command','name','id'],
+      translationKey: 'servicePage.containersTab.table.header.image',
+    },
+    {
+      translationKey: 'servicePage.containersTab.table.header.stats',
+      noSort:         true,
+    },
+    {
+      isActions: true,
+      width: '75',
+    },
+  ],
 });
