8bb91bd6e1c70104896834f14ac4c2030bb4e56b
lvuch <laurenvuchetich@gmail.com>
2017-03-08 17:02:36 -0700

    less secret but still kind of secret

--- app/components/login-user-pass/component.js
+++ app/components/login-user-pass/component.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   access: Ember.inject.service(),
-
+  isCaas: Ember.computed('app.mode', function() {
+    return this.get('app.mode') === 'caas' ? true : false;
+  }),
   waiting: null,
 
   username: null,
