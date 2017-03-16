858eea5cd3cb37a8496dfc4dd859043881909e0e
Aaron <aaronwang@rancher.com>
2017-03-16 16:27:49 +0800

    Implement Azure environment choice

--- app/utils/azure-choices.js
+++ app/utils/azure-choices.js
@@ -1,94 +1,133 @@
-export let regions =
-[
-  {
-    "name": "eastasia",
-    "displayName": "East Asia",
-  },
-  {
-    "name": "southeastasia",
-    "displayName": "Southeast Asia",
-  },
-  {
-    "name": "centralus",
-    "displayName": "Central US",
-  },
-  {
-    "name": "eastus",
-    "displayName": "East US",
-  },
-  {
-    "name": "eastus2",
-    "displayName": "East US 2",
-  },
-  {
-    "name": "westus",
-    "displayName": "West US",
-  },
-  {
-    "name": "northcentralus",
-    "displayName": "North Central US",
-  },
-  {
-    "name": "southcentralus",
-    "displayName": "South Central US",
-  },
-  {
-    "name": "northeurope",
-    "displayName": "North Europe",
-  },
-  {
-    "name": "westeurope",
-    "displayName": "West Europe",
-  },
-  {
-    "name": "japanwest",
-    "displayName": "Japan West",
-  },
-  {
-    "name": "japaneast",
-    "displayName": "Japan East",
-  },
-  {
-    "name": "brazilsouth",
-    "displayName": "Brazil South",
-  },
-  {
-    "name": "australiaeast",
-    "displayName": "Australia East",
-  },
-  {
-    "name": "australiasoutheast",
-    "displayName": "Australia Southeast",
-  },
-  {
-    "name": "southindia",
-    "displayName": "South India",
-  },
-  {
-    "name": "centralindia",
-    "displayName": "Central India",
-  },
-  {
-    "name": "westindia",
-    "displayName": "West India",
-  },
-  {
-    "name": "canadacentral",
-    "displayName": "Canada Central",
-  },
-  {
-    "name": "canadaeast",
-    "displayName": "Canada East",
-  },
-  {
-    "name": "westcentralus",
-    "displayName": "West Central US",
-  },
-  {
-    "name": "westus2",
-    "displayName": "West US 2",
-  }
-];
+export let regions = {
+  'AzurePublicCloud': [
+    {
+      "name": "eastasia",
+      "displayName": "East Asia",
+    },
+    {
+      "name": "southeastasia",
+      "displayName": "Southeast Asia",
+    },
+    {
+      "name": "centralus",
+      "displayName": "Central US",
+    },
+    {
+      "name": "eastus",
+      "displayName": "East US",
+    },
+    {
+      "name": "eastus2",
+      "displayName": "East US 2",
+    },
+    {
+      "name": "westus",
+      "displayName": "West US",
+    },
+    {
+      "name": "northcentralus",
+      "displayName": "North Central US",
+    },
+    {
+      "name": "southcentralus",
+      "displayName": "South Central US",
+    },
+    {
+      "name": "northeurope",
+      "displayName": "North Europe",
+    },
+    {
+      "name": "westeurope",
+      "displayName": "West Europe",
+    },
+    {
+      "name": "japanwest",
+      "displayName": "Japan West",
+    },
+    {
+      "name": "japaneast",
+      "displayName": "Japan East",
+    },
+    {
+      "name": "brazilsouth",
+      "displayName": "Brazil South",
+    },
+    {
+      "name": "australiaeast",
+      "displayName": "Australia East",
+    },
+    {
+      "name": "australiasoutheast",
+      "displayName": "Australia Southeast",
+    },
+    {
+      "name": "southindia",
+      "displayName": "South India",
+    },
+    {
+      "name": "centralindia",
+      "displayName": "Central India",
+    },
+    {
+      "name": "westindia",
+      "displayName": "West India",
+    },
+    {
+      "name": "canadacentral",
+      "displayName": "Canada Central",
+    },
+    {
+      "name": "canadaeast",
+      "displayName": "Canada East",
+    },
+    {
+      "name": "westcentralus",
+      "displayName": "West Central US",
+    },
+    {
+      "name": "westus2",
+      "displayName": "West US 2",
+    }
+  ].sortBy('name'),
+  'AzureGermanCloud': [
+    {
+      "name": "germanynortheast",
+      "displayName": "Germany Northeast",
+    },
+    {
+      "name": "germanycentral",
+      "displayName": "Germany Central",
+    }
+  ].sortBy('name'),
+  'AzureChinaCloud': [
+    {
+      "name": "chinanorth",
+      "displayName": "China North",
+    },
+    {
+      "name": "chinaeast",
+      "displayName": "China East",
+    }
+  ].sortBy('name'),
+  'AzureUSGovernment': [
+    {
+      "name": "usgovvirginia",
+      "displayName": "US Gov Virginia",
+    },
+    {
+      "name": "usgovlowa",
+      "displayName": "US Gov lowa",
+    },
+    {
+      "name": "usgovarizona",
+      "displayName": "US Gov Arizona",
+    },
+    {
+      "name": "usgovTexas",
+      "displayName": "US GovTexas",
+    }
+  ].sortBy('name')
+};
 
 export let sizes = [
   {
@@ -248,5 +287,20 @@ export let storageTypes = [
   {
     name: 'Premium LRS',
     value: 'Premium_LRS',
+  }
+];
+
+export let environments = [
+  {
+    value: 'AzurePublicCloud'
+  },
+  {
+    value: 'AzureGermanCloud'
+  },
+  {
+    value: 'AzureChinaCloud'
+  },
+  {
+    value: 'AzureUSGovernment'
   }
 ];
