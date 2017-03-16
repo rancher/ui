3750d97f623755b94ee97f6bf7877eb53be0cd2f
loganhz <logan@rancher.com>
2017-03-16 21:31:17 +0800

    make container shell larger

--- app/components/modal-shell/component.js
+++ app/components/modal-shell/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import ModalBase from 'lacsso/components/modal-base';
 
 export default ModalBase.extend({
-  classNames: ['lacsso', 'modal-container', 'large-modal', 'modal-shell'],
+  classNames: ['lacsso', 'modal-container', 'large-modal', 'fullscreen-modal', 'modal-shell'],
   originalModel: Ember.computed.alias('modalService.modalOpts.model'),
   init() {
     this._super(...arguments);
