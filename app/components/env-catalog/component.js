86e03da5a8673fc7397d5fbb8901fe0527406075
Westly Wright <westly.wright@gmail.com>
2017-03-20 16:27:54 -0700

    Add validation and global catalogs to env catalog component

--- app/components/env-catalog/component.js
+++ app/components/env-catalog/component.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
   project:     null,
   catalogs:    null,
   ary:         null,
+  global:      null,
   actions:     {
     add() {
       this.get('ary').pushObject(Ember.Object.create({name: '', branch: C.CATALOG.DEFAULT_BRANCH, url: '', toAdd: true}));
@@ -21,31 +22,70 @@ export default Ember.Component.extend({
       Ember.set(obj, 'toRemove', true);
     },
     save(cb) {
-      var newCatalogs = this.get('ary').filterBy('toAdd', true);
-      var catalogsToRemove = this.get('ary').filterBy('toRemove', true);
-      var all = [];
+      if (this.validate()) {
+        this.set('errors', []);
+        var newCatalogs = this.get('ary').filterBy('toAdd', true);
+        var catalogsToRemove = this.get('ary').filterBy('toRemove', true);
+        var all = [];
 
+        newCatalogs.forEach((cat) => {
+          all.push(this.addCatalogs(cat));
+        });
+
+        catalogsToRemove.forEach((cat) => {
+          all.push(this.removeCatalogs(cat));
+        });
+
+        Ember.RSVP.all(all).then(() => {
+          this.set('catalog.componentRequestingRefresh', true);
+          this.set('saving', false);
+          cb(true);
+          Ember.run.later(() => {
+            this.sendAction('cancel');
+          }, 500);
+        }).catch((err) => {
+          this.set('errors',err);
+          cb(false);
+          this.set('saving', false);
+        });
+      } else {
+        cb(false);
+        this.set('saving', false);
+      }
+    }
+  },
+  validate: function() {
+    var errors = [];
+    var globals = ['all', 'community', 'library']; // these should be removed when these terms are removed from the envid field
+    var newCatalogs = this.get('ary').filterBy('toAdd', true);
+
+    if (newCatalogs.length) {
       newCatalogs.forEach((cat) => {
-        all.push(this.addCatalogs(cat));
-      });
-
-      catalogsToRemove.forEach((cat) => {
-        all.push(this.removeCatalogs(cat));
-      });
-
-      Ember.RSVP.all(all).then(() => {
-        this.set('catalog.componentRequestingRefresh', true);
-        this.set('saving', false);
-        cb(true);
-        Ember.run.later(() => {
-          this.sendAction('cancel');
-        }, 500);
-      }).catch((err) => {
-        this.set('errors',err);
-        cb(true);
-        this.set('saving', false);
+        if ( (cat.name||'').trim().length === 0 )
+        {
+          errors.push('A name is required');
+        }
+        if ( (cat.url||'').trim().length === 0 )
+        {
+          errors.push('A url is required');
+        }
+        if (globals.indexOf(cat.name.toLowerCase()) >= 0) {
+          errors.push('Catalog name can not match a gloabl catalog name');
+        }
       });
     }
+
+    if ( errors.length )
+    {
+      this.set('errors',errors.uniq());
+      return false;
+    }
+    else
+    {
+      this.set('errors', null);
+    }
+
+    return true;
   },
   addCatalogs(catalogs) {
     return this.get('store').request({
@@ -71,6 +111,7 @@ export default Ember.Component.extend({
     this._super(...arguments);
     this.setProperties({
       ary: this.get('catalogs').filterBy('environmentId', this.get('project.id')),
+      global: this.get('catalogs').filterBy('environmentId', 'global') // this should change to falsey check when josh updates the catalog to remove 'global' from the id
     });
   }
 });
