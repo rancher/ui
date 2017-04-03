d951881157774db894185263c959ed8d82f3048b
Westly Wright <westly.wright@gmail.com>
2017-04-03 10:19:58 -0700

    fixes for new cloud hosts

--- app/components/cloud-host-add-or-edit/component.js
+++ app/components/cloud-host-add-or-edit/component.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   host: null,
+  config: null,
   hostTemplates: null,
   selectedHostTemplate: null,
-  providerClass: Ember.computed('host.provider', function() {
-    var provider = this.get('host.provider');
+  providerClass: Ember.computed('config.provider', function() {
+    var provider = this.get('config.provider');
 
     switch (provider) {
     case 'Amazon':
@@ -22,18 +23,23 @@ export default Ember.Component.extend({
     setLabels() {
     },
     saveTemp() {
-      if (this.get('selectedTemplateKey')) {
-        if (this.get('selectedTemplateKey.accountId')) {
+      debugger;
+      if (this.get('selectedHostTemplate')) {
+        if (this.get('selectedHostTemplate.accountId')) {
           // we have a hosttemplate so well send it with the driver
           Ember.run.later(() => {
             // TODO Actually save this model now that the template is saved
             this.sendAction('save');
           }, 1000);
         } else {
-          this.get('selectedTemplateKey').save().then((hstTemplate) => {
-            hstTemplate.waitForNotTransitioning(() => {
+          this.get('selectedHostTemplate').save().then((hstTemplate) => {
+            hstTemplate.waitForNotTransitioning.then(() => {
               Ember.run.later(() => {
                 // TODO Actually save this model now that the template is saved
+                var host = this.get('host');
+                host.setProperties({
+                  hostTemplateId: hstTemplate.id
+                });
                 this.sendAction('save');
               }, 1000);
             });
