78bcda806ab7a9639b04d81f52b40a5869a3e5e9
Westly Wright <westly.wright@gmail.com>
2017-04-03 12:49:04 -0700

    WIP

--- app/components/cloud-host-add-or-edit/component.js
+++ app/components/cloud-host-add-or-edit/component.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import Driver from 'ui/mixins/driver';
 
-export default Ember.Component.extend({
-  host: null,
+export default Ember.Component.extend(Driver, {
+  model: null,
   config: null,
   hostTemplates: null,
   selectedHostTemplate: null,
@@ -23,28 +24,52 @@ export default Ember.Component.extend({
     setLabels() {
     },
     saveTemp() {
-      debugger;
       if (this.get('selectedHostTemplate')) {
         if (this.get('selectedHostTemplate.accountId')) {
           // we have a hosttemplate so well send it with the driver
+          // debugger;
           Ember.run.later(() => {
             // TODO Actually save this model now that the template is saved
-            this.sendAction('save');
+            var model = this.get('model');
+            model.setProperties({
+              hostTemplateId: this.get('selectedHostTemplate.id'),
+              rancherConfig: {
+                flavor: this.get('config.id')
+              }
+            });
+            model.save().then((result) => {
+              // result;
+              // debugger;
+              this.sendAction('save');
+            }).catch((err) => {
+              console.log('err: ', err);
+            });
           }, 1000);
         } else {
           this.get('selectedHostTemplate').save().then((hstTemplate) => {
             hstTemplate.waitForNotTransitioning.then(() => {
               Ember.run.later(() => {
                 // TODO Actually save this model now that the template is saved
-                var host = this.get('host');
-                host.setProperties({
-                  hostTemplateId: hstTemplate.id
+                var model = this.get('model');
+                model.setProperties({
+                  hostTemplateId: this.get('selectedHostTemplate.id'),
+                  rancherConfig: {
+                    flavor: this.get('config.id')
+                  }
                 });
-                this.sendAction('save');
+                model.save().then((result) => {
+                  // result;
+                  // debugger;
+                  this.sendAction('save');
+                }).catch((err) => {
+                  console.log('err: ', err);
+                });
               }, 1000);
             });
           });
         }
+      } else {
+        // debugger;
       }
     },
   },
