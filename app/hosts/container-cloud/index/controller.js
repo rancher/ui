6227c5d81f5f76a93ffbae26c0c9d3198edbc07b
Westly Wright <westly.wright@gmail.com>
2017-04-07 15:15:38 -0700

    Remove ratings for now and move provider into a select sort on browse

--- app/hosts/container-cloud/index/controller.js
+++ app/hosts/container-cloud/index/controller.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 const DEFAULT_REALM = 'us-west';
 
+const PROVIDERS = [{id: 'All'}, {id: 'Amazon'}, {id: 'Digital Ocean' }, {id: 'Packet' }];
 export default Ember.Controller.extend({
   prefs        : Ember.inject.service(),
   queryParams: ['from'],
@@ -9,12 +10,15 @@ export default Ember.Controller.extend({
   initialTab:  'favorites',
   tab:         null,
   realmSort:   DEFAULT_REALM,
+  providers:   PROVIDERS,
   memSort:     null,
   storageSort: null,
   costSort:    null,
+  providerSort: 'All',
   sortBy:      'price',
   actions:     {
     selectMachine(id) {
+      this.set('providerSort', 'All');
       this.transitionToRoute('hosts.container-cloud.add', id);
     },
     favoriteChanged(id) {
@@ -67,18 +71,18 @@ export default Ember.Controller.extend({
       translationKey: 'hostsPage.cloudHostsPage.browsePage.table.transfer',
       sort: ['transfer'],
     },
-    {
-      name: 'cpuRating',
-      translationKey: 'hostsPage.cloudHostsPage.browsePage.table.cpu',
-      sort: ['cpuRating:desc','displayName'],
-      width: 120,
-    },
-    {
-      name: 'diskRating',
-      translationKey: 'hostsPage.cloudHostsPage.browsePage.table.disk',
-      sort: ['diskRating','displayName','zome'],
-      width: 120
-    },
+    // {
+    //   name: 'cpuRating',
+    //   translationKey: 'hostsPage.cloudHostsPage.browsePage.table.cpu',
+    //   sort: ['cpuRating:desc','displayName'],
+    //   width: 120,
+    // },
+    // {
+    //   name: 'diskRating',
+    //   translationKey: 'hostsPage.cloudHostsPage.browsePage.table.disk',
+    //   sort: ['diskRating','displayName','zome'],
+    //   width: 120
+    // },
     {
       name: 'price',
       translationKey: 'hostsPage.cloudHostsPage.browsePage.table.price',
@@ -95,16 +99,27 @@ export default Ember.Controller.extend({
     this.transitionToRoute('hosts.container-cloud', {queryParams: {from: this.get('tab')}});
   }),
 
-  filteredContent: Ember.computed('model.plans', 'realmSort', 'costSort', 'storageSort', 'memSort', function() {
+  providerContent: Ember.computed('model.plans', 'providerSort', function() {
+    var content = this.get('model.plans');
+    var prov = this.get('providerSort');
+
+    if (prov !== 'All') {
+      content = content.filterBy('provider', this.get('providerSort'))
+    }
+
+    return content;
+  }),
+
+  filteredContent: Ember.computed('providerContent', 'realmSort', 'costSort', 'storageSort', 'memSort', function() {
     var rs = this.get('realmSort');
     var cs = this.get('costSort');
     var ms = this.get('memSort');
     var ss = this.get('storageSort');
 
     if (rs === 'all') {
-      return this.get('model.plans');
+      return this.get('providerContent');
     } else {
-      return this.get('model.plans').filter((plan) => {
+      return this.get('providerContent').filter((plan) => {
         return (
           (!rs || plan.realm === rs) &&
           (!ms || plan.memory >= ms) &&
