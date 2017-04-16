56b7379a958c0df1595eb0b37ddd3d75141c2235
vimniky <vimniky@gmail.com>
2017-04-16 20:54:20 +0800

    add WeChat QR code

---
+++ app/components/modal-wechat/component.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import ModalBase from 'ui/mixins/modal-base';
+
+export default ModalBase.extend({
+  settings: Ember.inject.service(),
+  classNames: ['modal-container','about','span-6','offset-3'],
+});
