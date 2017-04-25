e005f1891cd3361c369acc123f952ab6a1ef2605
Vincent Fiduccia <vincent@rancher.com>
2017-04-25 11:53:24 -0700

    Switch tabs if balancer rules not available (rancher/rancher#8460)

--- app/service/port-rules/route.js
+++ app/service/port-rules/route.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     let service = this.modelFor('service').get('service');
+    if ( service.get('type').toLowerCase() !== 'loadbalancerservice' ) {
+      this.transitionTo('service.ports');
+      return;
+    }
+
     service.initPorts();
     return service;
   }
