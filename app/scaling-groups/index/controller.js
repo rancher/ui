ca2c66df4c3389d8a8de2ee9273d32db4f48fd03
lvuch <laurenvuchetich@gmail.com>
2017-04-27 10:27:01 -0700

    combine status and count

--- app/scaling-groups/index/controller.js
+++ app/scaling-groups/index/controller.js
@@ -62,19 +62,19 @@ export default Ember.Controller.extend({
       searchField: 'displayImage',
       translationKey: 'generic.image',
     },
-    {
-      name: 'instanceCount',
-      sort: ['stack.isDefault:desc','stack.displayName','instanceCount:desc','displayName'],
-      searchField: null,
-      width: 80,
-      icon: 'icon icon-lg icon-container',
-      dtTranslationKey: 'stacksPage.table.instanceCount'
-    },
+    // {
+    //   name: 'instanceCount',
+    //   sort: ['stack.isDefault:desc','stack.displayName','instanceCount:desc','displayName'],
+    //   searchField: null,
+    //   width: 80,
+    //   icon: 'icon icon-lg icon-container',
+    //   dtTranslationKey: 'stacksPage.table.instanceCount'
+    // },
     {
       name: 'instanceState',
       sort: ['stack.isDefault:desc','stack.displayName', 'instanceCountSort:desc','displayName'],
       searchField: null,
-      width: 100,
+      width: 140,
       icon: 'icon icon-lg icon-container',
       dtTranslationKey: 'stacksPage.table.instanceState',
       translationKey: 'stacksPage.table.instanceStateWithIcon',
