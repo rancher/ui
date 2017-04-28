a69a180904128fcf3a4e19e2bb1bcecee7e015b9
Westly Wright <westly.wright@gmail.com>
2017-04-28 11:17:21 -0700

    Update service details

--- app/service/index/route.js
+++ app/service/index/route.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  redirect: function(model) {
-    if (model.service.kind === 'dnsService') {
-      this.transitionTo('service.links');
-    } else {
-      this.transitionTo('service.containers');
-    }
-  }
 });
--- app/service/route.js
+++ app/service/route.js
@@ -1,13 +1,17 @@
 import Ember from 'ember';
+import FilteredSorted from 'ui/utils/filtered-sorted-array-proxy';
 
 export default Ember.Route.extend({
   model: function(params) {
     var service = this.get('store').getById('service', params.scaling_group_id);
     if ( service )
     {
-      return Ember.Object.create({
-        service: service,
-        stack: service.get('stack'),
+      return this.getServiceLogs(service).then((resp) => {
+        return Ember.Object.create({
+          service: service,
+          stack: service.get('stack'),
+          logs: resp.logs,
+        });
       });
     }
     else
@@ -20,4 +24,34 @@ export default Ember.Route.extend({
       });
     }
   },
+  afterModel(model) {
+    if (model.get('service.initPorts')) {
+      model.get('service').initPorts();
+    }
+  },
+  getServiceLogs(model) {
+    let par = model;
+    let serviceId = par.get('id');
+
+    // Find just the recent ones for this service
+    return this.get('store').find('serviceLog', null,{
+      filter: {serviceId: serviceId},
+      sortBy: 'id',
+      sortOrder: 'desc',
+      depaginate: false,
+      limit: 100
+    }).then(() => {
+      let all = this.get('store').all('serviceLog');
+      return Ember.Object.create({
+        logs: FilteredSorted.create({
+          sourceContent: all,
+          sortProperties: ['createdTS:desc'],
+          dependentKeys: ['sourceContent.@each.serviceId'],
+          filterFn: function(log) {
+            return log.get('serviceId') === serviceId;
+          }
+        })
+      });
+    });
+  }
 });
