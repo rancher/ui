6cc4f25560cacb1f34677c9af7acccb680bbf954
Westly Wright <westly.wright@gmail.com>
2017-05-23 16:33:28 -0700

    After inital save of host details page will refresh correctly

--- app/hosts/new/controller.js
+++ app/hosts/new/controller.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams : ['backTo', 'driver', 'hostId'],
-  backTo      : null,
-  driver      : null,
-  hostId      : null,
   actions: {
     completed() {}
   }
