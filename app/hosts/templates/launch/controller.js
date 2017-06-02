176e73eade940ad4a0f57cc5fbd08127e191edb1
Westly Wright <westly.wright@gmail.com>
2017-06-01 17:28:13 -0700

    fix bug in driver for host launch

--- app/hosts/templates/launch/controller.js
+++ app/hosts/templates/launch/controller.js
@@ -1,6 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  hostTemplate: Ember.computed.alias('model.template'),
-  machineConfig: Ember.computed.alias('model.config'),
 });
