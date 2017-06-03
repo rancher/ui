f811190e8a2bc92bc6cf44586d18d6adb542ee2a
Vincent Fiduccia <vincent@rancher.com>
2017-06-03 01:20:39 -0700

    Cleanup old add host

--- app/authenticated/project/host-template/keys/route.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model: function(/*params,transition*/) {
-    return this.get('store').find('hostTemplates', null, {forceReload: true}).then((templates) => {
-      return templates;
-    });
-  }
-});
