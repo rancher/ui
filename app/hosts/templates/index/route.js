3c4415239f4628d8c2bc345249ff00bd0f13247b
Vincent Fiduccia <vincent@rancher.com>
2017-06-05 11:41:20 -0700

    Go to add if no templates

--- app/hosts/templates/index/route.js
+++ app/hosts/templates/index/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(/* params */) {
     return this.get('store').findAll('hostTemplate').then((templates) => {
-      if ( templates.length ) {
+      if ( templates.get('length') ) {
         return templates;
       } else {
         this.transitionTo('hosts.new');
