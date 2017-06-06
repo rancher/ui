3492b0416c8cd976bfa8335c4d59cd3a21365793
Westly Wright <westly.wright@gmail.com>
2017-06-06 11:46:07 -0700

    Fix target rules not showing up on LB's and change to sort table
    rancher/rancher#8974

--- app/service/controller.js
+++ app/service/controller.js
@@ -4,7 +4,59 @@ export default Ember.Controller.extend({
   service: Ember.computed.alias('model.service'),
   stack: Ember.computed.alias('model.stack'),
   application: Ember.inject.controller(),
-  rules: Ember.computed.alias('model.lbConfig.portRules'),
+  rules: Ember.computed.alias('service.lbConfig.portRules'),
+  sortBy: 'priority',
+  headers: [
+    {
+      name: 'priority',
+      sort: ['priority'],
+      translationKey: 'formBalancerRules.priority.label',
+      width: 85,
+    },
+    {
+      name: 'access',
+      sort: ['access'],
+      translationKey: 'formBalancerRules.access.label',
+      width: 85,
+    },
+    {
+      name: 'protocol',
+      sort: ['protocol'],
+      translationKey: 'formBalancerRules.protocol.label',
+      width: 95
+    },
+    {
+      name: 'hostname',
+      sort: ['hostname'],
+      translationKey: 'formBalancerRules.hostname.label',
+    },
+    {
+      name: 'sourcePort',
+      sort: ['sourcePort'],
+      translationKey: 'formBalancerRules.sourcePort.label',
+      width: 145
+    },
+    {
+      name: 'path',
+      sort: ['path'],
+      translationKey: 'formBalancerRules.path.label',
+    },
+    {
+      name: 'target',
+      translationKey: 'formBalancerRules.target',
+    },
+    {
+      name: 'targetPort',
+      sort: ['targetPort'],
+      translationKey: 'formBalancerRules.targetPort.label',
+      width: 80
+    },
+    {
+      name: 'backendName',
+      sort: ['backendName'],
+      translationKey: 'formBalancerRules.backendName.label',
+    },
+  ],
 
   actions: {
     changeService(service) {
