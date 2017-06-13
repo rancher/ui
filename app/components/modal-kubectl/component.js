86e0a56e04a1b0e87247baef21a699109bda9872
Vincent Fiduccia <vincent@rancher.com>
2017-06-13 12:47:24 -0700

    RBAC support in kubectl shell

--- app/components/modal-kubectl/component.js
+++ app/components/modal-kubectl/component.js
@@ -3,8 +3,9 @@ import ModalBase from 'ui/mixins/modal-base';
 import C from 'ui/utils/constants';
 
 export default Ember.Component.extend(ModalBase, {
+  access: Ember.inject.service(),
+
   classNames: ['modal-container', 'large-modal', 'fullscreen-modal', 'modal-shell', 'alert'],
-  command: ['/bin/bash','-l','-c','echo "# Run kubectl commands inside here\n# e.g. kubectl get rc\n"; TERM=xterm-256color /bin/bash'],
   loading: true,
   model: null,
   error: null,
@@ -18,6 +19,15 @@ export default Ember.Component.extend(ModalBase, {
     this._super(...arguments);
     this.shortcuts.enable();
   },
+
+  command: Ember.computed('model.labels', function() {
+    var labels = container.get('labels')||{};
+    if ( labels[C.LABEL.K8S_TOKEN]+'' === 'true' ) {
+      return ['kubectl-shell.sh', this.get('access.token.jwt')];
+    } else {
+      return ['/bin/bash','-l','-c','echo "# Run kubectl commands inside here\n# e.g. kubectl get rc\n"; TERM=xterm-256color /bin/bash'];
+    }
+  }),
 
   didReceiveAttrs() {
     this.get('store').findAll('container').then((containers) => {
