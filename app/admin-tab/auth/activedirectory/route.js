fb6195b7176a181d81f006d0523ced3cf14c927f
Westly Wright <westly.wright@gmail.com>
2017-06-28 09:59:24 -0700

    update defaults logic for ad

--- app/admin-tab/auth/activedirectory/route.js
+++ app/admin-tab/auth/activedirectory/route.js
@@ -3,83 +3,34 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   resourceType: 'ldapconfig',
 
-  model: function() {
-    // fake ldap config for now until i get a real config back from the api
-    const ldapConfig = {
-      "id": null,
-      "type": "ldapconfig",
-      "links": {},
-      "baseType": "ldapconfig",
-      "actionLinks": {},
-      "accessMode": "unrestricted",
-      "allowedIdentities": [],
-      "connectionTimeout": 1000,
-      "domain": null,
-      "enabled": false,
-      "groupMemberMappingAttribute": "memberOf",
-      "groupNameField": "name",
-      "groupObjectClass": "group",
-      "groupSearchDomain": null,
-      "groupSearchField": "sAMAccountName",
-      "loginDomain": null,
-      "name": "ldapconfig",
-      "port": 389,
-      "server": null,
-      "serviceAccountPassword": null,
-      "serviceAccountUsername": null,
-      "tls": false,
-      "userDisabledBitMask": 2,
-      "userEnabledAttribute": "userAccountControl",
-      "userLoginField": "sAMAccountName",
-      "userMemberAttribute": "memberOf",
-      "userNameField": "name",
-      "userObjectClass": "person",
-      "userSearchField": "sAMAccountName"
-    };
-
-    return this.get('authStore').find('config', null, {forceReload: true}).then((collection) => {
+  model: function () {
+    return this.get('authStore').find('config', null, {
+      forceReload: true
+    }).then((collection) => {
 
       if (!collection.enabled) {
-        let existing = collection.ldapConfig;
+        let existing = Ember.Object.create(collection.ldapConfig);
+        let defaults = this.get('authStore').getById('schema', this.get('resourceType')).get('resourceFields');
 
-        // this should all come from the ldap config in the schema but it does not exist right now
-        Object.keys(ldapConfig).forEach((key) => {
-          let field = ldapConfig[key];
-
-          if ( field &&  !existing[key] ) {
-            existing[key] = field;
+        Object.keys(defaults).forEach((key) => {
+          var field = defaults[key];
+          if (field && field.default && !existing.get(key)) {
+            existing.set(key, field.default);
           }
-
         });
-      }
 
+        collection.ldapConfig = existing;
+      }
       return collection;
     });
-
-    // return this.get('userStore').find(this.get('resourceType'), null, {forceReload: true}).then((collection) => {
-    //   debugger;
-    //   var existing = collection.get('firstObject');
-
-    //   // On install the initial ldapconfig is empty.  For any fields that are empty, fill in the default from the schema.
-    //   var defaults = this.get('userStore').getById('schema',this.get('resourceType')).get('resourceFields');
-    //   Object.keys(defaults).forEach((key) => {
-    //     var field = defaults[key];
-    //     if ( field && field.default && !existing.get(key) )
-    //     {
-    //       existing.set(key, field.default);
-    //     }
-    //   });
-
-    //   return existing;
-    // });
   },
 
-  setupController: function(controller, model) {
+  setupController: function (controller, model) {
     controller.setProperties({
       model:          model,
       confirmDisable: false,
       testing:        false,
-      organizations:  this.get('session.orgs')||[],
+      organizations:  this.get('session.orgs') || [],
       errors:         null,
     });
   }
