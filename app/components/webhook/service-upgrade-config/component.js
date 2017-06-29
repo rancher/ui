c35e5a48c0f8b837eada35b1cc3776dfdb0c3122
loganhz <logan@rancher.com>
2017-06-29 14:14:13 +0800

    add Alibaba Cloud Docker Hub support

--- app/components/webhook/service-upgrade-config/component.js
+++ app/components/webhook/service-upgrade-config/component.js
@@ -2,9 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: '',
+  registryChoices: [
+    { label: 'newReceiver.registry.dockerhub', value: 'dockerhub' },
+    { label: 'newReceiver.registry.alicloud', value: 'alicloud' }
+  ],
 
   actions: {
-    optionsChanged: function(opt) {
+    optionsChanged: function (opt) {
       this.get('model').setProperties(opt);
     }
   }
