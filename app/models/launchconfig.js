03357a3702c7c0c464d972b7195a66efbb247434
Vincent Fiduccia <vincent@rancher.com>
2017-07-20 11:06:48 -0700

    Remove init hack

--- app/models/launchconfig.js
+++ app/models/launchconfig.js
@@ -6,22 +6,4 @@ var LaunchConfig = Resource.extend({
   }.property('imageUuid'),
 });
 
-LaunchConfig.reopenClass({
-  mangleIn(data) {
-    if (data.hasOwnProperty('init')) {
-      data._init = data.init;
-      delete data.init;
-    }
-    return data;
-  },
-  mangleOut(data) {
-    if (data.hasOwnProperty('_init')) {
-      data.init = data._init;
-      delete data._init;
-    }
-    return data;
-  }
-
-});
-
 export default LaunchConfig;
--- app/models/secondarylaunchconfig.js
+++ app/models/secondarylaunchconfig.js
@@ -6,22 +6,4 @@ var SecondaryLaunchConfig = Resource.extend({
   }.property('imageUuid'),
 });
 
-SecondaryLaunchConfig.reopenClass({
-  mangleIn(data) {
-    if (data.hasOwnProperty('init')) {
-      data._init = data.init;
-      delete data.init;
-    }
-    return data;
-  },
-  mangleOut(data) {
-    if (data.hasOwnProperty('_init')) {
-      data.init = data._init;
-      delete data._init;
-    }
-    return data;
-  }
-
-});
-
 export default SecondaryLaunchConfig;
