307d26b638dbb3906f0ef0fb0cf49fa142c7a9a4
Vincent Fiduccia <vincent@rancher.com>
2017-07-21 16:15:36 -0700

    New AWS instance types & grouping (rancher/rancher#8821)

--- app/components/machine/driver-amazonec2/component.js
+++ app/components/machine/driver-amazonec2/component.js
@@ -54,15 +54,76 @@ let RANCHER_INGRESS_RULES = [
 ];
 
 let INSTANCE_TYPES = [
-  't2.nano','t2.micro','t2.small','t2.medium', 't2.large','t2.xlarge','t2.2xlarge',
-  'm3.medium','m3.large','m3.xlarge','m3.2xlarge',
-  'm4.large','m4.xlarge','m4.2xlarge','m4.4xlarge','m4.10xlarge',
-  'c4.large','c4.xlarge','c4.2xlarge','c4.4xlarge','c4.8xlarge',
-  'c3.large','c3.xlarge','c3.2xlarge','c3.4xlarge','c3.8xlarge',
-  'r3.large','r3.xlarge','r3.2xlarge','r3.4xlarge','r3.8xlarge',
-  'g2.2xlarge','g2.8xlarge',
-  'i2.xlarge','i2.2xlarge','i2.4xlarge','i2.8xlarge',
-  'd2.xlarge','d2.2xlarge','d2.4xlarge','d2.8xlarge',
+  {group: 'T2 - Burstable', name: 't2.nano'},
+  {group: 'T2 - Burstable', name: 't2.micro'},
+  {group: 'T2 - Burstable', name: 't2.small'},
+  {group: 'T2 - Burstable', name: 't2.medium'},
+  {group: 'T2 - Burstable', name: 't2.large'},
+  {group: 'T2 - Burstable', name: 't2.xlarge'},
+  {group: 'T2 - Burstable', name: 't2.2xlarge'},
+
+  {group: 'M4 - General Purpose', name: 'm4.large'},
+  {group: 'M4 - General Purpose', name: 'm4.xlarge'},
+  {group: 'M4 - General Purpose', name: 'm4.2xlarge'},
+  {group: 'M4 - General Purpose', name: 'm4.4xlarge'},
+  {group: 'M4 - General Purpose', name: 'm4.10xlarge'},
+  {group: 'M4 - General Purpose', name: 'm4.16xlarge'},
+
+  {group: 'M3 - General Purpose', name: 'm3.medium'},
+  {group: 'M3 - General Purpose', name: 'm3.large'},
+  {group: 'M3 - General Purpose', name: 'm3.xlarge'},
+  {group: 'M3 - General Purpose', name: 'm3.2xlarge'},
+
+  {group: 'C4 - High CPU', name: 'c4.large'},
+  {group: 'C4 - High CPU', name: 'c4.xlarge'},
+  {group: 'C4 - High CPU', name: 'c4.2xlarge'},
+  {group: 'C4 - High CPU', name: 'c4.4xlarge'},
+  {group: 'C4 - High CPU', name: 'c4.8xlarge'},
+
+  {group: 'R4 - High Memory', name: 'r4.large'},
+  {group: 'R4 - High Memory', name: 'r4.xlarge'},
+  {group: 'R4 - High Memory', name: 'r4.2xlarge'},
+  {group: 'R4 - High Memory', name: 'r4.4xlarge'},
+  {group: 'R4 - High Memory', name: 'r4.8xlarge'},
+  {group: 'R4 - High Memory', name: 'r4.16xlarge'},
+
+  {group: 'R3 - High Memory', name: 'r3.large'},
+  {group: 'R3 - High Memory', name: 'r3.xlarge'},
+  {group: 'R3 - High Memory', name: 'r3.2xlarge'},
+  {group: 'R3 - High Memory', name: 'r3.4xlarge'},
+  {group: 'R3 - High Memory', name: 'r3.8xlarge'},
+
+  {group: 'C3 - High CPU', name: 'c3.large'},
+  {group: 'C3 - High CPU', name: 'c3.xlarge'},
+  {group: 'C3 - High CPU', name: 'c3.2xlarge'},
+  {group: 'C3 - High CPU', name: 'c3.4xlarge'},
+  {group: 'C3 - High CPU', name: 'c3.8xlarge'},
+
+  {group: 'D2 - High Density Storage', name: 'd2.xlarge'},
+  {group: 'D2 - High Density Storage', name: 'd2.2xlarge'},
+  {group: 'D2 - High Density Storage', name: 'd2.4xlarge'},
+  {group: 'D2 - High Density Storage', name: 'd2.8xlarge'},
+
+  {group: 'I3 - High I/O Storage', name: 'i3.large'},
+  {group: 'I3 - High I/O Storage', name: 'i3.xlarge'},
+  {group: 'I3 - High I/O Storage', name: 'i3.2xlarge'},
+  {group: 'I3 - High I/O Storage', name: 'i3.4xlarge'},
+  {group: 'I3 - High I/O Storage', name: 'i3.8xlarge'},
+  {group: 'I3 - High I/O Storage', name: 'i3.16xlarge'},
+
+  {group: 'F1 - FPGA', name: 'f1.2xlarge'},
+  {group: 'F1 - FPGA', name: 'f1.16xlarge'},
+
+  {group: 'G3 - GPU', name: 'g3.4xlarge'},
+  {group: 'G3 - GPU', name: 'g3.8xlarge'},
+  {group: 'G3 - GPU', name: 'g3.16xlarge'},
+
+  {group: 'P2 - GPU', name: 'p2.xlarge'},
+  {group: 'P2 - GPU', name: 'p2.8xlarge'},
+  {group: 'P2 - GPU', name: 'p2.16xlarge'},
+
+  {group: 'X1 - Really High Memory', name: 'x1.16xlarge'},
+  {group: 'X1 - Really High Memory', name: 'x1.32xlarge'},
 ];
 
 // These need to match the supported list in docker-machine:
