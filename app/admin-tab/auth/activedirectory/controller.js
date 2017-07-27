83665c617fb3666e812019638a952a29218bd67e
Westly Wright <westly.wright@gmail.com>
2017-07-27 12:17:55 -0700

    Revert Active Directory auth service switch

--- app/admin-tab/auth/activedirectory/controller.js
+++ app/admin-tab/auth/activedirectory/controller.js
@@ -3,153 +3,81 @@ import Errors from 'ui/utils/errors';
 import C from 'ui/utils/constants';
 
 var PLAIN_PORT = 389;
-var TLS_PORT   = 636;
+var TLS_PORT = 636;
 
 export default Ember.Controller.extend({
-  access:         Ember.inject.service(),
-  settings:       Ember.inject.service(),
+  access: Ember.inject.service(),
+  settings: Ember.inject.service(),
 
   confirmDisable: false,
-  errors:         null,
-  testing:        false,
+  errors: null,
+  testing: false,
 
-  providerName:   'ldap.providerName.ad',
-  userType:       C.PROJECT.TYPE_LDAP_USER,
-  groupType:      C.PROJECT.TYPE_LDAP_GROUP,
-  ldapConfig:     Ember.computed.alias('model.ldapConfig'),
+  providerName: 'ldap.providerName.ad',
+  userType: C.PROJECT.TYPE_LDAP_USER,
+  groupType: C.PROJECT.TYPE_LDAP_GROUP,
 
-  addUserInput:   '',
-  addOrgInput:    '',
+  addUserInput: '',
+  addOrgInput: '',
 
-  username:       '',
-  password:       '',
-  editing:        false,
+  username: '',
+  password: '',
 
-  createDisabled: Ember.computed('username.length','password.length', function() {
+  createDisabled: function() {
     return !this.get('username.length') || !this.get('password.length');
-  }),
+  }.property('username.length','password.length'),
 
-  numUsers: Ember.computed('model.allowedIdentities.@each.externalIdType','userType','groupType', function() {
+  numUsers: function() {
     return (this.get('model.allowedIdentities')||[]).filterBy('externalIdType', this.get('userType')).get('length');
-  }),
+  }.property('model.allowedIdentities.@each.externalIdType','userType','groupType'),
 
-  numGroups: Ember.computed('model.allowedIdentities.@each.externalIdType','userType','groupType', function() {
+  numGroups: function() {
     return (this.get('model.allowedIdentities')||[]).filterBy('externalIdType', this.get('groupType')).get('length');
-  }),
+  }.property('model.allowedIdentities.@each.externalIdType','userType','groupType'),
 
-  canEdit: Ember.computed('access.enabled', 'editing', function() {
-    var isEnabled = this.get('access.enabled');
-    var editing   = this.get('editing');
-
-    if (( isEnabled && editing ) || !isEnabled) {
-      return true;
-    } else if (isEnabled && !editing) {
-      return false;
-    }
-
-  }),
-
-  tlsChanged: Ember.observer('ldapConfig.tls', function() {
-    var on   = this.get('ldapConfig.tls');
-    var port = parseInt(this.get('ldapConfig.port'),10);
+  tlsChanged: function() {
+    var on = this.get('model.tls');
+    var port = parseInt(this.get('model.port'),10);
 
     if ( on && port === PLAIN_PORT )
     {
-      this.set('ldapConfig.port', TLS_PORT);
+      this.set('model.port', TLS_PORT);
     }
     else if ( !on && port === TLS_PORT )
     {
-      this.set('ldapConfig.port', PLAIN_PORT);
+      this.set('model.port', PLAIN_PORT);
     }
-  }),
-
-  testConfig: function(data) {
-    return this.get('authStore').rawRequest({
-      url:    'testlogin',
-      method: 'POST',
-      data:   data,
-    });
-  },
-
+  }.observes('model.tls'),
 
   actions: {
-    edit: function() {
-      this.toggleProperty('editing');
-      this.set('originalModel', this.get('model').clone());
-    },
-    cancel: function() {
-      this.send('clearError');
-      this.set('editing', false);
-      this.set('model', this.get('originalModel'));
-    },
     test: function() {
       this.send('clearError');
 
-      let model   = this.get('model');
-      let editing = this.get('editing');
-
-      if (!editing) {
-        model.setProperties({
-          'provider'          : 'ldapconfig',
-          'enabled'           : false, // It should already be, but just in case..
-          'accessMode'        : 'unrestricted',
-          'allowedIdentities' : [],
-        });
-      }
+      var model = this.get('model');
+      model.setProperties({
+        enabled: false,
+        'accessMode': 'unrestricted',
+      });
 
       var errors = model.validationErrors();
-
-
-      let data  = {
-        type:       'testAuthConfig',
-        authConfig: model,
-        code:       `${this.get('username')}:${this.get('password')}`,
-      };
-
-      if ( errors.get('length') ) {
-
+      if ( errors.get('length') )
+      {
         this.set('errors', errors);
-
-      } else {
-
+      }
+      else
+      {
         this.set('testing', true);
-
-        if (editing) {
-
-          this.testConfig(data).then((resp) => {
-
-            if (resp.status === 200) {
-
-              model.save().then(() => {
-                this.send('waitAndRefresh');
-              }).catch(err => {
-                this.send('gotError', err);
-              });
-
-            }
-
-          }).catch((err) => {
-
-            this.send('gotError', err.statusText);
-          });
-
-        } else {
-
-          this.set('testing', true);
-          model.save().then(() => {
-            this.send('authenticate');
-          }).catch(err => {
-            this.send('gotError', err);
-          });
-        }
+        model.save().then(() => {
+          this.send('authenticate');
+        }).catch(err => {
+          this.send('gotError', err);
+        });
       }
     },
 
     authenticate: function() {
       this.send('clearError');
-
-      var code = `${this.get('username')}:${this.get('password')}`;
-
+      var code = this.get('username')+':'+this.get('password');
       this.get('access').login(code).then(res => {
         this.send('authenticationSucceeded', res.body);
       }).catch(err => {
@@ -165,10 +93,9 @@ export default Ember.Controller.extend({
       this.set('access.enabled', true);
 
       var model = this.get('model');
-
       model.setProperties({
-        'enabled':           true,
-        'accessMode':        'unrestricted',
+        'enabled': true,
+        'accessMode': 'unrestricted',
         'allowedIdentities': [auth.userIdentity],
       });
 
@@ -182,7 +109,6 @@ export default Ember.Controller.extend({
 
     waitAndRefresh: function(url) {
       $('#loading-underlay, #loading-overlay').removeClass('hide').show();
-
       setTimeout(function() {
         window.location.href = url || window.location.href;
       }, 1000);
