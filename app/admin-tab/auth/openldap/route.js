46216cc049bdcf57650e601d88fc758e590bcd51
Westly Wright <westlywright@users.noreply.github.com>
2017-08-03 16:18:10 -0700

    Revert "Revert Active Directory auth service switch"

--- app/admin-tab/auth/openldap/route.js
+++ app/admin-tab/auth/openldap/route.js
@@ -1,5 +1,34 @@
-import ActiveDirectory from 'ui/admin-tab/auth/activedirectory/route';
+import Ember from 'ember';
 
-export default ActiveDirectory.extend({
+export default Ember.Route.extend({
   resourceType: 'openldapconfig',
+
+  model: function() {
+
+    return this.get('userStore').find(this.get('resourceType'), null, {forceReload: true}).then((collection) => {
+      var existing = collection.get('firstObject');
+
+      // On install the initial ldapconfig is empty.  For any fields that are empty, fill in the default from the schema.
+      var defaults = this.get('userStore').getById('schema',this.get('resourceType')).get('resourceFields');
+      Object.keys(defaults).forEach((key) => {
+        var field = defaults[key];
+        if ( field && field.default && !existing.get(key) )
+        {
+          existing.set(key, field.default);
+        }
+      });
+
+      return existing;
+    });
+  },
+
+  setupController: function(controller, model) {
+    controller.setProperties({
+      model: model,
+      confirmDisable: false,
+      testing: false,
+      organizations: this.get('session.orgs')||[],
+      errors: null,
+    });
+  }
 });
