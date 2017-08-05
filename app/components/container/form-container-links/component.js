124ae29704763defee0881c0c2307c31ef75a0f2
Vincent Fiduccia <vincent@rancher.com>
2017-08-04 18:10:27 -0700

    InstanceLinks

--- app/components/container/form-container-links/component.js
+++ app/components/container/form-container-links/component.js
@@ -6,107 +6,46 @@ export default Ember.Component.extend(ContainerChoices, {
   // Inputs
   editing: null,
   instance: null,
-  allHosts: null,
-  initialLinks: null,
-  linksArray: null,
+
+  linksArray: Ember.computed.alias('instance.instanceLinks'),
 
   tagName: '',
   errors: null,
 
   actions: {
     addLink: function() {
-      this.get('linksArray').pushObject({name: '', targetInstanceId: null});
+      let links = this.get('linksArray');
+      if ( !links ) {
+        links = [];
+        this.set('linksArray', links);
+      }
+
+      links.pushObject(this.get('store').createRecord({
+        type: 'link',
+        name: '',
+        alias: '',
+      }));
     },
+
     removeLink: function(obj) {
       this.get('linksArray').removeObject(obj);
     },
   },
 
-  init() {
-    this._super(...arguments);
-    var out = [];
-    var links = this.get('initialLinks')||[];
-
-    if ( Ember.isArray(links) )
-    {
-      links.forEach(function(value) {
-        // Objects, from edit
-        if ( typeof value === 'object' )
-        {
-          out.push({
-            existing: (value.id ? true : false),
-            obj: value,
-            name: value.linkName || value.name,
-            targetInstanceId: value.targetInstanceId,
-          });
-        }
-        else
-        {
-          // Strings, from create maybe
-          var match = value.match(/^([^:]+):(.*)$/);
-          if ( match )
-          {
-            out.push({name: match[1], targetInstanceId: match[2], existing: false});
-          }
-        }
-      });
-    }
-
-    this.set('linksArray', out);
-
-    Ember.run.scheduleOnce('afterRender', () => {
-      this.linksDidChange();
-    });
-  },
-
-  linksDidChange: function() {
-    var errors = [];
-    var linksAsMap = {};
-
-    this.get('linksArray').forEach((row) => {
-      if ( row.targetInstanceId )
-      {
-        var name = row.name;
-        // Lookup the container name if an "as name" wasn't given
-        if ( !name )
-        {
-          var container = this.get('store').getById('container', row.targetInstanceId);
-          if ( container )
-          {
-            name = container.name;
-          }
-        }
-
-        if ( name )
-        {
-          linksAsMap[ name ] = row.targetInstanceId;
-        }
-        else
-        {
-          errors.push('Link to container ' + row.targetInstanceId + '  must have an "as name".');
-        }
-      }
-    });
-
-    this.set('instance.instanceLinks', linksAsMap);
-    this.set('errors', errors);
-    this.sendAction('changed', this.get('linksArray'));
-  }.observes('linksArray.@each.{targetInstanceId,name}'),
-
   statusClass: null,
   status: function() {
     let k = STATUS.NONE;
-    let count = (this.get('linksArray')||[]).filterBy('targetInstanceId').get('length') || 0;
+    let count = (this.get('linksArray')||[]).filterBy('name').get('length') || 0;
 
     if ( count ) {
       if ( this.get('errors.length') ) {
         k = STATUS.INCOMPLETE;
       } else {
-        k = STATUS.CONFIGURED;
+        k = STATUS.COUNTCONFIGURED;
       }
     }
 
     this.set('statusClass', classForStatus(k));
     return this.get('intl').t(`${STATUS_INTL_KEY}.${k}`, {count: count});
-  }.property('userLabelArray.@each.key'),
+  }.property('linksArray.@each.name'),
 });
