53fc0e27b3da835d05932cc4f49aa99f8ad31301
Westly Wright <westly.wright@gmail.com>
2017-09-25 12:43:19 -0700

    Reset compose controller on exit

--- app/new-stack/route.js
+++ app/new-stack/route.js
@@ -77,6 +77,7 @@ export default Ember.Route.extend({
         githubRepo: null,
         githubBranch: null,
         composeFiles: null,
+        compose: null,
         system: false,
       });
     }
