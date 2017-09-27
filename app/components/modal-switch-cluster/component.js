b348d56abf7d659a2d8242e81a262ed79a0e6819
Westly Wright <westly.wright@gmail.com>
2017-09-27 11:44:39 -0700

    Add check for cluster delete to ensure user is not in the current

---
+++ app/components/modal-switch-cluster/component.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import ModalBase from 'ui/mixins/modal-base';
+
+export default Ember.Component.extend(ModalBase, {
+  classNames: ['generic', 'about', 'medium-modal'],
+});
