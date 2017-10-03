2e5fd911ff15710eaadb2c047593ceda565b485e
Westly Wright <westly.wright@gmail.com>
2017-10-03 15:51:49 -0700

    Change the way cluster delete selectes new projects in removed clust

--- app/components/modal-switch-cluster/component.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import ModalBase from 'ui/mixins/modal-base';
-
-export default Ember.Component.extend(ModalBase, {
-  classNames: ['generic', 'about', 'medium-modal'],
-});
