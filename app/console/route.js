94867b7fa95ede93fffe6d188c902f460bca7f48
Westly Wright <westly.wright@gmail.com>
2017-10-04 16:58:51 -0700

    add common command to k8s service

--- app/console/route.js
+++ app/console/route.js
@@ -3,35 +3,31 @@ import C from 'ui/utils/constants';
 
 export default Ember.Route.extend({
   projects: Ember.inject.service(),
+  k8s: Ember.inject.service(),
   model: function(params) {
     let store = this.get('store');
     if (params.kubernetes) {
-      let systemProject = this.get('projects.current.cluster.systemProject');
-      let inst;
-
-      if ( !systemProject ) {
-        return Ember.RSVP.reject('Unable to locate system environment');
-      }
-
-      return this.get('userStore').rawRequest({
-        url: systemProject.links.instances,
-      }).then((res) => {
-        inst = res.body.data.find((c) => {
-          return c.state === 'running'
-            && c.labels
-            && c.labels[C.LABEL.K8S_KUBECTL]+'' === 'true';
-        });
-
-        if ( inst ) {
-          return this.get('store').createRecord(inst);
-        } else {
-          return Ember.RSVP.reject('Unable to find running kubectl container');
-        }
-      });
+      return this.get('k8s').getInstanceToConnect();
     }
 
     return store.find('container', params.instanceId).then((response) => {
       return response;
     });
   },
+  setupController(controller, model) {
+    this._super(controller, model);
+    if (controller.get('kubernetes')) {
+      controller.set('command', Ember.computed('model.labels', function() {
+        var labels = this.get('model.labels')||{};
+        if ( labels[C.LABEL.K8S_TOKEN]+'' === 'true' ) {
+          return [
+            'kubectl-shell.sh',
+            this.get('cookies').get(C.COOKIE.TOKEN) || 'unauthorized'
+          ];
+        } else {
+          return ['/bin/bash','-l','-c','echo "# Run kubectl commands inside here\n# e.g. kubectl get rc\n"; TERM=xterm-256color /bin/bash'];
+        }
+      }));
+    }
+  }
 });
--- app/services/k8s.js
+++ app/services/k8s.js
@@ -4,6 +4,7 @@ import C from 'ui/utils/constants';
 
 export default Ember.Service.extend({
   store: Ember.inject.service('store'),
+  userStore: Ember.inject.service('user-store'),
   projects: Ember.inject.service(),
 
   kubernetesDashboard: function() {
@@ -52,6 +53,31 @@ export default Ember.Service.extend({
       status: err.status,
       code: err.body.exitCode,
       message: err.body.stdErr.split(/\n/),
+    });
+  },
+
+  getInstanceToConnect() {
+    let systemProject = this.get('projects.current.cluster.systemProject');
+    let inst;
+
+    if ( !systemProject ) {
+      return Ember.RSVP.reject('Unable to locate system environment');
+    }
+
+    return this.get('userStore').rawRequest({
+      url: systemProject.links.instances,
+    }).then((res) => {
+      inst = res.body.data.find((c) => {
+        return c.state === 'running'
+          && c.labels
+          && c.labels[C.LABEL.K8S_KUBECTL]+'' === 'true';
+      });
+
+      if ( inst ) {
+        return this.get('store').createRecord(inst);
+      } else {
+        return Ember.RSVP.reject('Unable to find running kubectl container');
+      }
     });
   },
 });
