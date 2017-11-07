5fb74a0d1aff63bad7ce60e9436640b448c118ea
Westly Wright <westly.wright@gmail.com>
2017-11-07 16:55:07 -0700

    migrate final admin-tab routes

--- app/app.js
+++ app/app.js
@@ -27,6 +27,7 @@ const App = Application.extend({
         services: [
           'access',
           'auth-store',
+          'catalog',
           'endpoint',
           'github',
           'intl',
