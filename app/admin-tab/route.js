ceaa077bf99bfe5225e34b9a86e26bfabc804f27
Westly Wright <westly.wright@gmail.com>
2017-11-07 14:52:59 -0700

    remove more unneeded routes

--- app/admin-tab/route.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-  activate() {
-    this._super();
-    this.controllerFor('authenticated').setPageScope('admin');
-  },
-});
