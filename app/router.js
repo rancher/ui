4adde7cdeec2e3b078d976d849041a4537a30fbf
Westly Wright <westly.wright@gmail.com>
2017-11-08 11:18:52 -0700

    Update to 2.16.2 from beta branch for fixes

--- app/router.js
+++ app/router.js
@@ -37,41 +37,6 @@ Router.map(function() {
 
     this.route('prefs');
 
-    // Admin
-    // this.route('admin-tab', {path: '/admin', resetNamespace: true}, function() {
-    //   this.route('accounts', {path: '/accounts'}, function() {
-    //     this.route('index', {path: '/'});
-    //     this.route('new', {path: '/add'});
-    //   });
-
-    //   this.route('audit-logs');
-    //   this.route('catalog');
-    //   this.route('ha');
-
-    //   this.route('settings', function() {
-    //     this.route('auth', {path: '/access'}, function() {
-    //       this.route('activedirectory');
-    //       this.route('azuread');
-    //       this.route('github');
-    //       this.route('openldap');
-    //       this.route('localauth', {path: 'local'});
-    //       this.route('shibboleth');
-    //     });
-
-    //     this.route('machine');
-    //     this.route('registration');
-    //     this.route('advanced');
-    //   });
-
-    //   this.route('processes', {path: '/processes'}, function() {
-    //     this.route('index', {path: '/'});
-    //     this.route('pools', {path: '/pools'});
-    //     this.route('list', {path: '/list'});
-    //   });
-    //   this.route('process', {path: '/processes/:process_id'});
-
-    // });
-
     // Clusters
     this.route('clusters', {path: '/clusters'}, function() {
       this.route('index', {path: '/'});
