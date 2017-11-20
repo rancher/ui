fe12d6472312d7eb45016f62fc7299a76150cb71
Westly Wright <westly.wright@gmail.com>
2017-11-20 15:34:02 -0700

    More add cluster works

--- app/authenticated/clusters/new/rke/controller.js
+++ app/authenticated/clusters/new/rke/controller.js
@@ -1,8 +1,11 @@
 import Controller from '@ember/controller';
 import { copy } from '@ember/object/internals';
 import { get, set } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { computed, observer } from '@ember/object';
 
 export default Controller.extend({
+  modalService: service('modal'),
   step:        null,
   loading:     null,
   newHost:     null,
@@ -10,6 +13,8 @@ export default Controller.extend({
   clusterList: null,
   managementList: null,
   nodesList: null,
+  canAdd: null,
+
   init() {
     this._super(...arguments);
     let newHost = {
@@ -21,15 +26,37 @@ export default Controller.extend({
       step: 1,
       newHost: newHost,
       loading: false,
+      canAdd: false,
       newOrReuse: 'new',
       clusterList: [],
       managementList: [],
       nodesList: [],
     });
   },
+
+  hostObserver: observer('newHost.clusterName','newHost.hostName','newHost.ssh', function() {
+    let cName = get(this, 'newHost.clusterName');
+    let hName = get(this, 'newHost.hostName');
+    let ssh = get(this, 'newHost.ssh');
+    (cName.length > 0) && (hName.length > 0) && (ssh.length > 0) ? set(this, 'canAdd', true) : set(this, 'canAdd', false);
+  }),
+
   actions: {
+    save() {
+    },
     cancel(prev) {
       this.send('goToPrevious',prev);
+    },
+    addNew(type) {
+      let newHost = {
+        clusterName: '',
+        hostName: '',
+        ssh: '',
+      }
+      this.get('modalService').toggleModal('modal-add-cluster', {
+        newHost: newHost,
+        list: get(this, type),
+      });
     },
     go() {
       this.incrementProperty('step');
@@ -51,6 +78,5 @@ export default Controller.extend({
     useFor(host, list) {
       get(this, list).pushObject(host);
     },
-    addManagementHost() {},
   }
 });
