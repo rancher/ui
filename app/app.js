bb7f7cf53137b0760ae260be7e0b26b4f226c679
loganhz <logan@rancher.com>
2017-11-20 17:48:10 +0800

    Add authz-store as dependency

--- app/app.js
+++ app/app.js
@@ -27,6 +27,7 @@ const App = Application.extend({
         services: [
           'access',
           'authn-store',
+          'authz-store',
           'catalog',
           'endpoint',
           'github',
