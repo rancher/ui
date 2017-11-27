2683d101f205ac875f3a2443aa447d206590aba7
loganhz <logan@rancher.com>
2017-11-27 21:03:42 +0800

    Format the code

--- app/authenticated/projects/edit/route.js
+++ app/authenticated/projects/edit/route.js
@@ -3,14 +3,14 @@ import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    authzStore: service('authz-store'),
-    model: function (params) {
-        return hash({
-            project: this.get('authzStore').find('project', params.project_id),
-            projectRoleTemplateBindings: this.get('authzStore').findAll('projectRoleTemplateBinding', { forceReload: true })
-                .then(bindings => bindings.filter(b => b.projectId === params.project_id)),
-            projects: this.get('authzStore').findAll('project'),
-            roles: this.get('authzStore').findAll('projectRoleTemplate'),
-        });
-    },
+  authzStore: service('authz-store'),
+  model: function (params) {
+    return hash({
+      project: this.get('authzStore').find('project', params.project_id),
+      projectRoleTemplateBindings: this.get('authzStore').findAll('projectRoleTemplateBinding', { forceReload: true })
+        .then(bindings => bindings.filter(b => b.projectId === params.project_id)),
+      projects: this.get('authzStore').findAll('project'),
+      roles: this.get('authzStore').findAll('projectRoleTemplate'),
+    });
+  },
 });
--- app/authenticated/projects/new/route.js
+++ app/authenticated/projects/new/route.js
@@ -3,23 +3,23 @@ import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    authzStore: service('authz-store'),
-    model: function () {
-        const project = this.get('authzStore').createRecord({
-            type: `project`,
-            name: '',
-        });
-        const projectRoleTemplateBindings = [{
-            subjectKind: 'User',
-            subjectName: '',
-            projectRoleTemplateId: '',
-            projectId: '',
-        }];
-        return hash({
-            project,
-            projectRoleTemplateBindings,
-            projects: this.get('authzStore').findAll('project'),
-            roles: this.get('authzStore').findAll('projectRoleTemplate'),
-        });
-    },
+  authzStore: service('authz-store'),
+  model: function () {
+    const project = this.get('authzStore').createRecord({
+      type: `project`,
+      name: '',
+    });
+    const projectRoleTemplateBindings = [{
+      subjectKind: 'User',
+      subjectName: '',
+      projectRoleTemplateId: '',
+      projectId: '',
+    }];
+    return hash({
+      project,
+      projectRoleTemplateBindings,
+      projects: this.get('authzStore').findAll('project'),
+      roles: this.get('authzStore').findAll('projectRoleTemplate'),
+    });
+  },
 });
