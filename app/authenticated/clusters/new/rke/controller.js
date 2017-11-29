196b8123f35f2ab963ac7c7fa3ebe7f0c72113e8
Westly Wright <westly.wright@gmail.com>
2017-11-29 12:59:32 -0700

    final mocks for demo

--- app/authenticated/clusters/new/rke/controller.js
+++ app/authenticated/clusters/new/rke/controller.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 // import { copy } from '@ember/object/internals';
 import { get, set } from '@ember/object';
 import { inject as service } from '@ember/service';
-// import { computed,observer } from '@ember/object';
+import { computed,observer } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import NewOrEdit from 'ui/mixins/new-or-edit';
 import EmberObject from '@ember/object';
@@ -21,12 +21,14 @@ const headersAll = [
     sort: ['sortState','displayName'],
     searchField: 'displayState',
     translationKey: 'generic.state',
+    scope: 'embedded',
   },
   {
     name: 'name',
     sort: ['displayName','id'],
     searchField: 'displayName',
     translationKey: 'generic.name',
+    scope: 'embedded',
   },
   {
     name: 'etcd',
@@ -45,8 +47,11 @@ const headersAll = [
     sort: false,
     searchField: null,
     translationKey: 'clustersPage.addPage.rke.new.headers.labels.worker',
+    scope: 'embedded',
   },
 ];
+
+const workerHeaders = headersAll.filter((x) => x.scope === 'embedded');
 
 const NETWORK = [
   {label: 'clustersPage.addPage.rke.new.options.network.flannel', value: 'flannel'},
@@ -59,21 +64,20 @@ const AUTH = [
 
 export default Controller.extend(NewOrEdit, {
   modal:           service('modal'),
-  clusterStore:           service('cluster-store'),
+  clusterStore:    service('cluster-store'),
   loading:         null,
   newHost:         null,
   canSave:         null,
   primaryResource: alias('model.cluster'),
   config:          alias('primaryResource.rancherKubernetesEngineConfig'),
   scope:           null,
-  headers:         headersAll,
+  headersAll:      headersAll,
+  workerHeaders:   workerHeaders,
   sortBy:          'name',
   searchText:      '',
-  hasEtcd:         null,
-  hasControlPlane: null,
-  hasWorkers:      null,
   authChoices:     AUTH,
   networkChoices:  NETWORK,
+  countMap: null,
 
   init() {
     this._super(...arguments);
@@ -81,11 +85,46 @@ export default Controller.extend(NewOrEdit, {
       loading:         false,
       canSave:         false,
       scope:           'dedicated',
-      hasEtcd:         false,
-      hasControlPlane: false,
-      hasWorkers:      false,
+      countMap: {
+        etcd: 0,
+        controlplane: 0,
+        worker: 0,
+      }
     });
   },
+
+  etcdSafe: computed('countMap.etcd', function() {
+    return get(this, 'countMap.etcd') % 2 ? true : false;
+  }),
+
+  cpSafe: computed('countMap.controlplane', function() {
+    return get(this, 'countMap.controlplane') >= 1 ? true : false;
+  }),
+
+  workerSafe: computed('countMap.worker', function() {
+    return get(this, 'countMap.worker') >= 1 ? true : false;
+  }),
+
+  countState: observer('config.hosts.[]', function() {
+    let hosts = get(this, 'config.hosts');
+    let countMap = {
+      etcd: 0,
+      controlplane: 0,
+      worker: 0,
+    };
+
+    hosts.forEach((host) => {
+      get(host, 'role').forEach((role) => {
+        countMap[role]++;
+      });
+    });
+    set(this, 'countMap', countMap);
+  }),
+
+  scopeChanged: observer('scope', function() {
+    let config = get(this, 'config');
+    set(config, 'hosts', []);
+  }),
 
   actions: {
     addHost() {
@@ -105,7 +144,7 @@ export default Controller.extend(NewOrEdit, {
       let clusterStore = get(this, 'clusterStore');
       let neu = EmberObject.create(RKECONFIGHOST_DEFAULT);
       let config = get(this, 'config');
-      let hosts = get(config, 'hosts') || [];
+      let hosts = ( get(config, 'hosts') || [] ).slice();
       let match = hosts.findBy('advertisedHostname', get(host, 'displayName'))
       if (match) {
         let roles = get(match, 'role');
@@ -132,6 +171,7 @@ export default Controller.extend(NewOrEdit, {
         neu = clusterStore.createRecord(neu);
         hosts.addObject(neu);
       }
+      set(config, 'hosts', hosts); //so the countState observer updates
     }
   }
 });
