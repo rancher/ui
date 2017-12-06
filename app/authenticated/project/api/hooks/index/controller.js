5a63f077475254c573fdaa17641d0898e1027c19
Aaron <aaronwang@rancher.com>
2017-12-06 10:46:25 +0800

    Filter driver `forwardPost` of webhook-service in UI
    We currently just keep `forwardPost` for internal usage. So, users should not be able to see it.
    
    Related Issue is [here](https://github.com/rancher/rancher/issues/10479)

--- app/authenticated/project/api/hooks/index/controller.js
+++ app/authenticated/project/api/hooks/index/controller.js
@@ -4,7 +4,10 @@ import Sortable from 'ui/mixins/sortable';
 export default Ember.Controller.extend(Sortable, {
   settings: Ember.inject.service(),
 
-  sortableContent: Ember.computed.alias('model.receivers'),
+  sortableContent: function() {
+    let receivers = this.get('model.receivers');
+    return receivers.filter(ele => ele.driver !== 'forwardPost');
+  }.property('model.receivers.@each.driver'),
   sortBy: 'name',
   sorts: {
     state:        ['stateSort','name','id'],
