16490cdac4545467f2860a6f7dd8ac489535168b
loganhz <logan@rancher.com>
2017-12-12 15:14:30 +0800

    Fix eslint issue

--- app/authenticated/cluster/projects/edit/route.js
+++ app/authenticated/cluster/projects/edit/route.js
@@ -1,5 +1,6 @@
 import { hash } from 'rsvp';
 import { get } from '@ember/object';
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
