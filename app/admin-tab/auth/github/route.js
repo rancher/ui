294ce294ff26fca8c37dd2ff78546931e43a9ee2
loganhz <logan@rancher.com>
2017-12-14 14:59:59 +0800

    Fix typo

---
+++ app/admin-tab/auth/github/route.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return this.get('authStore').find('config', null, {forceReload: true}).then(function(collection) {
+      return collection;
+    });
+  },
+
+  setupController: function(controller, model) {
+    controller.setProperties({
+      model: model,
+      confirmDisable: false,
+      testing: false,
+      organizations: this.get('session.orgs')||[],
+      errors: null,
+      isEnterprise: (model.get('githubConfig.hostname') ? true : false),
+      secure: model.get('githubConfig.scheme') === 'https://'
+    });
+
+    controller.set('saved',true);
+  }
+});
