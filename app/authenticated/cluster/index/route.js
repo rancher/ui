6b29599bcfdcabe3bce2ff210202aec62fa8b047
loganhz <logan@rancher.com>
2017-12-15 09:49:40 +0800

    Fix

--- app/authenticated/cluster/index/route.js
+++ app/authenticated/cluster/index/route.js
@@ -2,11 +2,11 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  store: service(),
+  globalStore: service(),
   scope: service(),
 
   model: function () {
-    return this.get('store').findAll('machine', { filter: { clusterId: this.get('scope.currentCluster.id') } }).then((nodes) => {
+    return this.get('globalStore').findAll('machine', { filter: { clusterId: this.get('scope.currentCluster.id') } }).then((nodes) => {
       const cluster = this.get('scope').currentCluster;
       return {
         displayName: cluster.name,
