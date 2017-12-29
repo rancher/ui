25deafb8fdcd801846b40685ae8abcb8a5d71d9d
Westly Wright <westly.wright@gmail.com>
2017-12-28 17:33:12 -0700

    get prefs page loading, add change pw

--- app/authenticated/prefs/route.js
+++ app/authenticated/prefs/route.js
@@ -1,57 +1,65 @@
 import EmberObject from '@ember/object';
-import { resolve } from 'rsvp';
+// import { resolve } from 'rsvp';
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
-import { loadScript } from 'ui/utils/load-script';
+// import { loadScript } from 'ui/utils/load-script';
 import C from 'ui/utils/constants';
-import fetch from 'ember-api-store/utils/fetch';
+// import fetch from 'ember-api-store/utils/fetch';
+import { get, set } from '@ember/object';
 
 export default Route.extend({
-  session:   service(),
-  accountId: alias(`session.${C.SESSION.ACCOUNT_ID}`),
-  scope:     service(),
+  session:     service(),
+  accountId:   alias(`session.${C.SESSION.ACCOUNT_ID}`),
+  scope:       service(),
+  globalStore: service(),
 
-  beforeModel() {
-    return loadScript('https://js.stripe.com/v2/').then(() => {
-      Stripe.setPublishableKey(this.get('app.stripe.publishableKey'));
-      return resolve();
-    });
-  },
+  // TODO 3.0?
+  // beforeModel() {
+  //   return loadScript('https://js.stripe.com/v2/').then(() => {
+  //     Stripe.setPublishableKey(this.get('app.stripe.publishableKey'));
+  //     return resolve();
+  //   });
+  // },
 
   model(/*params, transition*/) {
 
     var modelOut = EmberObject.create({
       account: null,
-      stripeCards: null,
+      // stripeCards: null,
+    });
+
+    return get(this, 'globalStore').find('user', 'admin', {forceReload: true}).then((user) => { // TODO 2.0 'user?me=true'
+      set(modelOut, 'account', user);
+      return modelOut;
     });
 
     //only need to populate the passwords for the account right now
-    return this.get('globalStore').find('password').then((/* pwds */) => {
+    // return this.get('globalStore').find('password').then((/* pwds */) => {
 
-      return this.get('globalStore').find('account', this.get('accountId')).then((resp) => {
-        let stripeAccountId = null;
-        if (resp.description && typeof resp.description === 'object') {
-          stripeAccountId = JSON.parse(resp.description).stripeAccountId;
-        }
-        modelOut.account = resp;
+    //   return this.get('globalStore').find('account', this.get('accountId')).then((resp) => {
+    //     let stripeAccountId = null;
+    //     if (resp.description && typeof resp.description === 'object') {
+    //       stripeAccountId = JSON.parse(resp.description).stripeAccountId;
+    //     }
+    //     modelOut.account = resp;
 
-        if (stripeAccountId) {
-          return fetch(`/payment?type=stripe&accountId=${stripeAccountId}`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-          }).then((customer) => {
-            modelOut.stripeCards = customer.body;
-            return modelOut;
-          }).catch(() => {
-            return modelOut;
-          });
-        } else {
-          return modelOut;
-        }
-      });
-    });
+    //     if (stripeAccountId) {
+    //       return fetch(`/payment?type=stripe&accountId=${stripeAccountId}`, {
+    //         method: 'GET',
+    //         headers: {
+    //           'Content-Type': 'application/json'
+    //         },
+    //       }).then((customer) => {
+    //         modelOut.stripeCards = customer.body;
+    //         return modelOut;
+    //       }).catch(() => {
+    //         return modelOut;
+    //       });
+    //     } else {
+    //       return modelOut;
+    //     }
+    //   });
+    // });
   },
 });
