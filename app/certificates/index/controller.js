1e74881c80d879904971e694426c538c65cd1cb0
loganhz <logan@rancher.com>
2017-12-29 22:52:08 +0800

    Update certificates page

--- app/certificates/index/controller.js
+++ app/certificates/index/controller.js
@@ -4,27 +4,16 @@ export default Controller.extend({
   sortBy: 'name',
   headers: [
     {
-      name:           'state',
-      sort:           ['sortState','name','id'],
-      translationKey: 'certificatesPage.index.table.header.state',
-      width:          125,
-    },
-    {
-      name:           'name',
-      sort:           ['name','id'],
+      name: 'name',
+      sort: ['name', 'id'],
       translationKey: 'certificatesPage.index.table.header.name',
     },
     {
-      name:           'cn',
-      searchField:    ['CN'],
-      sort:           ['CN','id'],
-      translationKey: 'certificatesPage.index.table.header.domain',
-    },
-    {
-      name:           'expires',
-      sort:           ['expiresDate','id'],
-      translationKey: 'certificatesPage.index.table.header.expires',
-      width:          120,
+      name: 'created',
+      sort: ['created', 'id'],
+      classNames: 'text-right pr-20',
+      searchField: 'created',
+      translationKey: 'certificatesPage.index.table.header.created',
     },
   ],
 });
