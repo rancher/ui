f8d083685d8e676a065bf3c4d05b9fe5d6b8925d
loganhz <logan@rancher.com>
2018-01-09 18:20:25 +0800

    Update command form

--- app/container/route.js
+++ app/container/route.js
@@ -1,15 +1,17 @@
 import { hash } from 'rsvp';
+import { get } from '@ember/object';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model: function(params) {
+  model: function (params) {
 
-    return this.get('store').find('pod', params.container_id).then((container) => {
+    return get(this, 'store').find('pod', params.container_id).then((container) => {
       let out = {
-        container:     container,
+        instance: get(container, 'containers.firstObject'),
+        container: container,
       };
       if (container.serviceId) {
-        out.service = this.get('store').getById('service', container.serviceId);
+        out.service = get(this, 'store').getById('service', container.serviceId);
       }
       return out;
     });
