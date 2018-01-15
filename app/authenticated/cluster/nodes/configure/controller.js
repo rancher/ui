572024e532d535f19d62bad5d52ed1e671fa4b1f
Westly Wright <westly.wright@gmail.com>
2018-01-15 11:24:05 -0700

    finish updating nodes

--- app/authenticated/cluster/nodes/configure/controller.js
+++ app/authenticated/cluster/nodes/configure/controller.js
@@ -1,7 +1,6 @@
 import Controller from '@ember/controller'
 
 export default Controller.extend({
-  driver: 'azure', //temp for now
   actions: {
     completed(/* neu */) {
       this.transitionToRoute('authenticated.cluster.nodes.index');
