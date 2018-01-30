3f243f1a0f476a8d768cb6160abe12b38671f4c5
loganhz <logan@rancher.com>
2018-01-30 13:29:03 +0800

    Fix api key issue
    https://github.com/rancher/rancher/issues/11183

--- app/models/token.js
+++ app/models/token.js
@@ -46,11 +46,6 @@ export default Resource.extend({
     let l = get(this,'links');
 
     var choices = [
-      { label: 'action.edit', icon: 'icon icon-edit', action: 'edit', enabled: !!l.update },
-      { divider: true },
-      { label: 'action.activate',    icon: 'icon icon-play',         action: 'activate',     enabled: !!a.activate, bulkable: true},
-      { label: 'action.deactivate',  icon: 'icon icon-pause',        action: 'deactivate',   enabled: !!a.deactivate, bulkable: true},
-      { divider: true },
       { label: 'action.remove', icon: 'icon icon-trash', action: 'promptDelete', enabled: !!l.remove, altAction: 'delete', bulkable: true },
       { divider: true },
       { label: 'action.viewInApi', icon: 'icon icon-external-link', action: 'goToApi', enabled: true },
