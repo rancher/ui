0012e40cc34efd7a90fb645199efbdf9d7a2a7fd
Vincent Fiduccia <vincent@rancher.com>
2018-01-31 12:45:18 -0700

    Don't send redundant Authorization header

--- app/initializers/aws-sdk.js
+++ app/initializers/aws-sdk.js
@@ -4,8 +4,9 @@ export function initialize(application) {
   AWS.XHRClient.prototype.handleRequest = function handleRequest(httpRequest, httpOptions, callback, errCallback) {
     httpRequest.endpoint.protocol = 'http:';
     httpRequest.endpoint.port = 80;
-    httpRequest.headers['X-API-Headers-Restrict'] = 'Content-Length';
-    httpRequest.headers['X-API-AUTH-HEADER'] = httpRequest.headers['Authorization'];
+    httpRequest.headers['X-Api-Headers-Restrict'] = 'Content-Length';
+    httpRequest.headers['X-Api-Auth-Header'] = httpRequest.headers['Authorization'];
+    delete httpRequest.headers['Authorization'];
     httpRequest.headers['Content-Type'] = 'rancher:' + httpRequest.headers['Content-Type'];
 
     var endpoint = application.proxyEndpoint+'/';
