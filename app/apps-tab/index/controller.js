2eafbdb55a4e0290e790d7d10bb4809bf199dac1
Westly Wright <westly.wright@gmail.com>
2018-02-23 10:26:14 -0700

    Bugs

--- app/apps-tab/index/controller.js
+++ app/apps-tab/index/controller.js
@@ -2,31 +2,21 @@ import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import Controller, { inject as controller } from '@ember/controller';
 import C from 'ui/utils/constants';
-import { computed, get } from '@ember/object';
+import { computed, get, observer } from '@ember/object';
+import { once } from '@ember/runloop';
 
 export default Controller.extend({
   projectController: controller('authenticated.project'),
   prefs:             service(),
   intl:              service(),
+  catalog:           service(),
   tags:              alias('projectController.tags'),
   sortBy:            'name',
-  expandedApps:    null,
 
-  init() {
-    this._super(...arguments);
-    this.set('expandedApps',[]);
-  },
 
-  actions: {
-    toggleExpand(instId) {
-      let list = get(this,'expandedApps');
-      if ( list.includes(instId) ) {
-        list.removeObject(instId);
-      } else {
-        list.addObject(instId);
-      }
-    },
-  },
+  templatesObsvr: observer('model.apps.[]', function() {
+    once(() => this.send('fetchDeps', get(this, 'model.apps')))
+  }),
 
   filteredApps: computed('model.apps.@each.{type,isFromCatalog,tags,state}','tags', function() {
     var needTags = get(this,'tags');
@@ -39,6 +29,6 @@ export default Controller.extend({
       out = out.filter((obj) => obj.hasTags(needTags));
     }
 
-    return out;
+    return out.sortBy('displayName');
   }),
 });
--- app/apps-tab/index/route.js
+++ app/apps-tab/index/route.js
@@ -1,30 +1,41 @@
-import EmberObject from '@ember/object';
 import { get } from '@ember/object';
-import { allSettled } from 'rsvp';
+import { all } from 'rsvp';
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import { /* parseExternalId, */ parseHelmExternalId } from 'ui/utils/parse-externalid';
 
 export default Route.extend({
   catalog: service(),
-  store: service(),
+  store:   service(),
+
+  actions: {
+    fetchDeps(apps) {
+      this.fetchAppTemplates(apps);
+    },
+  },
+  fetchAppTemplates: function(apps) {
+    const catalog = get(this, 'catalog');
+    let deps      = [];
+
+    apps.forEach((app) => {
+      let extInfo = parseHelmExternalId(app.get('externalId'));
+      deps.push(catalog.fetchTemplate(extInfo.templateId, false));
+    });
+
+    return all(deps);
+  },
 
   model() {
-    const catalog = get(this, 'catalog');
 
-    return this.get('store').find('app').then((apps) => {
-      let deps = [];
-
-      apps.forEach((app) => {
-        let extInfo = parseHelmExternalId(app.get('externalId'));
-        deps.push(catalog.fetchTemplate(extInfo.templateId, false));
-      });
-
-      return allSettled(deps).then(() => {
-        return EmberObject.create({
-          apps,
-        });
-      });
+    return this.get('store').findAll('app').then((apps) => {
+      return {
+        apps,
+      };
     });
+
   },
+
+  afterModel(model/* , transition */) {
+    return this.fetchAppTemplates(get(model, 'apps'));
+  }
 });
