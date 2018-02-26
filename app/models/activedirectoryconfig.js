9807651edc8e4ff8fa0eb05946497c30cfbe5005
Westly Wright <westly.wright@gmail.com>
2018-02-26 15:04:53 -0700

    ad round 2

---
+++ app/models/activedirectoryconfig.js
@@ -0,0 +1,7 @@
+import Resource from 'ember-api-store/models/resource';
+
+var ActiveDirectoryConfig = Resource.extend({
+  type: 'activeDirectoryConfig',
+});
+
+export default ActiveDirectoryConfig;
