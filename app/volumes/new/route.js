4780ecb51ec94801f7db6a2880c70095fcb495f8
Vincent Fiduccia <vincent@rancher.com>
2018-03-20 13:01:37 -0700

    PVC create

--- app/volumes/new/route.js
+++ app/volumes/new/route.js
@@ -1,10 +1,15 @@
-import EmberObject, { get } from '@ember/object';
+import { get } from '@ember/object';
+import { hash } from 'rsvp';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(/*params, transition*/) {
-    const store = get(this,'store');
-    return EmberObject.create({
+    const store = get(this, 'store');
+    const clusterStore = get(this, 'clusterStore');
+
+    return hash({
+      persistentVolumes: clusterStore.findAll('persistentVolume'),
+      storageClasses: clusterStore.findAll('storageClass'),
       pvc: store.createRecord({
         type: 'persistentVolumeClaim',
       }),
