1060c2ce41a9c560024fde0f0f3fa8e8cdc89b84
Vincent Fiduccia <vincent@rancher.com>
2018-03-22 16:39:36 -0700

    kubectl shell, maybe

--- app/authenticated/cluster/route.js
+++ app/authenticated/cluster/route.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { get } from '@ember/object';
 import Preload from 'ui/mixins/preload';
-import { reject } from 'rsvp';
+import { reject, all as PromiseAll } from 'rsvp';
 
 export default Route.extend(Preload, {
   scope: service(),
@@ -13,7 +13,13 @@ export default Route.extend(Preload, {
     return get(this, 'globalStore').find('cluster', params.cluster_id).then((cluster) => {
       return get(this, 'scope').startSwitchToCluster(cluster).then(() => {
         return this.loadSchemas('clusterStore').then(() => {
-          return cluster;
+          return PromiseAll([
+            this.preload('namespace','clusterStore'),
+            this.preload('storageClass','clusterStore'),
+            this.preload('persistentVolume','clusterStore'),
+          ]).then(() => {
+            return cluster;
+          });
         });
       }).catch((err) => {
         // @TODO-2.0 right now the API can't return schemas for a not-active cluster
