827438686646f71c4d8750cab7121b2c4a6dfba3
Vincent Fiduccia <vincent@rancher.com>
2018-03-23 01:03:33 -0700

    Container volume binding

--- app/authenticated/cluster/index/controller.js
+++ app/authenticated/cluster/index/controller.js
@@ -14,18 +14,20 @@ export default Controller.extend({
 
   actions: {
     dashboard() {
-      window.open(this.get('k8s.kubernetesDashboard'),'_blank');
+  //    window.open(this.get('k8s.kubernetesDashboard'),'_blank');
     },
 
     kubectl(e) {
-      if (e.metaKey) {
+/* @TODO-2.0
+   if (e.metaKey) {
         let proj = this.get('scope.currentProject.id');
         later(() => {
           window.open(`//${window.location.host}/env/${proj}/infra/console?kubernetes=true&isPopup=true`, '_blank', "toolbars=0,width=900,height=700,left=200,top=200");
         });
       } else {
+*/
         this.get('modalService').toggleModal('modal-kubectl');
-      }
+ //     }
     },
 
     kubeconfig() {
