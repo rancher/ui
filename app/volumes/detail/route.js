7358657f4d6d7aaa4119b8704215c0c2aaee6dcf
loganhz <logan@rancher.com>
2018-04-08 13:57:17 +0800

    Add volume detail page
    https://github.com/rancher/rancher/issues/12389

---
+++ app/volumes/detail/route.js
@@ -0,0 +1,16 @@
+import { get } from '@ember/object';
+import { hash } from 'rsvp';
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model(params) {
+    const store = get(this, 'store');
+    const clusterStore = get(this, 'clusterStore');
+
+    return hash({
+      persistentVolumes: clusterStore.findAll('persistentVolume'),
+      storageClasses: clusterStore.findAll('storageClass'),
+      pvc: store.find('persistentVolumeClaim', params.volume_id),
+    });
+  },
+});
