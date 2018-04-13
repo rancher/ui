5147d9b9cf1e463f6062f6b5be3bd4eae2c7c74b
Westly Wright <westly.wright@gmail.com>
2018-04-13 11:15:06 -0700

    updates for settings page

--- app/models/setting.js
+++ app/models/setting.js
@@ -2,18 +2,6 @@ import { later } from '@ember/runloop';
 import Resource from 'ember-api-store/models/resource';
 
 export default Resource.extend({
-  isDefault: function() {
-    let source = this.get('source');
-    if ( !source ) {
-      return true;
-    }
-
-    if ( source === 'Database' ) {
-      return false;
-    }
-
-    return this.get('value') === this.get('activeValue');
-  }.property('source','value','activeValue'),
 
   delete() {
     return this._super().then((res) => {
