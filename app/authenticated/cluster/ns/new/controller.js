683359f712e7a0d22fb0f81b896a0f0c642aa54f
Westly Wright <westly.wright@gmail.com>
2018-04-18 17:02:47 -0700

    add 'add namespace' to empty project rows

--- app/authenticated/cluster/ns/new/controller.js
+++ app/authenticated/cluster/ns/new/controller.js
@@ -1,10 +1,14 @@
 import Controller from '@ember/controller'
 import NewOrEdit from 'ui/mixins/new-or-edit';
 import { alias } from '@ember/object/computed';
-import { computed, get } from '@ember/object';
+import { computed, get/* , set */ } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend(NewOrEdit, {
+  scope: service(),
   primaryResource: alias('model.namespace'),
+  queryParams: ['addTo'],
+  addTo: null,
 
   actions: {
     cancel() {
@@ -15,6 +19,10 @@ export default Controller.extend(NewOrEdit, {
   doneSaving() {
     this.send('cancel');
   },
+
+  allProjects: computed('model.allProjects', 'scope.currentCluster.id', function() {
+    return get(this, 'model.allProjects').filterBy('clusterId', get(this,'scope.currentCluster.id'))
+  }),
 
   nameExists: computed('primaryResource.name', 'model.namespaces.@each.name', function () {
     return get(this, 'primaryResource.name') && get(this, 'model.namespaces').findBy('name', get(this, 'primaryResource.name'));
