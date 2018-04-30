81dc15d82585e190d82bc8841123e7b98b41d976
Vincent Fiduccia <vincent@rancher.com>
2018-04-30 15:49:02 -0700

    Init container, container status, EC certs

---
+++ app/models/container.js
@@ -0,0 +1,10 @@
+import Resource from 'ember-api-store/models/resource';
+import DisplayImage from 'shared/mixins/display-image';
+
+var Container = Resource.extend(DisplayImage, {
+});
+
+Container.reopenClass({
+});
+
+export default Container;
