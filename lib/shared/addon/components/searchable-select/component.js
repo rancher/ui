rancher/ui

commit f5b2a95d728aee5b53a42c25b3f16940a6a22549

Fix seachable select issue
https://github.com/rancher/rancher/issues/13482

lib/shared/addon/components/searchable-select/component.js
@@ -95,8 +95,17 @@ export default Component.extend({
       const toBottom = $('body').height() - $(this.$()[0]).offset().top - 60; // eslint-disable-line
       this.set('maxHeight', toBottom < MAX_HEIGHT ? toBottom : MAX_HEIGHT)
       this.set('filter', null);
-      // select text inside input search box, which will let users easey to clear the inputed text.
-      // this.$('.input-search').select();
+      
+      next(() => {
+        const checked = this.$(".searchable-option .icon-check");
+        const options = this.$(".searchable-options");
+        if ( options && checked ) {
+          options.animate({ 
+            scrollTop: `${checked.parent().offset().top - options.offset().top}px` 
+          });
+        }
+      });
+
       this.set('showOptions', true);
     },
     hide() {

commit 201a4a2867c8c1ce9360ce830353a74c7177e533

safety check
fix bug introduced by f5b2a95d728aee5b53a42c25b3f16940a6a22549

lib/shared/addon/components/searchable-select/component.js
@@ -95,13 +95,13 @@ export default Component.extend({
       const toBottom = $('body').height() - $(this.$()[0]).offset().top - 60; // eslint-disable-line
       this.set('maxHeight', toBottom < MAX_HEIGHT ? toBottom : MAX_HEIGHT)
       this.set('filter', null);
-      
+
       next(() => {
         const checked = this.$(".searchable-option .icon-check");
         const options = this.$(".searchable-options");
-        if ( options && checked ) {
-          options.animate({ 
-            scrollTop: `${checked.parent().offset().top - options.offset().top}px` 
+        if ( options.length && checked.length ) {
+          options.animate({
+            scrollTop: `${checked.parent().offset().top - options.offset().top}px`
           });
         }
       });