8adbe0d3be2575539597eb4cf6845cd22581cb8b
loganhz <logan@rancher.com>
2018-05-18 19:14:13 +0800

    Fix workload detail page issue

--- app/workload/controller.js
+++ app/workload/controller.js
@@ -1,146 +1,19 @@
 import { computed } from '@ember/object';
-import { oneWay } from '@ember/object/computed';
-import { inject as service } from '@ember/service';
-import Controller, { inject as controller } from '@ember/controller';
-import { get, set } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import Controller from '@ember/controller';
+import { get } from '@ember/object';
 
 export default Controller.extend({
-  application:        controller(),
-  scope:              service(),
-  allWorkloads:       service(),
+  service:            alias('model.workload'),
 
-  service:            oneWay('model.workload'),
-  namespace: "", 
-  sortBy:             'priority',
-  fixedLaunchConfig:  null,
-  activeLaunchConfig: null,
-  portSortBy:         'privatePort',
-  init(){
-    this._super(...arguments);
-    let namespaceId = null;
-    namespaceId = get(this, 'service.namespaceId');
-
-    if (namespaceId) {
-      let namespace = get(this, 'clusterStore').getById('namespace', namespaceId);
-      if (namespace) {
-        set(this, 'namespace', namespace);
-      }
-    }
-  },
-  headers: [
-    {
-      name:           'priority',
-      sort:           ['priority'],
-      translationKey: 'formBalancerRules.priority.label',
-      width:          85,
-    },
-    {
-      name:           'access',
-      sort:           ['access'],
-      translationKey: 'formBalancerRules.access.label',
-      width:          85,
-    },
-    {
-      name:           'protocol',
-      sort:           ['protocol'],
-      translationKey: 'formBalancerRules.protocol.label',
-      width:          95
-    },
-    {
-      name:           'hostname',
-      sort:           ['hostname'],
-      translationKey: 'formBalancerRules.hostname.label',
-    },
-    {
-      name:           'sourcePort',
-      sort:           ['sourcePort'],
-      translationKey: 'formBalancerRules.sourcePort.label',
-      width:          145
-    },
-    {
-      name:           'path',
-      sort:           ['path'],
-      translationKey: 'formBalancerRules.path.label',
-    },
-    {
-      name:           'target',
-      translationKey: 'formBalancerRules.target',
-    },
-    {
-      name:           'targetPort',
-      sort:           ['targetPort'],
-      translationKey: 'formBalancerRules.targetPort.label',
-      width:          80
-    },
-    {
-      name:           'backendName',
-      sort:           ['backendName'],
-      translationKey: 'formBalancerRules.backendName.label',
-    },
-  ],
-
-  portHeaders: [
-    {
-      name:           'publicIp',
-      sort:           ['ipAddress'],
-      searchField:    'ipAddress',
-      translationKey: 'generic.ipAddress',
-    },
-    {
-      name:           'publicPort',
-      sort:           ['port'],
-      searchField:    'port',
-      translationKey: 'containerPage.portsTab.table.public',
-    },
-  ],
-
-  actions: {
-    selectMaster() {
-      this.set('activeLaunchConfig', this.get('fixedLaunchConfig'));
-    },
-    selectSidekick(idx) {
-      let service = this.get('model.workload');
-      let configs = service.get('secondaryLaunchConfigs');
-
-      this.set('activeLaunchConfig', configs[idx]);
-    },
-    changeService(service) {
-      var transitionTo = this.get('application.currentRouteName');
-
-      if (service.type === 'dnsService') {
-        transitionTo = 'service.links';
-      }
-
-      this.transitionToRoute(transitionTo, service.get('id'));
-    }
-  },
+  launchConfig: null,
 
   displayEnvironmentVars: computed('service.launchConfig.environment', function() {
     var envs = [];
-    var environment = this.get('service.launchConfig.environment')||{};
+    var environment = get(this, 'service.launchConfig.environment')||{};
     Object.keys(environment).forEach((key) => {
       envs.pushObject({key: key, value: environment[key]})
     });
     return envs;
   }),
-
-  serviceLinksNamed: computed('service.serviceLinks.[]', function() {
-    let as = this.get('allWorkloads');
-
-    ( this.get('service.serviceLinks') || []).forEach((link) => {
-      if (link) {
-        let matching = as.matching(link.name, this.get('model.stack'));
-
-        if (matching) {
-          link.setProperties({
-            service: matching
-          })
-        }
-      }
-      return link;
-    });
-
-    return this.get('service.serviceLinks');
-
-  })
 });
