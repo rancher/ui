eaa083e87bdc38f2a35bf94fb1988a3fab0dae50
loganhz <logan@rancher.com>
2018-06-04 16:52:00 +0800

    Update pipeline

--- app/models/pipeline.js
+++ app/models/pipeline.js
@@ -1,210 +1,111 @@
 import Resource from 'ember-api-store/models/resource';
-import branchConditionsEnums from 'shared/utils/branchConditionsEnums';
 import { inject as service } from '@ember/service';
-import { download } from 'shared/utils/util';
-import { get } from '@ember/object';
-
-const ENUMS_STATUSCLASS = {
-  'true':  'bg-success',
-  'false': 'bg-warning',
-};
+import { get, computed } from '@ember/object';
+import C from 'shared/utils/pipeline-constants';
 
 let Pipeline = Resource.extend({
-  router:       service(),
-  access:       service(),
-  modalService: service('modal'),
+  router:          service(),
+  modalService:    service('modal'),
+  type:            'pipeline',
+  canEdit:         false,
+  canViewYaml:     false,
+  canDownloadYaml: false,
 
-  type:         'pipeline',
-  init(...args) {
-    this._super(...args);
-  },
+  lastRun: computed('nextRun', function() {
+    return parseInt(get(this, 'nextRun'), 10) - 1;
+  }),
 
-  isActive: function() {
-    return get(this, 'pipelineState') === 'active' ? true : false;
-  }.property('pipelineState'),
+  relevantState: computed('lastRunState', 'state', function() {
+    if ( get(this, 'state') === 'removing' ) {
+      return 'removing';
+    }
 
-  availableActions: function() {
-    var isActive = get(this, 'isActive');
+    return get(this, 'lastRunState') || 'untriggered';
+  }),
+
+  displayName: computed('repositoryUrl', function() {
+    let tokens = get(this, 'repositoryUrl').split('/') ;
+
+    tokens = tokens.slice(tokens.length - 2);
+    const last = tokens[tokens.length - 1];
+
+    if ( last.endsWith('.git')) {
+      tokens[tokens.length - 1] = last.slice(0, last.length - 4);
+    }
+
+    return tokens.join('/');
+  }),
+
+  availableActions: computed('links.yaml', function() {
     let l = get(this, 'links') || {};
+    let a = get(this, 'actions') || {};
+    const isExample = C.DEMO_REPOSITORIES.findBy('url', get(this, 'repositoryUrl'));
 
-    return [
-      { divider: true },
+    return [{ divider: true },
       {
         label:    'action.run',
         icon:     'icon icon-play',
         action:   'run',
-        enabled:  true,
+        enabled:  !!a.run,
         bulkable: false
       },
       { divider: true },
       {
-        label:    'action.viewConfig',
+        label:    'action.editConfig',
+        icon:     'icon icon-edit',
+        action:   'editConfig',
+        enabled:  !!l.update,
+        bulkable: false
+      },
+      {
+        label:    'action.editYaml',
+        action:   'editYaml',
         icon:     'icon icon-files',
-        action:   'viewConfig',
-        enabled:  !!l.export,
-        bulkable: false
-      },
-      {
-        label:    'action.exportConfig',
-        icon:     'icon icon-download',
-        action:   'exportConfig',
-        enabled:  !!l.export,
+        enabled:  !!l.yaml,
         bulkable: false
       },
       { divider: true },
       {
-        label:    'action.activate',
-        icon:     'icon icon-play',
-        action:   'activate',
-        enabled:  !isActive,
-        bulkable: !isActive
-      },
-      {
-        label:    'action.deactivate',
-        icon:     'icon icon-stop',
-        action:   'deactivate',
-        enabled:  isActive,
-        bulkable: isActive
+        label:    'action.setting',
+        icon:     'icon icon-process',
+        action:   'setting',
+        enabled:  !!l.update && !isExample,
+        bulkable: false
       },
     ];
-  }.property('actions.{run,remove,deactivate,activate,edit,clone}', 'piplineState', 'isActive'),
-
-  images: function() {
-    var images = [];
-    var stages = get(this, 'stages');
-
-    for (var i = 0; i < stages.length; i++) {
-      var item = stages[i].steps;
-
-      for (var j = 0; j < item.length;j++){
-        var itemJ = item[j];
-
-        if (itemJ.type === 'task'){
-          var exist = images.findIndex((ele) => ele === itemJ.image);
-
-          (exist === -1) && images.push(itemJ.image);
-        }
-        if (itemJ.type === 'build'){
-          exist = images.findIndex((ele) => ele === itemJ.targetImage);
-          (exist === -1) && images.push(itemJ.targetImage);
-        }
-      }
-    }
-
-    return images;
-  }.property('stages.@each.{steps}'),
-
-  repository: function() {
-    return get(this, 'stages')[0].steps[0].sourceCodeConfig.url;
-  }.property('stages'),
-
-  branch: function() {
-    return get(this, 'stages')[0].steps[0].sourceCodeConfig.branch;
-  }.property('stages'),
-
-  branchConditionLabel: function() {
-    let branchCondition = get(this, 'stages')[0].steps[0].sourceCodeConfig.branchCondition;
-    let branchEnum = branchConditionsEnums.find((ele) => ele.value === branchCondition);
-
-    return branchEnum && branchEnum.label || '';
-  }.property('stages'),
-
-  branchCondition: function() {
-    let branchCondition = get(this, 'stages')[0].steps[0].sourceCodeConfig.branchCondition;
-
-    return branchCondition;
-  }.property('stages'),
-
-  statusClass: function() {
-    var status = `${ !!get(this, 'isActive') }`;
-
-    return ENUMS_STATUSCLASS[status];
-  }.property('isActive'),
-
-  lastRun: function() {
-    return get(this, 'nextRun') - 1;
-  }.property('nextRun'),
-  cb() {
-    this.delete().then(() => {
-      get(this, 'router').transitionTo('authenticated.project.pipeline.pipelines');
-    })
-  },
-
+  }),
   actions: {
     run() {
-      let branchCondition = get(this, 'branchCondition');
-
-      if (branchCondition === 'only'){
-        return this.doAction('run', { branch: '' })
-          .then(() => {
-            get(this, 'router').transitionTo('authenticated.project.pipeline.pipeline', get(this, 'id'), { queryParams: { mode: 'review' } })
-          });
-      } else {
-        let cb = (params) => {
-          return this.doAction('run', params)
-            .then(() => {
-              get(this, 'router').transitionTo('authenticated.project.pipeline.pipeline', get(this, 'id'), { queryParams: { mode: 'review' } })
-            });
-        }
-
-        get(this, 'modalService').toggleModal('modal-pipeline-run', { cb });
-      }
+      get(this, 'modalService').toggleModal('modal-pipeline-run', {
+        originalModel: this,
+        escToClose:    true,
+      });
     },
 
-    clone() {
-      get(this, 'router').transitionTo('authenticated.project.pipeline.pipeline', get(this, 'id'), { queryParams: { mode: 'duplicate' } })
+    setting() {
+      get(this, 'modalService').toggleModal('modal-pipeline-setting', {
+        originalModel: this,
+        escToClose:    true,
+      });
     },
 
-    edit() {
-      get(this, 'router').transitionTo('authenticated.project.pipeline.pipeline', get(this, 'id'), { queryParams: { mode: '' } })
+    editConfig() {
+      get(this, 'router').transitionTo('authenticated.project.pipeline.pipelines.edit', get(this, 'id'))
     },
 
-    exportConfig() {
-      download(this.linkFor('export'));
+    editYaml() {
+      get(this, 'modalService').toggleModal('modal-pipeline-yaml', {
+        originalModel: this,
+        escToClose:    true,
+      });
     },
-
-    viewConfig() {
-      get(this, 'router').transitionTo('authenticated.project.pipeline.view-config', get(this, 'id'));
-    },
-
-    activate() {
-      return this.doAction('activate');
-    },
-
-    deactivate() {
-      return this.doAction('deactivate');
-    },
-  },
-
-  validationErrors() {
-    var errors = []
-
-    if (!get(this, 'name')) {
-      errors.push('"Pipeline Name" is required');
-    }
-    var allStageNotEmpty = true
-    var stages = get(this, 'stages');
-
-    for (var i = 0; i < stages.length;i++){
-      var item = stages[i]
-
-      if (!item.steps || item.steps.length === 0){
-        allStageNotEmpty = false
-        break;
-      }
-    }
-    if (!allStageNotEmpty){
-      errors.push('Stage must contain at least one Step');
-    }
-
-    return errors;
   },
 
 });
 
 Pipeline.reopenClass({
   mangleIn(data) {
-    if ( data && data.sourceCodeCredential ) {
+    if (data && data.sourceCodeCredential) {
       data.sourceCodeCredential._id = data.sourceCodeCredential.id;
       delete data.sourceCodeCredential.id;
     }
