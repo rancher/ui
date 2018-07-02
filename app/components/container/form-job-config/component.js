b48cbf065715b86811e2b4283d85a3f93c44adcd
loganhz <logan@rancher.com>
2018-07-02 12:39:19 +0800

    Fix eslint issue

--- app/components/container/form-job-config/component.js
+++ app/components/container/form-job-config/component.js
@@ -4,28 +4,42 @@ import { get, computed } from '@ember/object';
 
 export default Component.extend({
   layout,
-  workload: null,
-  scaleMode: null,
-  editing: null,
+  workload:   null,
+  scaleMode:  null,
+  editing:    null,
 
   classNames: ['accordion-wrapper'],
 
-  didReceiveAttrs() {
-    if (!this.get('expandFn')) {
-      this.set('expandFn', function(item) {
-          item.toggleProperty('expanded');
-      });
-    }
-  },
-
   jobConfig: computed('scaleMode', function() {
+
     const scaleMode = get(this, 'scaleMode');
     let config;
+
     if ( scaleMode === 'job' ) {
+
       config = get(this, 'workload.jobConfig');
+
     } else {
+
       config = get(this, 'workload.cronJobConfig.jobConfig');
+
     }
+
     return config;
+
   }),
+
+  didReceiveAttrs() {
+
+    if (!this.get('expandFn')) {
+
+      this.set('expandFn', (item) => {
+
+        item.toggleProperty('expanded');
+
+      });
+
+    }
+
+  },
 });
