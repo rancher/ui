9c2e8bbbd18d8081d85ac76d04f0b02727da14a2
Westly Wright <westly.wright@gmail.com>
2018-07-11 15:27:53 -0700

    Clone Catalog

--- app/models/catalog.js
+++ app/models/catalog.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 import { get } from '@ember/object';
 import { ucFirst } from 'shared/utils/util';
+import C from 'ui/utils/constants';
 
 export default Resource.extend({
   displayKind: computed('kind', function() {
@@ -10,6 +11,17 @@ export default Resource.extend({
     return ucFirst(get(this, 'kind'));
 
   }),
+
+  canClone: computed('actions.clone', function() {
+
+    const name         = get(this, 'name');
+    const catalogNames = get(C, 'CATALOG');
+    const builtIn      = [get(catalogNames, 'LIBRARY_KEY'), get(catalogNames, 'HELM_STABLE_KEY'), get(catalogNames, 'HELM_INCUBATOR_KEY')];
+
+    return !builtIn.includes(name);
+
+  }),
+
   modalService: service('modal'),
 
   actions: {
@@ -17,7 +29,29 @@ export default Resource.extend({
 
       this.get('modalService').toggleModal('modal-edit-catalog', this);
 
+    },
+
+    clone() {
+
+      debugger;
+      const clone = this.cloneForNew();
+
+      this.get('modalService').toggleModal('modal-edit-catalog', clone);
+
     }
   },
 
+  cloneForNew() {
+
+    var copy = this.clone();
+
+    delete copy.id;
+    delete copy.name;
+    delete copy.actionLinks;
+    delete copy.links;
+    delete copy.uuid;
+
+    return copy;
+
+  },
 });
