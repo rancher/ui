c43a842c74c0917d0daab0fec398b3169e25153b
Westly Wright <westly.wright@gmail.com>
2018-07-13 12:35:24 -0700

    Registries clone

--- app/authenticated/project/config-maps/new/route.js
+++ app/authenticated/project/config-maps/new/route.js
@@ -5,9 +5,11 @@ export default Route.extend({
   model(params/* , transition */) {
 
     if (get(params, 'id')) {
+
       const record = get(this, 'store').getById('configMap', get(params, 'id'));
 
       return record.cloneForNew();
+
     }
 
     return this.get('store').createRecord({ type: 'configMap' });
--- app/models/namespaceddockercredential.js
+++ app/models/namespaceddockercredential.js
@@ -1,8 +1,24 @@
 import DockerCredential from './dockercredential';
 import { reference } from 'ember-api-store/utils/denormalize';
 import { inject as service } from '@ember/service';
+import { get } from '@ember/object';
 
 export default DockerCredential.extend({
   namespace:    reference('namespaceId', 'namespace', 'clusterStore'),
   clusterStore: service(),
+
+  canClone: true,
+
+  actions: {
+    clone() {
+
+      get(this, 'router').transitionTo('authenticated.project.registries.new', {
+        queryParams: {
+          id:   get(this, 'id'),
+          type: get(this, 'type')
+        }
+      });
+
+    }
+  },
 });
