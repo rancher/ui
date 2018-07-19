7125a8facaaa000880acd296e97e66d8816212d7
loganhz <logan@rancher.com>
2018-07-19 23:03:20 +0800

    Add shell and logs actions for container in pod detail page
    https://github.com/rancher/rancher/issues/12656

--- app/models/container.js
+++ app/models/container.js
@@ -1,8 +1,49 @@
 import Resource from 'ember-api-store/models/resource';
 import DisplayImage from 'shared/mixins/display-image';
+import { get, computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-var Container = Resource.extend(DisplayImage, {});
+var Container = Resource.extend(DisplayImage, {
+  availableActions: computed('state', function() {
 
-Container.reopenClass({});
+    let isRunning = get(this, 'state') === 'running';
+
+    var choices = [
+      {
+        label:     'action.execute',
+        icon:      'icon icon-terminal',
+        action:    'shell',
+        enabled:   isRunning,
+      },
+      {
+        label:     'action.logs',
+        icon:      'icon icon-file',
+        action:    'logs',
+        enabled:   true,
+      },
+    ];
+
+    return choices;
+
+  }),
+  modalService: service('modal'),
+  links:        {},
+
+  actions:      {
+    shell() {
+
+      get(this, 'modalService').toggleModal('modal-shell', { model: get(this, 'pod') });
+
+    },
+
+    logs() {
+
+      get(this, 'modalService').toggleModal('modal-container-logs', get(this, 'pod'));
+
+    },
+  },
+
+  type:  'container',
+});
 
 export default Container;
