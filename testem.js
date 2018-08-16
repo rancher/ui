540fe6f1d00d07e055d965bc8c26c5370e16b41f
Westly Wright <westly.wright@gmail.com>
2018-08-16 15:06:54 -0700

    Missed what seemed to be a critical update to the testem.js file 3.3

--- testem.js
+++ testem.js
@@ -10,13 +10,15 @@ module.exports = {
   ],
   browser_args: {
     Chrome: {
-      mode: 'ci',
-      args: [
+      ci: [
         // --no-sandbox is needed when running Chrome inside a container
         process.env.TRAVIS ? '--no-sandbox' : null,
         '--disable-gpu',
         '--headless',
         '--no-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
         '--remote-debugging-port=9222',
         '--window-size=1440,900'
       ].filter(Boolean)
