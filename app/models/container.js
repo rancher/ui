83576d3e909f94d97f9cff96d11edf9aca0c72f5
loganhz <logan@rancher.com>
2018-09-03 17:58:18 +0800

    Support exec and logs to selected container
    https://github.com/rancher/rancher/issues/12656

--- app/models/container.js
+++ app/models/container.js
@@ -1,8 +1,53 @@
 import Resource from 'ember-api-store/models/resource';
+import { reference } from 'ember-api-store/utils/denormalize';
 import DisplayImage from 'shared/mixins/display-image';
+import { get, computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-var Container = Resource.extend(DisplayImage, {});
+var Container = Resource.extend(DisplayImage, {
+  modalService: service('modal'),
+  links:        {},
+  type:         'container',
 
-Container.reopenClass({});
+  pod: reference('podId'),
+
+  availableActions: computed('state', function() {
+    let isRunning = get(this, 'state') === 'running';
+
+    var choices = [
+      {
+        label:     'action.execute',
+        icon:      'icon icon-terminal',
+        action:    'shell',
+        enabled:   isRunning,
+      },
+      {
+        label:     'action.logs',
+        icon:      'icon icon-file',
+        action:    'logs',
+        enabled:   true,
+      },
+    ];
+
+    return choices;
+  }),
+
+  actions:      {
+    shell() {
+      get(this, 'modalService').toggleModal('modal-shell', {
+        model:         get(this, 'pod'),
+        containerName: get(this, 'name')
+      });
+    },
+
+    logs() {
+      get(this, 'modalService').toggleModal('modal-container-logs', {
+        model:         get(this, 'pod'),
+        containerName: get(this, 'name')
+      });
+    },
+  },
+
+});
 
 export default Container;
