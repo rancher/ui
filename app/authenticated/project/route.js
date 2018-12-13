176b3d494637c968de3d5cde10f97c11dfa35721
loganhz <logan@rancher.com>
2018-12-13 15:04:21 +0800

    Fix catalog routing issue
    https://github.com/rancher/rancher/issues/17089

--- app/authenticated/project/route.js
+++ app/authenticated/project/route.js
@@ -6,7 +6,7 @@ import { all as PromiseAll } from 'rsvp';
 import Preload from 'ui/mixins/preload';
 import C from 'ui/utils/constants';
 
-const VALID_ROUTES = ['apps-tab', 'authenticated.project.security.members.index',
+const VALID_ROUTES = ['apps-tab', 'catalog-tab.index', 'catalog-tab.launch', 'authenticated.project.security.members.index',
   'authenticated.project.ns', 'authenticated.project.certificates',
   'authenticated.project.secrets', 'authenticated.project.config-maps',
   'authenticated.project.registries', 'authenticated.project.alert',
