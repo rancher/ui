796534504ac54d26502c0f72d8c63f9235ccc19c
Westly Wright <westly.wright@gmail.com>
2018-12-14 14:29:27 -0700

    expose new settings on settings page for user auth info refresh
    rancher/rancher#17074
    
    fix namespaced import error
    
    Add refresh all tokens collection action
    
    rancher/rancher#17075

--- app/models/token.js
+++ app/models/token.js
@@ -1,7 +1,11 @@
 import Resource from '@rancher/ember-api-store/models/resource';
-import { get, computed } from '@ember/object';
+import { get, computed, set } from '@ember/object';
+import { next } from '@ember/runloop'
+import { inject as service } from '@ember/service';
 
 export default Resource.extend({
+  growl:       service(),
+
   state: computed('expired', function() {
     if ( get(this, 'expired') ) {
       return 'expired';
@@ -9,4 +13,48 @@ export default Resource.extend({
 
     return 'active';
   }),
+
+  availableActions: computed('enabled', function() { // eslint-disable-line
+
+    return [
+      {
+        label:      'action.activate',
+        icon:       'icon icon-play',
+        action:     'activate',
+        // enabled: !this.enabled,
+        enabled:    false, // backend was supposed to be ready but its not ready, when it is flip this switch and bingo bango yor're off to the races
+        bulkable:   false
+      },
+      {
+        label:      'action.deactivate',
+        icon:       'icon icon-pause',
+        action:     'deactivate',
+        // enabled: this.enabled,
+        enabled:    false, // backend was supposed to be ready but its not ready, when it is flip this switch and bingo bango yor're off to the races
+        bulkable:   false
+      },
+    ];
+  }),
+
+  actions: {
+    deactivate() {
+      next(() => {
+        set(this, 'enabled', false);
+        this.save().catch((err) => {
+          set(this, 'enabled', true);
+          get(this, 'growl').fromError('Error deactivating token', err)
+        });
+      });
+    },
+
+    activate() {
+      next(() => {
+        set(this, 'enabled', true);
+        this.save().catch((err) => {
+          set(this, 'enabled', false);
+          get(this, 'growl').fromError('Error activating token', err)
+        });
+      });
+    },
+  },
 });
