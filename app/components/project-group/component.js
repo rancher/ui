96e18e241e6723358b01ffdd4350df6cc553c057
Westly Wright <westly.wright@gmail.com>
2019-03-20 23:04:15 -0700

    project group needs to know about other ns's
    rancher/rancher#19022

--- app/components/project-group/component.js
+++ app/components/project-group/component.js
@@ -3,12 +3,13 @@ import layout from './template';
 
 export default Component.extend({
   layout,
-  model:        null,
-  fullColspan:  null,
-  afterName:    0,
-  afterState:   0,
-  alignState:   'text-center',
-  showActions:  true,
-  noGroup:      'namespaceGroup.none',
-  tagName:      '',
+  model:          null,
+  fullColspan:    null,
+  afterName:      0,
+  afterState:     0,
+  alignState:     'text-center',
+  showActions:    true,
+  noGroup:        'namespaceGroup.none',
+  tagName:        '',
+  otherNamespace: null,
 });
