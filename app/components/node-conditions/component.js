b761ed91841f18145094c0f42774c4f5b19d5de0
loganhz <logan@rancher.com>
2019-03-26 10:39:55 +0800

    Fix incorrect status bar issue in node dashboard
    https://github.com/rancher/rancher/issues/18733

--- app/components/node-conditions/component.js
+++ app/components/node-conditions/component.js
@@ -2,6 +2,8 @@ import Component from '@ember/component';
 import { get, observer } from '@ember/object';
 import { inject as service } from '@ember/service';
 import layout from './template';
+const FALSE = 'False';
+const TRUE = 'True';
 
 export default Component.extend({
   intl: service(),
@@ -22,21 +24,26 @@ export default Component.extend({
   setConditions() {
     const conditions = get(this, 'conditionsSource');
 
+    const outOfDisk = conditions.find((c) => c.type === 'OutOfDisk');
+    const diskPressure = conditions.find((c) => c.type === 'DiskPressure');
+    const memoryPressure = conditions.find((c) => c.type === 'MemoryPressure');
+    const ready = conditions.find((c) => c.type === 'Ready');
+
     this.set('conditions', [{
       name:    get(this, 'intl').t('hostsPage.hostPage.conditions.diskSpace'),
-      healthy: (conditions.find((c) => c.type === 'OutOfDisk') || {}).status === 'False',
+      healthy: !outOfDisk || get(outOfDisk, 'status') === FALSE,
     },
     {
       name:    get(this, 'intl').t('hostsPage.hostPage.conditions.diskPressure'),
-      healthy: (conditions.find((c) => c.type === 'DiskPressure') || {}).status === 'False',
+      healthy: !diskPressure || get(diskPressure, 'status') === FALSE,
     },
     {
       name:    get(this, 'intl').t('hostsPage.hostPage.conditions.memoryPressure'),
-      healthy: (conditions.find((c) => c.type === 'MemoryPressure') || {}).status === 'False',
+      healthy: !memoryPressure || get(memoryPressure, 'status') === FALSE,
     },
     {
       name:    get(this, 'intl').t('hostsPage.hostPage.conditions.ready'),
-      healthy: (conditions.find((c) => c.type === 'Ready') || {}).status === 'True',
+      healthy: ready && get(ready, 'status') === TRUE,
     }
     ]);
   },
