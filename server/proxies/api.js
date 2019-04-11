d5eb00012aaec2c7c6df6ce68d6a92a1b8230cf6
Westly Wright <westly.wright@gmail.com>
2019-04-11 10:32:47 -0700

    formatting

--- server/proxies/api.js
+++ server/proxies/api.js
@@ -64,19 +64,19 @@ module.exports = function(app, options) {
   });
 
   let map = {
-    'Project': config.projectEndpoint.replace(config.projectToken, ''),
-    'Cluster': config.clusterEndpoint.replace(config.clusterToken, ''),
-    'Global':  config.apiEndpoint,
-    'Public':  config.publicApiEndpoint,
-    'Magic': config.magicEndpoint,
+    'Project':   config.projectEndpoint.replace(config.projectToken, ''),
+    'Cluster':   config.clusterEndpoint.replace(config.clusterToken, ''),
+    'Global':    config.apiEndpoint,
+    'Public':    config.publicApiEndpoint,
+    'Magic':     config.magicEndpoint,
     'Telemetry': config.telemetryEndpoint,
 
-    'K8s': '/k8s',
-    'Meta': '/meta',
-    'Swagger': '/swaggerapi',
-    'Version': '/version',
-    'Apiui': '/api-ui',
-    'SamlAuth': '/v1-saml',
+    'K8s':       '/k8s',
+    'Meta':      '/meta',
+    'Swagger':   '/swaggerapi',
+    'Version':   '/version',
+    'Apiui':     '/api-ui',
+    'Samlauth':  '/v1-saml',
   }
 
   app.use('/', function(req, res, next) {
