530845a91adeb8e61ab9dc523aa6bc09d59dba97
Westly Wright <westly.wright@gmail.com>
2019-06-26 06:58:29 -0700

    Add ui warning about expiring certificates

--- app/components/modal-rotate-certificates/component.js
+++ app/components/modal-rotate-certificates/component.js
@@ -3,6 +3,7 @@ import { get, set, setProperties } from '@ember/object';
 import Component from '@ember/component';
 import ModalBase from 'shared/mixins/modal-base';
 import layout from './template';
+import { alias } from '@ember/object/computed';
 
 export default Component.extend(ModalBase, {
   growl:            service(),
@@ -13,6 +14,7 @@ export default Component.extend(ModalBase, {
   services:         null,
   selectedServices: null,
   mode:             'single',
+  model:            alias('modalService.modalOpts.model'),
 
   init() {
     this._super(...arguments);
@@ -24,7 +26,38 @@ export default Component.extend(ModalBase, {
   },
 
   didReceiveAttrs() {
-    set(this, 'services', this.modalOpts.serviceDefaults);
+    if (this.model.certsExpiring) {
+      const { expiringCerts } = this.model;
+      let etcdNodes           = (expiringCerts || []).filter((cert) => cert.expiringCertName.includes('etcd'));
+
+      set(this, 'services', this.modalOpts.serviceDefaults.map((cert) => {
+        let expiringCert = null;
+        let expiresIn = null;
+        // this.modalOpts.serviceDefaults;
+
+        if (cert === 'kubelet') {
+          expiringCert = expiringCerts.findBy('expiringCertName', 'kube-node');
+          expiresIn    = expiringCert.daysUntil;
+        } else if (cert === 'etcd'){
+          // there can be multiple etcd nodes with different cert expires, we can grab and alert the soonest expiring cert date since 'rofateCertificates' action will rotates all etcd node certs at the same time.
+          expiringCert = etcdNodes.sortBy('daysUntil').get('firstObject');
+          expiresIn    = expiringCert.daysUntil;
+        } else {
+          expiringCert = expiringCerts.findBy('expiringCertName', cert);
+          expiresIn    = expiringCert.daysUntil;
+        }
+
+        return {
+          label: expiringCert ? `${ cert } (expires in ${ expiresIn } days)` : `${ cert }`,
+          value: cert,
+        }
+      }));
+    } else {
+      set(this, 'services', this.modalOpts.serviceDefaults.map((serv) => ( {
+        label: serv,
+        value: serv
+      } )));
+    }
   },
 
   actions: {
