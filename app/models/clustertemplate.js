3672d97914d0986114242435f3da777f6218d0be
Westly Wright <westly.wright@gmail.com>
2019-07-19 11:57:34 -0700

    Limit cluster template and revision actions for read-only users

--- app/models/clustertemplate.js
+++ app/models/clustertemplate.js
@@ -2,25 +2,29 @@ import Resource from '@rancher/ember-api-store/models/resource';
 import { hasMany } from '@rancher/ember-api-store/utils/denormalize';
 import { computed, get } from '@ember/object';
 import { inject as service } from '@ember/service';
+import C from 'shared/utils/constants';
+import { not } from '@ember/object/computed';
 
 const ClusterTemplate =  Resource.extend({
   globalStore: service(),
   router:      service(),
   modal:       service(),
+  access:      service(),
 
-  revisions:   hasMany('id', 'clustertemplaterevision', 'clusterTemplateId', 'globalStore', null, 'globalStore'),
+  revisions: hasMany('id', 'clustertemplaterevision', 'clusterTemplateId', 'globalStore', null, 'globalStore'),
 
-  canEdit: true,
+  type:      'clustertemplate',
 
-  type:        'clustertemplate',
+  canCloneRevision: not('isReadOnly'),
+  canEdit:          not('isReadOnly'),
 
-  availableActions: computed('actionLinks.[]', () => {
+  availableActions: computed('actionLinks.[]', function() {
     return [
       {
         label:     'action.revision',
         icon:      'icon icon-copy',
         action:    'newRevision',
-        enabled:   true,
+        enabled:   this.canCloneRevision,
       },
     ];
   }),
@@ -31,6 +35,31 @@ const ClusterTemplate =  Resource.extend({
 
   displayDefaultRevisionId: computed('revisionsCount', 'revisions.[]', function() {
     return get(this, 'defaultRevisionId').split(':')[1];
+  }),
+
+  canRemove: computed('links.remove', 'isReadOnly', function() {
+    return !!get(this, 'links.remove') && !get(this, 'isReadOnly');
+  }),
+
+  isReadOnly: computed('access.principal.id', function() {
+    let {
+      members = [],
+      creatorId,
+      access: { principal: { id: currentPrincipalId } }
+    }                  = this;
+    let roles          = C.CLUSTER_TEMPLATE_ROLES;
+    let accessType     = roles.READ_ONLY;
+    let principalMatch = members.findBy('principalId', currentPrincipalId);
+
+    if (principalMatch) {
+      accessType = principalMatch.accessType;
+    } else {
+      if (this.access.me.id === creatorId) {
+        accessType = roles.OWNER;
+      }
+    }
+
+    return accessType === roles.READ_ONLY;
   }),
 
   actions: {
