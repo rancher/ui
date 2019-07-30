e6a80707b470aeb363483eea8103f439e1b1a4fd
Westly Wright <westly.wright@gmail.com>
2019-07-30 10:29:45 -0700

    fix bug in template enforcement and standard user template selct
    rancher/rancher#21747

--- app/authenticated/cluster/edit/controller.js
+++ app/authenticated/cluster/edit/controller.js
@@ -1,7 +1,10 @@
 import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  settings: service(),
+
   queryParams:             ['provider', 'clusterTemplateRevision'],
   provider:                null,
   clusterTemplateRevision: null,
