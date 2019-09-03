d95adf44047667884979703384a2a140bcfd42d8
张浩 <zhanghao@rancher.local>
2019-09-03 16:43:28 +0800

    Show related workloads in secret detail page

--- app/models/secret.js
+++ app/models/secret.js
@@ -4,7 +4,8 @@ import { computed, get } from '@ember/object';
 import Resource from '@rancher/ember-api-store/models/resource';
 
 export default Resource.extend({
-  router:   service(),
+  router:       service(),
+  allWorkloads: service(),
 
   state:    'active',
   canClone: true,
@@ -12,6 +13,18 @@ export default Resource.extend({
   firstKey: alias('keys.firstObject'),
   keys:     computed('data', function() {
     return Object.keys(get(this, 'data') || {}).sort();
+  }),
+
+  workloads: computed('allWorkloads.list.@each.volumes', 'allWorkloads.list.@each.containers', function() {
+    return (get(this, 'allWorkloads.list') || []).map((item) => item.obj).filter((workload) => {
+      if ( get(this, 'namespaceId') && get(workload, 'namespaceId') !== get(this, 'namespaceId')) {
+        return false;
+      }
+      const volume = (get(workload, 'volumes') || []).find((volume) => get(volume, 'secret.secretName') === get(this, 'name'));
+      const env = (get(workload, 'containers') || []).find((container) => (get(container, 'environmentFrom') || []).find((env) => get(env, 'source') === 'secret' && get(env, 'sourceName') === get(this, 'name')));
+
+      return volume || env;
+    });
   }),
 
   actions: {
