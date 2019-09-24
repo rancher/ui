cc7d1bb47773a61c939d5280aa52fcf5e3163ba2
loganhz <logan@rancher.com>
2019-09-24 11:19:07 +0800

    Add Alibaba App Hub in catalog list

--- app/models/catalog.js
+++ app/models/catalog.js
@@ -21,7 +21,7 @@ const Catalog = Resource.extend({
   canClone: computed('actions.clone', function() {
     const name         = get(this, 'name');
     const catalogNames = get(C, 'CATALOG');
-    const builtIn      = [get(catalogNames, 'LIBRARY_KEY'), get(catalogNames, 'HELM_STABLE_KEY'), get(catalogNames, 'HELM_INCUBATOR_KEY')];
+    const builtIn      = [get(catalogNames, 'LIBRARY_KEY'), get(catalogNames, 'ALIBABA_APP_HUB_KEY'), get(catalogNames, 'HELM_STABLE_KEY'), get(catalogNames, 'HELM_INCUBATOR_KEY')];
 
     return !builtIn.includes(name);
   }),
