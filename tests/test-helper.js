a5a103eab6d006dcc1c51dc42608d7245049309b
Westly Wright <westly.wright@gmail.com>
2019-10-07 16:47:00 -0700

    First run on Feature Flags ui
    rancher/rancher#23184
    
    Translation changes
    
    Failing tests
    
    cleanup

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,9 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import preloadAssets from 'ember-asset-loader/test-support/preload-assets';
+import manifest from 'ui/config/asset-manifest';
 
 setApplication(Application.create(config.APP));
 
-start();
+preloadAssets(manifest).then(start); // This ensures all engine resources are loaded before the tests
