b3fd5dff557c989e70af34ae30e8c1fc4c778a54
Cody Jackson <cody@rancher.com>
2019-12-17 16:35:05 -0700

    Make the security-scan-config load for each route
    The security-scan-config wasn't being reloaded when
    navigating between clusters because the config map
    was getting cached. This forces it to be reloaded.
    
    rancher/rancher#24623

--- app/authenticated/cluster/cis/scan/detail/route.js
+++ app/authenticated/cluster/cis/scan/detail/route.js
@@ -15,7 +15,7 @@ export default Route.extend({
     const configMaps = (async() => {
       this.securityScanConfig.setReport(await report);
 
-      return get(this, 'securityScanConfig.asyncConfigMap');
+      return this.securityScanConfig.loadAsyncConfigMap();
     })();
 
     return hash({
