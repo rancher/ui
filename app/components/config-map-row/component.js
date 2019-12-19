eadafbc780792874a07f5cbc501c226090555b55
Westly Wright <westly.wright@gmail.com>
2019-12-19 11:00:54 -0700

    refactor config-map-row to check for binary data keys on configmap

--- app/components/config-map-row/component.js
+++ app/components/config-map-row/component.js
@@ -1,8 +1,27 @@
 import Component from '@ember/component';
 import layout from './template';
+import { computed } from '@ember/object';
+import { isEmpty } from '@ember/utils';
 
 export default Component.extend({
   layout,
   model:    null,
   tagName:  '',
+
+  configKeys: computed('model.keys', 'model.binaryData', function() {
+    const { model: config } = this;
+
+    if (isEmpty(config.keys)) {
+      if (isEmpty(config.binaryData)) {
+        return [];
+      } else {
+        if (!isEmpty(Object.keys(config.binaryData))) {
+          return Object.keys(config.binaryData);
+        }
+      }
+    } else {
+      return config.keys;
+    }
+  }),
+
 });
