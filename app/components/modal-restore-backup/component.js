7c446c6740c6cd88f4c48b56d3c511a26f866fa7
Cody Jackson <cody@rancher.com>
2020-02-26 14:20:25 -0700

    Improve the Restore Snapshot modal - Update the Available Snapshots labels - Reverse the order of Available Snapshots - Change 'Save' button to 'Restore' button - Add restoration type section 'etcd', 'kubernetes version', 'etcd and    kuberneters version'
    rancher/rancher#25525

--- app/components/modal-restore-backup/component.js
+++ app/components/modal-restore-backup/component.js
@@ -1,5 +1,7 @@
 import { inject as service } from '@ember/service';
-import { get, set, setProperties, computed } from '@ember/object';
+import {
+  get, set, setProperties, computed, observer
+} from '@ember/object';
 import Component from '@ember/component';
 import ModalBase from 'shared/mixins/modal-base';
 import layout from './template';
@@ -12,6 +14,7 @@ export default Component.extend(ModalBase, {
   layout,
   classNames:       ['large-modal'],
   backupId:         null,
+  restoreRkeConfig: null,
   loadingBackups:   false,
 
   init() {
@@ -23,11 +26,14 @@ export default Component.extend(ModalBase, {
 
   actions: {
     restore() {
-      const { backupId } = this;
+      const { backupId, restoreRkeConfig } = this;
       const out          = {};
 
       if (backupId) {
         set(out, 'etcdBackupId', backupId);
+        if (restoreRkeConfig !== 'etcd') {
+          set(out, 'restoreRkeConfig', restoreRkeConfig);
+        }
 
         this.modalOpts.cluster.doAction('restoreFromEtcdBackup', out).then(() => {
           this.send('cancel');
@@ -38,17 +44,48 @@ export default Component.extend(ModalBase, {
     }
   },
 
+  updateRestoreRkeConfig: observer('backupId', function() {
+    const value = get(this, 'backupId') ? 'etcd' : '';
+
+    set(this, 'restoreRkeConfig', value);
+  }),
+
   availableBackups: computed('modalOpts.cluster.etcdbackups.[]', function() {
     return get(this, 'modalOpts.cluster.etcdbackups').map((backup) => {
       let time = moment(get(backup, 'created'));
+      const hyphenatedVersion = backup.status.kubernetesVersion
+        ? ` - ${ backup.status.kubernetesVersion }`
+        : '';
 
       return {
         id:      backup.id,
-        label:   `${ backup.displayName } ( ${ time.format('MMMM Do YYYY, H:mm:ss') })`,
+        label:   `( ${ time.format('MMMM Do YYYY, H:mm:ss') })${ hyphenatedVersion }`,
         created: backup.created,
         state:   backup.state,
       }
-    }).sortBy('created');
+    }).sortBy('created').reverse();
+  }),
+
+  selectedBackup: computed('modalOpts.cluster.etcdbackups.[]', 'backupId', function() {
+    const backupId = get(this, 'backupId');
+
+    return !backupId ? null : get(this, 'modalOpts.cluster.etcdbackups').findBy('id', backupId);
+  }),
+
+  selectedVersion: computed('selectedBackup.status.kubernetesVersion', function() {
+    return get(this, 'selectedBackup.status.kubernetesVersion') || this.intl.t('modalRestoreBackup.type.versionUnknown');
+  }),
+
+  k8sVersionDisabled: computed('selectedVersion', 'restorationTypeDisabled', function() {
+    return !get(this, 'restorationTypeDisabled') && get(this, 'selectedVersion') === this.intl.t('modalRestoreBackup.type.versionUnknown');
+  }),
+
+  k8sVersionRadioDisabled: computed('k8sVersionDisabled', 'restorationTypeDisabled', function() {
+    return get(this, 'k8sVersionDisabled') || get(this, 'restorationTypeDisabled');
+  }),
+
+  restorationTypeDisabled: computed('selectedBackup', function() {
+    return !get(this, 'selectedBackup');
   }),
 
   initOwnProperties() {
