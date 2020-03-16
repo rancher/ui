c6ae9d07a3fd65a7a03c6394da2b49b0778e2f81
Cody Jackson <cody@rancher.com>
2020-03-16 13:44:04 -0700

    Adding support for CIS error messages
    We now support displaying an error message when CIS scan fails to execute
    properly.
    
    rancher/rancher#25792

--- app/components/cluster/cis/scan/table-row/component.js
+++ app/components/cluster/cis/scan/table-row/component.js
@@ -1,7 +1,14 @@
 import Component from '@ember/component';
 import layout from './template';
+import { computed } from '@ember/object'
 
 export default Component.extend({
   layout,
-  tagName:    ''
+  tagName:    '',
+  error:   computed('model.status.conditions.@each', function() {
+    return this.model.status.conditions.find((condition) => condition.type === 'Failed')
+  }),
+  errorMessage: computed('error.message', function() {
+    return this.error.message;
+  })
 });
