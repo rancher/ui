d8f049f4f123d50cdda8ce630b66ce0efa7a7122
Westly Wright <westly.wright@gmail.com>
2020-10-02 09:03:58 -0700

    Move Continuous Delivery back to global tools

--- app/instance-initializers/nav.js
+++ app/instance-initializers/nav.js
@@ -332,12 +332,6 @@ const rootNav = [
     resourceScope:  'global',
   },
   {
-    id:                'tools-fleet',
-    localizedLabel:    'nav.tools.fleet',
-    dashboardBaseLink: '/c/local/fleet',
-    scope:             'global',
-  },
-  {
     scope:          'global',
     id:             'global-settings',
     localizedLabel: 'nav.settings.tab',
@@ -440,6 +434,12 @@ const rootNav = [
         route:          'global-admin.cluster-templates',
         resource:       ['clustertemplate'],
         resourceScope:  'global',
+      },
+      { divider: true },
+      {
+        id:                'tools-fleet',
+        localizedLabel:    'nav.tools.fleet',
+        dashboardBaseLink:  '/c/local/fleet',
       },
     ],
   },
