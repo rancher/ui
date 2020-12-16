40f971fc05baed44b8801c08f3ba310d0287c66f
Westly Wright <westly.wright@gmail.com>
2020-12-16 16:18:47 -0700

    Expose rotate encryption keys action and modal
    rancher/rancher#30077

---
+++ app/components/modal-rotate-encryption-key/component.js
@@ -0,0 +1,51 @@
+import Component from '@ember/component';
+import ModalBase from 'shared/mixins/modal-base';
+import layout from './template';
+import { inject as service } from '@ember/service';
+import { get, computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import moment from 'moment';
+
+export default Component.extend(ModalBase, {
+  growl: service(),
+
+  layout,
+  classNames: ['large-modal'],
+
+  etcdBackups: alias('modalOpts.model.etcdbackups'),
+  cluster:     alias('modalOpts.model'),
+
+  actions: {
+    async rotate(cb) {
+      const { cluster } = this;
+
+      try {
+        await cluster.doAction('rotateEncryptionKeys');
+
+        cb(true);
+
+        this.send('cancel');
+      } catch (error) {
+        cb(false);
+      }
+    }
+  },
+
+  lastBackup: computed('etcdBackups.@each.created', function() {
+    const { etcdBackups } = this;
+    const latest = get(etcdBackups, 'firstObject');
+
+    if (!latest) {
+      return null;
+    }
+
+    const backupName = get(latest, 'displayName');
+    const created = moment(get(latest, 'created'));
+    const backupTime = created ? created.format('MMMM Do YYYY, HH:mm:ss') : 'N/A';
+
+    return {
+      backupName,
+      backupTime,
+    }
+  }),
+});
