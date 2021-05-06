e75b037f1d622209f7d69b96413693218adcacbe
Neil MacDougall <neil_macdougall@hotmail.com>
2021-05-06 11:21:56 +0100

    Fix bug with getting url for transition

--- app/initializers/route-spy.js
+++ app/initializers/route-spy.js
@@ -14,11 +14,18 @@ export function initialize() {
 
       willTranstionNotify: function(transition) {
         const router = window.ls('router');
+        let url;
+
+        if (transition.targetName && transition.to) {
+          try {
+            url = router.urlFor(transition.targetName, transition.to.params );
+          } catch (e) {}
+        }
 
         window.top.postMessage({
           action: 'before-navigation',
           target: transition.targetName,
-          url:    router.urlFor(transition.targetName)
+          url
         })
       }.on('willTransition')
     });
@@ -31,7 +38,7 @@ export function initialize() {
       if (msg.action === 'navigate') {
         const router = window.ls('router');
 
-        // If the route being asked for is already loaded, then
+        // If the route being asked for is already loaded, send a did-transition event
         if (router.currentRouteName === msg.name) {
           window.top.postMessage({
             action: 'did-transition',
