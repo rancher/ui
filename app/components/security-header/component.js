7406e370d4ead23bf24d4c3159a3675c57f8650c
Neil MacDougall <neil_macdougall@hotmail.com>
2021-08-19 08:31:19 +0100

    Show legacy banner for project secrets when embedded

--- app/components/security-header/component.js
+++ app/components/security-header/component.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
+
 import layout from './template';
 
-export default Component.extend({ layout, });
+export default Component.extend({
+  layout,
+  showLegacyMessage: window.top !== window,
+});
